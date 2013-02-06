53f781490195bf9a18b54f14d9f7c03be410b031
Surya Vakkalanka <surya.vakkalanka@palm.com>
2013-02-06 11:33:00 -0800

    GF-1370: Moon: Add "disabled" property to all actionable controls

--- samples/RadioButtonSample.js
+++ samples/RadioButtonSample.js
@@ -4,10 +4,18 @@ enyo.kind({
 	fit: true,
 	components: [
 		{kind: "enyo.Spotlight"},
+		{tag:"h3", content:"Radio Buttons"},
 		{name: "testSelectButton", kind: "moon.RadioButtonGroup", onActivate: "buttonActivated", components: [
 			{content: "Cat"},
 			{content: "Dog"},
 			{content: "Hippopotamus"}
+		]},
+		{tag: "br"},{tag: "br"},{tag: "br"},
+		{tag:"h3", content:"Disabled Radio Buttons"},
+		{name: "disabledButton", kind: "moon.RadioButtonGroup", disabled: true, onActivate: "buttonActivated", components: [
+			{content: "Whale", disabled: true},
+			{content: "Shark"},
+			{content: "Dolhpin"}
 		]},
 		{name: "result", classes: "moon-sample-result", content: "result"}
 	],
