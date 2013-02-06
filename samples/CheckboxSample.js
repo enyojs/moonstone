874df7d90e1c9c6b2928cf13894ba5a6de0daf97
Surya Vakkalanka <surya.vakkalanka@palm.com>
2013-02-05 16:06:34 -0800

    Changed item to option

--- samples/CheckboxSample.js
+++ samples/CheckboxSample.js
@@ -7,44 +7,44 @@ enyo.kind({
 		{components: [
 			{classes: "checkbox-sample-wrapper", components: [
 				{classes: "checkbox-sample-wrapper-title", content: "Labeled Checkboxes"},
-				{kind: "moon.LabeledCheckbox", content: "Item 1"},
-				{kind: "moon.LabeledCheckbox", content: "Item 2"},
-				{kind: "moon.LabeledCheckbox", content: "Item 3"},
+				{kind: "moon.LabeledCheckbox", content: "Option 1"},
+				{kind: "moon.LabeledCheckbox", content: "Option 2"},
+				{kind: "moon.LabeledCheckbox", content: "Option 3"},
 				{kind: "moon.LabeledCheckbox", disabled: true, content: "Disabled"},
-				{kind: "moon.LabeledCheckbox", content: "Item 4"},
-				{kind: "moon.LabeledCheckbox", content: "This is a verrry long item 5"}
+				{kind: "moon.LabeledCheckbox", content: "Option 4"},
+				{kind: "moon.LabeledCheckbox", content: "This is a verrry long option 5"}
 			]},
 			{classes: "checkbox-sample-wrapper", components: [
 				{classes: "checkbox-sample-wrapper-title", content: "Labeled Checkbox Group"},
 				{kind: "Group", components: [
-					{kind: "moon.LabeledCheckbox", content: "Item 1"},
-					{kind: "moon.LabeledCheckbox", content: "Item 2"},
-					{kind: "moon.LabeledCheckbox", content: "Item 3"},
+					{kind: "moon.LabeledCheckbox", content: "Option 1"},
+					{kind: "moon.LabeledCheckbox", content: "Option 2"},
+					{kind: "moon.LabeledCheckbox", content: "Option 3"},
 					{kind: "moon.LabeledCheckbox", disabled: true, content: "Disabled"},
-					{kind: "moon.LabeledCheckbox", content: "Item 4"},
-					{kind: "moon.LabeledCheckbox", content: "Item 5"}
+					{kind: "moon.LabeledCheckbox", content: "Option 4"},
+					{kind: "moon.LabeledCheckbox", content: "Option 5"}
 				]}
 			]}
 		]},
 		{components: [
 			{classes: "checkbox-sample-wrapper", components: [
 				{classes: "checkbox-sample-wrapper-title", content: "Labeled Toggle Buttons"},
-				{kind: "moon.LabeledToggleButton", content: "Item 1"},
-				{kind: "moon.LabeledToggleButton", content: "Item 2"},
-				{kind: "moon.LabeledToggleButton", content: "Item 3"},
+				{kind: "moon.LabeledToggleButton", content: "Option 1"},
+				{kind: "moon.LabeledToggleButton", content: "Option 2"},
+				{kind: "moon.LabeledToggleButton", content: "Option 3"},
 				{kind: "moon.LabeledToggleButton", disabled: true, content: "Disabled"},
-				{kind: "moon.LabeledToggleButton", content: "Item 4"},
-				{kind: "moon.LabeledToggleButton", content: "This is a verrry long item 5"}
+				{kind: "moon.LabeledToggleButton", content: "Option 4"},
+				{kind: "moon.LabeledToggleButton", content: "This is a verrry long option 5"}
 			]},
 			{classes: "checkbox-sample-wrapper", components: [
 				{classes: "checkbox-sample-wrapper-title", content: "Labeled Toggle Button Group"},
 				{kind: "Group", components: [
-					{kind: "moon.LabeledToggleButton", content: "Item 1"},
-					{kind: "moon.LabeledToggleButton", content: "Item 2"},
-					{kind: "moon.LabeledToggleButton", content: "Item 3"},
+					{kind: "moon.LabeledToggleButton", content: "Option 1"},
+					{kind: "moon.LabeledToggleButton", content: "Option 2"},
+					{kind: "moon.LabeledToggleButton", content: "Option 3"},
 					{kind: "moon.LabeledToggleButton", disabled: true, content: "Disabled"},
-					{kind: "moon.LabeledToggleButton", content: "Item 4"},
-					{kind: "moon.LabeledToggleButton", content: "Item 5"}
+					{kind: "moon.LabeledToggleButton", content: "Option 4"},
+					{kind: "moon.LabeledToggleButton", content: "Option 5"}
 				]}
 			]}
 		]}
