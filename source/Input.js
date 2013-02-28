6df5df3999d24f0a596c68b029b6228c56c7250b
Lex Podgorny <ap.public1@gmail.comap.public1@gmail.com>
2013-02-28 14:59:57 -0800

    Implementing InputDecorator to work with allowDomDefault()

--- source/Input.js
+++ source/Input.js
@@ -1,42 +1,48 @@
 /**
 	_moon.Input_ is an moon-styled input control, derived from
-	<a href="#enyo.Input">enyo.Input</a>. Typically, an _moon.Input_ is placed
-	inside an <a href="#moon.InputDecorator">moon.InputDecorator</a>, which
+	<a href='#enyo.Input'>enyo.Input</a>. Typically, an _moon.Input_ is placed
+	inside an <a href='#moon.InputDecorator'>moon.InputDecorator</a>, which
 	provides styling, e.g.:
 
-		{kind: "moon.InputDecorator", components: [
-			{kind: "moon.Input", placeholder: "Enter some text...", onchange: "inputChange"}
+		{kind: 'moon.InputDecorator', components: [
+			{kind: 'moon.Input', placeholder: 'Enter some text...', onchange: 'inputChange'}
 		]}
 
 	For more information, see the documentation on
 	[Text Fields](https://github.com/enyojs/enyo/wiki/Text-Fields) in the Enyo
 	Developer Guide.
 */
+
 enyo.kind({
-	name: "moon.Input",
-	kind: "enyo.Input",
+	name	: 'moon.Input',
+	kind 	: 'enyo.Input',
+	classes	: 'moon-input',
+	
 	published: {
-		fieldType: "numeric" // all, email, only text, text/number (no special chars)
+		fieldType: 'numeric' // all, email, only text, text/number (no special chars)
 	},
+	
 	//* @protected
-	classes: "moon-input",
+	/**********************************************/
+	
 	blur: function() {
 		if (this.hasNode()) {
 			this.node.blur();
 		}
 	},
-	left: function(inEvent) {
+	
+	left: function() {
 		if (!this.hasNode() || this.node.selectionStart == 0) {
 			return false;
 		}
-		inEvent.validKey = false;
 		return true;
 	},
-	right: function(inEvent) {
+	
+	right: function() {
 		if (!this.hasNode() || this.node.selectionStart == this.node.value.length) {
 			return false;
 		}
-		inEvent.validKey = false;
 		return true;
 	}
+	
 });
--- source/InputDecorator.js
+++ source/InputDecorator.js
@@ -1,102 +1,162 @@
 /**
 	_moon.InputDecorator_ is a control that provides input styling. Any controls
 	in the InputDecorator will appear to be inside an area styled as an	input.
-	Usually, an InputDecorator surrounds an	<a href="#moon.Input">moon.Input</a>.
+	Usually, an InputDecorator surrounds an	<a href='#moon.Input'>moon.Input</a>.
 
-		{kind: "moon.InputDecorator", components: [
-			{kind: "moon.Input"}
+		{kind: 'moon.InputDecorator', components: [
+			{kind: 'moon.Input'}
 		]}
 
 	Other controls, such as buttons, may be placed to the right or left of the
 	input control, e.g.:
 
-		{kind: "moon.InputDecorator", components: [
-			{kind: "moon.IconButton", src: "search.png"},
-			{kind: "moon.Input"},
-			{kind: "moon.IconButton", src: "cancel.png"}
+		{kind: 'moon.InputDecorator', components: [
+			{kind: 'moon.IconButton', src: 'search.png'},
+			{kind: 'moon.Input'},
+			{kind: 'moon.IconButton', src: 'cancel.png'}
 		]}
 
 	Note that the InputDecorator fits around the content inside it. If the
 	decorator is sized, then its contents will likely need to be sized as well.
 
-		{kind: "moon.InputDecorator", style: "width: 500px;", components: [
-			{kind: "moon.Input", style: "width: 100%;"}
+		{kind: 'moon.InputDecorator', style: 'width: 500px;', components: [
+			{kind: 'moon.Input', style: 'width: 100%;'}
 		]}
 */
+
 enyo.kind({
-	name: "moon.InputDecorator",
-	kind: "enyo.ToolDecorator",
-	tag: "label",
-	classes: "moon-input-decorator",
-	//* @protected
-	handlers: {
-		onDisabledChange: "disabledChange",
-		onfocus: "receiveFocus",
-		onblur: "receiveBlur",
-/*		onSpotlightSelect: "spotSelect",
-		onSpotlightLeft: "spotLeft",
-		onSpotlightRight: "spotRight",
-		onSpotlightUp: "spotUp",
-		onSpotlightDown: "spotDown"*/
+	name		: 'moon.InputDecorator',
+	kind 		: 'enyo.ToolDecorator',
+	tag			: 'label',
+	classes 	: 'moon-input-decorator',
+	spotlight	: true,
+	handlers	: {
+		onDisabledChange	: 'onDisabledChange',
+		onfocus				: 'onFocus',
+		onblur				: 'onBlur',
+		// onSpotlightSelect	: 'onSpotlightSelect',
+		onSpotlightLeft		: 'onSpotlightLeft',
+		onSpotlightRight	: 'onSpotlightRight',
+		// onSpotlightUp		: 'onSpotlightUp',
+		// onSpotlightDown		: 'onSpotlightDown'
+		onSpotlightKeyDown	: 'onSpotlightKeyDown'
 	},
-	spotlight: true,
-	create:function() {
+	
+	//* @protected
+	/**************************************************/
+	
+	_oInputControl: null,
+	
+	// Is control an input field?
+	_isInput: function(oControl) {
+		return (
+			oControl instanceof moon.Input 		||
+			oControl instanceof moon.RichText 	||
+			oControl instanceof moon.TextArea
+		);
+	},
+	
+	// Traverse tree of children to find input control
+	_findInputControl: function(oControl) {
+		oControl = oControl || this;
+		
+		var oInputControl = null;
+		
+		for (var n=0; n<oControl.children.length; n++) {
+			if (this._isInput(oControl.children[n])) {
+				return oControl.children[n];
+			}
+			if (oInputControl = this._findInputControl(oControl.children[n])) {
+				return oInputControl;
+			}
+		}
+	},
+	
+	//* @public
+	/**************************************************/
+	
+	create: function() {
 		this.inherited(arguments);
 		this.updateFocus(false);
+		this._oInputControl = this._findInputControl();
 	},
-	updateFocus:function(focus) {
-		this.focused = focus;
-		this.addRemoveClass("moon-focused", this.alwaysLooksFocused || this.focused);
+	
+	createComponent: function() {
+		this.inherited(arguments);
+		this._oInputControl = this._findInputControl();
 	},
-	receiveFocus: function(inSender, inEvent) {
+	
+	createComponents: function() {
+		this.inherited(arguments);
+		this._oInputControl = this._findInputControl();
+	},
+	
+	updateFocus: function(bFocus) {
+		this.focused = bFocus;
+		this.addRemoveClass('moon-focused', this.alwaysLooksFocused || this.focused);
+	},
+	
+	getInputControl: function() {
+		return this._oInputControl;
+	},
+	
+	
+	// Event handlers:
+	/**************************************************/
+	
+	onFocus: function(oSender, oEvent) {
 		enyo.Spotlight.spot(this);
 		this.updateFocus(true);
 	},
-	receiveBlur: function() {
+	
+	onBlur: function() {
 		this.updateFocus(false);
 	},
-	disabledChange: function(inSender, inEvent) {
-		this.addRemoveClass("moon-disabled", inEvent.originator.disabled);
+	
+	onDisabledChange: function(oSender, oEvent) {
+		this.addRemoveClass('moon-disabled', oEvent.originator.disabled);
 	},
-	spotSelect: function(inSender, inEvent) {
-		var c = inSender.children[0];
-		if (c) {
-			if (!c.hasFocus()) {
-				c.focus();
-				if (c.kind == "moon.RichText") {
-					c.moveCursorToEnd();
-				}
-				return true;
+	
+	// Spotlight Event handlers:
+	/**************************************************/
+	
+	onSpotlightLeft: function(oSender, oEvent) {
+		var oInput = this.getInputControl();
+		if (oInput && oInput.hasFocus() && oInput.left) {
+			if (oInput.left()) {
+				oEvent.allowDomDefault();		// Allow keydown to bubble
+				return true;					// Prevent onSpotlightLeft to bubble
 			}
 		}
-		return false;
 	},
-	spotLeft: function(inSender, inEvent) {
-		var c = inSender.children[0];
-		if (c && c.hasFocus() && c.left) {
-			return c.left(inEvent);
+		
+	onSpotlightRight: function(oSender, oEvent) {
+		var oInput = this.getInputControl();
+		if (oInput && oInput.hasFocus() && oInput.right) {
+			if (oInput.right()) {
+				oEvent.allowDomDefault();		// Allow keydown to bubble
+				return true;					// Prevent onSpotlightLeft to bubble
+			}
 		}
-		return false;
 	},
-	spotRight: function(inSender, inEvent) {
-		var c = inSender.children[0];
-		if (c && c.hasFocus() && c.right) {
-			return c.right(inEvent);
+	 
+	onSpotlightUp: function(oSender, oEvent) {
+		var oInput = this.getInputControl();
+		if (oInput && oInput.hasFocus() && oInput.up) {
+			if (oInput.up()) {
+				oEvent.allowDomDefault();		// Allow keydown to bubble
+				return true;					// Prevent onSpotlightLeft to bubble
+			}
 		}
-		return false;
 	},
-	spotUp: function(inSender, inEvent) {
-		var c = inSender.children[0];
-		if (c && c.hasFocus() && c.up) {
-			return c.up(inEvent);
+
+	onSpotlightDown: function(oSender, oEvent) {
+		var oInput = this.getInputControl();
+		if (oInput && oInput.hasFocus() && oInput.down) {
+			if (oInput.down()) {
+				oEvent.allowDomDefault();		// Allow keydown to bubble
+				return true;					// Prevent onSpotlightLeft to bubble
+			}
 		}
-		return false;
-	},
-	spotDown: function(inSender, inEvent) {
-		var c = inSender.children[0];
-		if (c && c.hasFocus() && c.down) {
-			return c.down(inEvent);
-		}
-		return false;
 	}
 });
