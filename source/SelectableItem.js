1ec07fd7f6387ce423bdeade28c6113ab50642f8
David.Um <joonho.um.snu@gmail.com>
2013-03-02 14:39:15 +0900

    GF-1929: Implement highlight effect at underline
    after 0.5 second, initial color will be faded out.

--- source/SelectableItem.js
+++ source/SelectableItem.js
@@ -19,26 +19,52 @@ enyo.kind({
 	},
 	tools: [
 		{kind: "Animator", onStep: "animatorStep", onEnd: "destroyOverlay"},
-		{tag: "span"}
+		{name: "textUnderline", tag: "span"}
 	],
 	initComponents: function() {
 		var content = this.getContent();
 		this.createChrome(this.tools);
-		this.controls[0].setContent(content);
+		this.$.textUnderline.setContent(content);
 		this.inherited(arguments);
+	},
+	shouldDoTransition: function(inSelected) {
+		return inSelected === true;
 	},
 	tap: function(inSender, e) {
 		if (!this.disabled) {
 			this.setActive(!this.getActive());
+			if(this.shouldDoTransition(this.getActive())) {
+				this.glowTransition(this.getActive());
+			} else {
+				this.destroyOverlay(this.$.overlay);
+			}
 			this.bubble("onchange");
 		}
 		return !this.disabled;
+	},
+	glowTransition: function(inSelected) {
+		if(this.$.overlay) {
+			this.destroyOverlay(this.$.overlay);
+		}
+		var overlay = this.createOverlayComponent().render();
+		setTimeout(function() { overlay.addClass("off"); }, 50);
+	},
+	createOverlayComponent: function() {
+		var content = this.getContent();
+		return this.createComponent({tag: "span", name: "overlay", classes: "moon-overlay", content: content});
+	},
+	destroyOverlay: function(inSender, inEvent) {
+		var overlay = this.$.overlay;
+		if(overlay && inSender === overlay) {
+			overlay.setShowing(false);
+			overlay.destroy();
+		}
 	},
 	selectedChanged: function() {
 		this.setNodeProperty("selected", this.selected);
 		this.setAttribute("selected", this.selected ? "selected" : "");
 		this.setActive(this.selected);
-		this.controls[0].addRemoveClass("moon-underline", this.selected)
+		this.$.textUnderline.addRemoveClass("moon-underline", this.selected)
 	},
 	// active property, and onActivate event, are part of "GroupItem" interface
 	// that we support in this object
