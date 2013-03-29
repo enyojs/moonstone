25ef57414875f180c9260d20e530ad46981b53a0
steven feaster <steven@stevens-MacBook-Pro-2.local>
2013-03-28 17:23:35 -0700

    https://jira2.palm.com/browse/GF-2249 and https://jira2.palm.com/browse/GF-2802 - adding scrollers to ImageItemSample, LabeledTextItemSamle, SelectableItemSample, TimePickerSample + DatePickerSample and fixing scrollers on ButtonSample, CheckboxSample and InputSample

--- samples/DatePickerSample.js
+++ samples/DatePickerSample.js
@@ -7,24 +7,26 @@ enyo.kind({
 		onChange: "changed"
 	},
 	components: [
-		{kind: "moon.ExpandablePicker", noneText: "No Language Selected", autoCollapse: true, content: $L("Choose Locale"), classes: "moon-expandable-picker-wrapper", onChange:"pickerHandler", components: [
-			{content: 'en_us', active:true},
-			{content: 'en_ca'},
-			{content: 'en_ie'},
-			{content: 'en_gb'},
-			{content: 'en_mx'},
-			{content: 'de_de'},
-			{content: 'fr_fr'},
-			{content: 'fr_ca'},
-			{content: 'it_it'},
-			{content: 'es_es'},
-			{content: 'es_mx'},
-			{content: 'es_us'}																																																								
-		]},	
-		{kind: "enyo.Spotlight"},
-		{kind: "moon.DatePicker", noneText: "Pick a Date", content: "Date", classes: "moon-date-picker-wrapper"},		
-		{name:"date", style:"font-size:0.35em;font-family:PreludeWGL Light"},
-		{kind: "moon.DatePicker", disabled: true, noneText: "Disabled Date Picker", content: "Disabled Date", classes: "moon-date-picker-wrapper"}
+		{kind: "enyo.Spotlight"},	
+		{name: 'scroller', kind: 'moon.Scroller', fit: true, touch: true, classes:"moon-date-picker-sample-scroller", components: [	
+			{kind: "moon.ExpandablePicker", noneText: "No Language Selected", autoCollapse: true, content: $L("Choose Locale"), classes: "moon-expandable-picker-wrapper", onChange:"pickerHandler", components: [
+				{content: 'en_us', active:true},
+				{content: 'en_ca'},
+				{content: 'en_ie'},
+				{content: 'en_gb'},
+				{content: 'en_mx'},
+				{content: 'de_de'},
+				{content: 'fr_fr'},
+				{content: 'fr_ca'},
+				{content: 'it_it'},
+				{content: 'es_es'},
+				{content: 'es_mx'},
+				{content: 'es_us'}																																																								
+			]},	
+			{kind: "moon.DatePicker", noneText: "Pick a Date", content: "Date", classes: "moon-date-picker-wrapper"},		
+			{name:"date", style:"font-size:0.35em;font-family:PreludeWGL Light"},
+			{kind: "moon.DatePicker", disabled: true, noneText: "Disabled Date Picker", content: "Disabled Date", classes: "moon-date-picker-wrapper"}
+		]}
 	],
 	create: function(){
 		this.inherited(arguments);
