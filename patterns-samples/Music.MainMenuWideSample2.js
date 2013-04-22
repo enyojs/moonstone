f064415a4026fd0289dcf3c4a4b9bca6b2386c79
David.Um <joonho.um.snu@gmail.com>
2013-04-22 15:34:01 -0700

    GF-3690: Apply moon.Panel.
    Actual css styel can be adjusted after Red Line decided.
    
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

--- patterns-samples/Music.MainMenuWideSample2.js
+++ patterns-samples/Music.MainMenuWideSample2.js
@@ -1,16 +1,17 @@
 enyo.kind({
     name: "moon.sample.music.MainMenuWideSample2",
-    layoutKind: "enyo.FittableRowsLayout",
-    fit: true,
-    title: "Main Menu",
-    titleAbove: "01",
+    kind: "moon.Panel",
     classes: "enyo-unselectable moon moon-music-mainmenu",
+    fit: true,
+    index: "01",
+    title: "Main Menu",
+    
     components: [
         {kind: "enyo.Spotlight"},
-        {kind: "moon.Header", classes: "moon-music-mainmenu-header", content: "Main Menu", titleAbove: "01"},
         {
             name: "columns",
             kind: "FittableColumns",
+            fit: true,
             components: [
                 {
                     classes: "moon-music-mainmenu-menu",
@@ -21,30 +22,14 @@ enyo.kind({
                     ]
                 },
                 {
-                    name: "content",
                     fit: true,
+                    /**
+                        place any control instead of followings.
+                    */
                     classes: "moon-music-mainmenu-content",
-                    components: [
-                        {
-                            name: "branding",
-                            fit: true,
-                            classes: "moon-music-mainmenu-branding",
-                            content: "branding"
-                        }
-                    ]
+                    content: "branding"
                 }
             ]
         }
-    ],
-    
-    rendered: function() {
-        this.inherited(arguments);
-        this.resizeBranding();
-    },
-    
-    resizeBranding: function() {
-        var w = this.$.content.getBounds().width;
-        var h = this.getBounds().height - this.$.columns.getBounds().top - 2;
-        this.$.branding.setBounds({width: w, height: h});
-    }
+    ],  
 });
