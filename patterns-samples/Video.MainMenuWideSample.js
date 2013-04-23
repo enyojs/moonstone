350787b1021e1c25d7d07223a83049b81caabdfb
David.Um <joonho.um.snu@gmail.com>
2013-04-23 14:30:02 -0700

    Adjust tempalte sample using moon.Panel.
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

--- patterns-samples/Video.MainMenuWideSample.js
+++ patterns-samples/Video.MainMenuWideSample.js
@@ -1,51 +1,26 @@
 enyo.kind({
     name: "moon.sample.video.MainMenuWideSample",
-    layoutKind: "enyo.FittableRowsLayout",
+    kind: "moon.Panel",
     classes: "enyo-unselectable moon moon-video-mainmenu",
     fit: true,
-    title: "Main Menu",
     titleAbove: "01",
+    title: "Main Menu",
     components: [
         {kind: "enyo.Spotlight"},
-        {kind: "moon.Header", content: "Main Menu", titleAbove: "01"},
-        {
-            name: "columns",
-            kind: "FittableColumns",
-            components: [
-                {
-                    classes: "moon-video-mainmenu-menu",
-                    components: [
-                        {kind: "moon.Item", content: "Browser Movies", spotlight: true},
-                        {kind: "moon.Item", content: "Browser TV Shows", spotlight: true},
-                        {kind: "moon.Item", content: "Queue", spotlight: true},
-                        {kind: "moon.Item", content: "Search", spotlight: true}
-                    ]
-                },
-                {
-                    name: "content",
-                    fit: true,
-                    classes: "moon-video-mainmenu-content",
-                    components: [
-                        {
-                            name: "branding",
-                            fit: true,
-                            classes: "moon-video-mainmenu-branding",
-                            content: "branding"
-                        }
-                    ]
-                }
-            ]
-        }
-    ],
-    
-    rendered: function() {
-        this.inherited(arguments);
-        this.resizeBranding();
-    },
-    
-    resizeBranding: function() {
-        var w = this.$.content.getBounds().width;
-        var h = this.getBounds().height - this.$.columns.getBounds().top - 2;
-        this.$.branding.setBounds({width: w, height: h});
-    }
-});
+        {kind: "FittableColumns", components: [
+            {
+                classes: "moon-video-mainmenu-menu",
+                components: [
+                    {kind: "moon.Item", content: "Browser Movies", spotlight: true},
+                    {kind: "moon.Item", content: "Browser TV Shows", spotlight: true},
+                    {kind: "moon.Item", content: "Queue", spotlight: true},
+                    {kind: "moon.Item", content: "Search", spotlight: true}
+                ]
+            },
+            {
+                classes: "moon-video-mainmenu-content",
+                content: "branding"
+            }
+        ]}
+    ]
+ });
