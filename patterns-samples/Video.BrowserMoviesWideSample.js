80674ce290241693e0a035ac24021017ccc9f539
David.Um <joonho.um.snu@gmail.com>
2013-04-23 16:12:53 -0700

    Apply moon.Panel to Video browser movie sample
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

--- patterns-samples/Video.BrowserMoviesWideSample.js
+++ patterns-samples/Video.BrowserMoviesWideSample.js
@@ -1,97 +1,58 @@
 enyo.kind({
     name: "moon.sample.video.BrowserMoviesWideSample",
-    layoutKind: "enyo.FittableRowsLayout",
-	classes: "enyo-unselectable moon moon-video-browsermovies",
+    kind: "moon.Panel",
+    classes: "enyo-unselectable moon moon-video-browsermovies",
     fit: true,
-    title: "Browser Movies",
     titleAbove: "02",
+    title: "Browser Movies",
+    imgList: [
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},
+        {src: "assets/album.png", name: "MOVIE NAME"},                
+    ],    
     components: [
         {kind: "enyo.Spotlight"},
         {
-            name: "header",
-            kind: "moon.Header",
-            content: "Browser Movies",
-            titleAbove: "02",
-        	components: [
-                {
-                    classes: "moon-video-browsermovie-header-button",
-                    components: [
-                        {kind: "moon.IconButton", src: "assets/icon-list.png"}
-                    ]
-                }
-            ]
-        },
-        {
-            name: "container",
             classes: "moon-video-browsermovies-container",
+            name: "gridlist",
+            kind: "moon.GridList",
             fit: true,
-            spotlight: "container",
+            count: 12,
+            onSetupItem: "setupGridItem",
+            touch: true,
+            itemWidth: 230,
+            itemHeight: 130,
+            itemSpacing: 30,
             components: [
-                {
-                    name: "list",
-                    kind: "moon.List",
-                    spotlight: true,
-                    orient:"v",
-                    count: 0,
-                    multiSelect: false,
-                    showing: false,
-                    classes: "enyo-fit moon-video-browsermovies-list",
-            		onSetupItem: "setupItem",
-                    components: [
-            			{
-                            name: "item",
-                            kind: "enyo.FittableColumns",
-                            classes: "moon-video-browsermovies-item",
-                            components: [
-                                {name: "image", tag: "img", classes: "moon-video-browsermovies-item-image"},
-                                {kind: "enyo.FittableRows", classes: "moon-video-browsermovies-item-label", components: [{name: "name"}, {name: "id"}]}
-                            ]
-                        }
-            		]
-                }
+                {name: "gridItem", kind: "moon.GridList.ImageItem"},
+                {name: "gridCaption", classes: "moon-video-browsermovies-item-label"}
             ]
         }
     ],
-    
-    rendered: function() {
-        this.inherited(arguments);
-        this.resizeList();
-        this.search();
+
+    headerComponents: [
+        {classes: "moon-video-browsermovie-header-button", components: [
+            {kind: "moon.IconButton", src: "assets/icon-list.png"}
+        ]}
+    ],    
+
+    setupGridItem: function(inSender, inEvent) {
+        var i = inEvent.index;
+        var gridItem = this.imgList[i];
+        if (!gridItem.src) {
+            return;
+        }
+        this.$.gridItem.setSource(gridItem.src);
+        this.$.gridCaption.setContent(gridItem.name);
+        this.$.gridItem.setSelected(this.$.gridlist.isSelected(i));
     },
-    
-    resizeList: function() {
-        var rect = this.$.container.getBounds();
-        this.$.list.setBounds({top: rect.top});
-    },
-    
-    search: function() {
-		var params = {
-			method: "flickr.photos.search",
-			format: "json",
-			api_key: "2a21b46e58d207e4888e1ece0cb149a5",
-			per_page: 50,
-			page: 0,
-			text: "korean sushi",
-			sort: "date-posted-desc",
-			extras: "url_m"
-		};
-		new enyo.JsonpRequest({url: "http://api.flickr.com/services/rest/", callbackName: "jsoncallback"}).response(this, "processFlickrResults").go(params);
-	},
-    
-	processFlickrResults: function(inRequest, inResponse) {
-		this.results = inResponse.photos.photo;
-        this.$.header.set("titleBelow", this.results.length + " Movies");
-        this.$.list.setCount(this.results.length);
-        this.$.list.setShowing(true);
-        this.$.list.render();
-	},
-    
-    setupItem: function(inSender, inEvent) {
-		var i = inEvent.index;
-        var item = this.results[i];
-		this.$.item.addRemoveClass("list-sample-selected", inSender.isSelected(i));
-		this.$.image.set("src", item.url_m);
-		this.$.name.setContent(item.title);
-		this.$.id.setContent(item.id);
-	}
 });
