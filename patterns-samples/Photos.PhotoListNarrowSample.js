21dc1af7748dc8e4c81ec8bd35b69bdbab4f866c
sojujazz <sojujazz@naver.com>
2013-04-24 08:23:08 +0900

    GF-3704 : change css apply method
    GF-3704 : change css apply method

--- patterns-samples/Photos.PhotoListNarrowSample.js
+++ patterns-samples/Photos.PhotoListNarrowSample.js
@@ -1,81 +1,47 @@
 enyo.kind({
     name: "moon.sample.photos.PhotoListNarrowSample",
     kind : "moon.Panel",
-    classes: "moon enyo-unselectable",
+    //classes: "moon enyo-unselectable",
+    classes : "photo-album",
     fit: true,
     title : "ALBUM",
     titleAbove : "02",
     titleBelow : "97 Photos",
-
-    create: function() {
-        this.inherited(arguments);
-    },
-
     components: [
         {kind : "enyo.Spotlight"},
         {
             kind : "FittableRows",
             fit : true,
-            classes : "sample-photo-narrow-list",   
             components : [
                 {                   
-                    kind : "moon.sample.photos.PhotoListNarrowSample.photoList",
+                    kind : "moon.Scroller",
+                    style : "width:300px;height:500px;",
+                    touch : true,
+                    horizontal : "hidden",
+                    components : [
+                        {
+                            kind: "moon.ImageItem",
+                            source: "./assets/default-movie.png",
+                            label : "",
+                        },
+                        {
+                            kind: "moon.ImageItem",
+                            source: "./assets/default-movie.png",
+                            label : "",
+                        },
+                        {
+                            kind: "moon.ImageItem",
+                            source: "./assets/default-movie.png",
+                            label : "",
+                        },
+                        {
+                            kind: "moon.ImageItem",
+                            source: "./assets/default-movie.png",
+                            label : "",
+                        },
+                    ]
                 }
             ]
         }
     ]
-});
-
-// photo list
-enyo.kind({
-    name : "moon.sample.photos.PhotoListNarrowSample.photoList",
-    kind : "moon.Scroller",
-    style : "width:350px;height:550px;",
-    touch : true,
-    horizontal : "hidden",
-    components : [
-        {
-            kind: "sample.photo.PhotoListItem",
-            source: "./assets/default-movie.png",
-        },
-        {
-            kind: "sample.photo.PhotoListItem",
-            source: "./assets/default-movie.png",
-        },
-        {
-            kind: "sample.photo.PhotoListItem",
-            source: "./assets/default-movie.png",
-        },
-        {
-            kind: "sample.photo.PhotoListItem",
-            source: "./assets/default-movie.png",
-        },
-    ]
-});
-
-// phot list item
-enyo.kind({
-    name: "sample.photo.PhotoListItem",
-    classes: "sample-photo-list-item",
-    kind: "moon.Item",
-    components:[
-        {
-            name: 'image', 
-            kind: 'enyo.Image',
-        }
-    ],
-    published: {
-        source: '',
-    },
-    create: function() {
-        this.inherited(arguments);
-        this.sourceChanged();
-    },
-
-    sourceChanged: function() {
-        if (!this.source || this.source === '') {
-            return;
-        }
-        this.$.image.setAttribute('src', this.source);
-    },
 });
