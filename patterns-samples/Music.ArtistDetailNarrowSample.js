05f8a17dc38f54778cae3a43648f878e0d5f6ff2
kyduke <kyduke@gmail.com>
2013-04-23 11:31:29 -0700

    GF-3698: Apply moon.Panel and remove moon.Header
    Enyo-DCO-1.1-Signed-off-by: Youngduke Koh <youngduke.koh@lge.com>

--- patterns-samples/Music.ArtistDetailNarrowSample.js
+++ patterns-samples/Music.ArtistDetailNarrowSample.js
@@ -1,26 +1,21 @@
 enyo.kind({
     name: "moon.sample.music.ArtistDetailNarrowSample",
-    layoutKind: "enyo.FittableRowsLayout",
+    kind: "moon.Panel",
 	classes: "enyo-unselectable moon moon-music-detail",
     fit: true,
-    title: "Movie Name",
-    titleAbove: "03",
+    title: "Artist",
+    titleAbove: "04",
+    headerComponents: [
+        {
+            classes: "moon-music-detail-header-button",
+            components: [
+                {kind: "moon.IconButton", src: "assets/icon-like.png"},
+                {kind: "moon.IconButton", src: "assets/icon-next.png", classes: "moon-music-detail-header-button-right"}
+            ]
+        }
+    ],
     components: [
         {kind: "enyo.Spotlight"},
-        {
-            kind: "moon.Header",
-            content: "Artist",
-            titleAbove: "04",
-            components: [
-                {
-                    classes: "moon-music-detail-header-button",
-                    components: [
-                        {kind: "moon.IconButton", src: "assets/icon-like.png"},
-                        {kind: "moon.IconButton", src: "assets/icon-next.png", classes: "moon-music-detail-header-button-right"}
-                    ]
-                }
-            ]
-        },
         {
             name: "container",
             kind: "FittableColumns",
@@ -73,14 +68,12 @@ enyo.kind({
                         {kind: "moon.Divider", classes: "moon-music-detail-top-devider", content: "Top 10 Tracks"},
                         {
                             name: "listContainer",
-                            fit: true,
                             spotlight: "container",
                             components: [
                                 {
                                     name: "list",
                                     kind: "moon.List",
-                                    classes: "enyo-fit",
-                                    orient: "v",
+                                    style: "height: 300px;",
                                     count: 10,
                                     multiSelect: false,
                             		onSetupItem: "setupItem",
@@ -132,8 +125,9 @@ enyo.kind({
         d = Math.round((d - 168) * 0.5);
         this.$.play.setStyle("margin: " + d + "px 0px 0px " + d + "px;");
         
+        this.$.listContainer.setBounds({height: this.getAbsoluteBounds().height - this.$.listContainer.getAbsoluteBounds().top});
         var rect = this.$.listContainer.getBounds();
-        this.$.list.setBounds({top: rect.top, left: rect.left});
+        this.$.list.setBounds({height: rect.height});
     },
     
     setupItem: function(inSender, inEvent) {
