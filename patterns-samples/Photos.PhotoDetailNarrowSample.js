b67c18ba7250fde3724128ecb0c978f7ab85dc25
sojujazz <sojujazz@naver.com>
2013-04-24 08:42:23 +0900

    GF-3713 : change css style
    GF-3713 : change css style

--- patterns-samples/Photos.PhotoDetailNarrowSample.js
+++ patterns-samples/Photos.PhotoDetailNarrowSample.js
@@ -1,20 +1,16 @@
 enyo.kind({
     name: "moon.sample.photos.PhotoDetailNarrowSample",
     kind : "moon.Panel",
-    classes: "moon enyo-unselectable",
+    classes: "photo-detail",
     fit: true,
     title : "PHOTO NAME",
     titleAbove : "03",
     titleBelow : "2013-04-08",
-
-    create: function() {
-        this.inherited(arguments);
-    },
-
+    
     headerComponents : [
-        { kind : "moon.IconButton", style : "border:none;", src : "assets/icon-favorite.png", style : "margin : 0px 20px 10px 0px;" },
-        { kind : "moon.IconButton", style : "border:none;", src : "assets/icon-download.png", style : "margin : 0px 20px 10px 0px;"},
-        { kind : "moon.IconButton", style : "border:none;", src : "assets/icon-next.png", style : "margin : 0px 20px 10px 0px;" },
+        { kind : "moon.IconButton", style : "border:none;", src : "assets/icon-favorite.png"},
+        { kind : "moon.IconButton", style : "border:none;", src : "assets/icon-download.png"},
+        { kind : "moon.IconButton", style : "border:none;", src : "assets/icon-next.png"},
     ],
 
     components: [
@@ -22,7 +18,7 @@ enyo.kind({
         {
             kind : "enyo.Image",
             src : "./assets/default-movie.png",
-            style : "margin: 20px 0px 0px 0px;width:600px; height:400px;"
+            style : "width:600px;height:400px;"
         }
     ]
 });
