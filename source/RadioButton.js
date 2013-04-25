cb4168c4ad69f9c3496a75d12b8ba54774a77539
Krishna Rangarajan <krishna.rangarajan@lge.com>
2013-04-25 12:09:38 -0700

    GF-2731: moon.RadioButton - Text is cropped and underline is not positioned correctly Enyo-DCO-1.1-Signed-off-by: Krishna Rangarajan <krishna.rangarajan@lge.com>

--- source/RadioButton.js
+++ source/RadioButton.js
@@ -10,12 +10,26 @@ enyo.kind({
 	name: "moon.RadioButton",
 	kind: "moon.Button",
 	classes: "moon-item moon-radio-button",
+	handlers: {
+		onSpotlightFocus : 'focus',
+		onSpotlightBlur	: 'blur'
+	},
+	
 	//* @protected
 	rendered: function() {
 		this.inherited(arguments);
 		this.contentWidth = this.getBounds().width;
 		// Resize the button to fit RadioButton kerning state
-		this.applyStyle("width", this.contentWidth + "px");
+		//25 extra pixels to make room for spotlight focus
+		this.applyStyle("width", this.contentWidth + 25 + "px");
+	},
+	focus: function(inSender, inEvent) {
+		this.bubble(inSender, inEvent);
+		// return false;
+	},
+	blur: function(inSender, inEvent) {
+		this.bubble(inSender, inEvent);
+		// return false;
 	},
 	contentWidth: 0
 });
