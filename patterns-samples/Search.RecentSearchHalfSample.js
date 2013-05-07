fc5a38ae54437bc82003a0678d923e5cd2e37410
David.Um <joonho.um.snu@gmail.com>
2013-05-07 21:44:43 +0900

    GF-4284: Implement Recent Search half sample
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

---
+++ patterns-samples/Search.RecentSearchHalfSample.js
@@ -0,0 +1,50 @@
+enyo.kind({
+    name: "moon.sample.search.RecentSearchHalfSample",
+    kind: "moon.Panel",
+	classes: "enyo-unselectable moon moon-search",
+    fit: true,
+    titleAbove: "02",
+    title: "SEARCH",
+
+    components: [
+        {kind: "enyo.Spotlight"},
+        {
+            layoutKind: "FittableRowsLayout",
+            fit: true,
+            components: [
+                {
+                    layoutKind: "FittableColumnsLayout",
+                    components: [
+                        {kind: "moon.Divider", classes: "moon-search-devider", content: "SUGGESTED SEARCH"},
+                        {kind: "moon.Divider", fit: true, classes: "moon-search-devider", content: "RECENT SEARCHES"},
+                        {kind: "moon.Divider", classes: "moon-search-button", components: [
+                            {kind: "moon.IconButton", src: "assets/icon-round-delete.png", classes: "moon-search-button-right"},
+                        ]},
+                    ]
+                },
+                {
+                    kind: "moon.List",
+                    count: 10,
+                    fit: true,
+                    onSetupItem: "setupItem",
+                    components: [
+                        {
+                            layoutKind: "FittableColumnsLayout",
+                            fit: true,
+                            classes: "moon-search-recent-list",
+                            components: [
+                                {name: "name"},
+                                {kind: 'enyo.Image', classes: "moon-search-images", src: "assets/album.png"},
+                                {kind: 'enyo.Image', classes: "moon-search-images", src: "assets/album.png"},
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+
+    setupItem: function(inSender, inEvent) {
+        this.$.name.setContent("RECENT SEARCH");
+    }
+});
