ec5c951a03d4b843bc18ebaac15d37f3809020cf
SoonGil Choi <otisnate@empal.com>
2013-05-10 02:00:06 +0900

    Chanage Images for controller buttons

--- patterns-samples/SlideshowControl/LayeredContainer/PhotoSlideshow.js
+++ patterns-samples/SlideshowControl/LayeredContainer/PhotoSlideshow.js
@@ -3,7 +3,6 @@ enyo.kind({
 	kind: "enyo.Popup",
 	classes: "moon-photo-slideshow",
 	components: [
-		//{name: "photo", kind: "enyo.Image", ontap: "tapHandler", classes: "moon-photo-slideshow-image"},
 		{name: "photo", ontap: "tapHandler", classes: "moon-photo-slideshow-image"},
 		{name: "slideControl", kind: "moon.PhotoSlideshowControl", onClose: "closeHandler", onChangeSlide: "changeSlideHandler"}
 	],
@@ -35,7 +34,7 @@ enyo.kind({
 	},
 	requestShow: function(inUrl) {
 		// TODO: set inUrl as default image path when path is invalid
-		this.$.photo.setSrc(inUrl);
+		this.$.photo.applyStyle("background-image", "url('" + inUrl + "')");
 		
 		this.inherited(arguments);
 	},
@@ -44,11 +43,10 @@ enyo.kind({
 		return true;
 	},
 	changeSlideHandler: function(inSender, inEvent) {
-		this.$.photo.setSrc(this.results[inEvent.index].url);
+		this.$.photo.applyStyle("background-image", "url('" + this.results[inEvent.index].url + "')");
 		return true;
 	}
 });
-
 
 enyo.kind({
 	name: "moon.PhotoSlideshowControl",
@@ -71,14 +69,14 @@ enyo.kind({
 		{kind: "enyo.Spotlight"},
 		{layoutKind: 'HFlexLayout', components: [
 			{flex: true, classes:"moon-photo-slideshow-control-left-button", components: [
-				{kind: "moon.IconButton", src: "../assets/trash-can-icon.png", ontap: "closeHandler"},
-				{kind: "moon.IconButton", src: "../assets/trash-can-icon.png", ontap: "deleteActivated"},
-				{kind: "moon.IconButton", src: "../assets/trash-can-icon.png", ontap: "deleteActivated"}
+				{kind: "moon.IconButton", src: "../assets/fit-icon.png", ontap: "closeHandler"},
+				{kind: "moon.IconButton", src: "../assets/favorite_icon.png", ontap: "deleteActivated"},
+				{kind: "moon.IconButton", src: "../assets/share_icon.png", ontap: "deleteActivated"}
 			]},
 			{content:"Item 1-2", classes: "moon-photo-slideshow-control-middle-button", components: [
-				{kind: "moon.IconButton", src: "../assets/trash-can-icon.png", ontap: "prevHandler"},
-				{kind: "moon.IconButton", src: "../assets/trash-can-icon.png", ontap: "playHandler"},
-				{kind: "moon.IconButton", src: "../assets/trash-can-icon.png", ontap: "nextHandler"}
+				{kind: "moon.IconButton", src: "../assets/icon_previous.png", ontap: "prevHandler"},
+				{kind: "moon.IconButton", src: "../assets/icon_play.png", ontap: "playHandler"},
+				{kind: "moon.IconButton", src: "../assets/icon_next.png", ontap: "nextHandler"}
 			]},
 			{flex: true, components: [
 				{kind: "moon.ExpandablePicker", content: "SlideShow Speed", style: "width:240px;", components: [
@@ -87,8 +85,8 @@ enyo.kind({
 				]}
 			]}
 		]},
-		{name: "list", kind: "moon.List", orient:"h", count: 10, multiSelect: false, spotlight: true, 			onSetupItem: "setupItem", components: [
-				{name: "item", kind: "enyo.Image", src: "../assets/album.png", classes: "moon-photo-slideshow-control-item"}
+		{name: "list", kind: "moon.List", orient:"h", multiSelect: false, spotlight: true, 			onSetupItem: "setupItem", onSpotlightSelect: "itemSelectHandler",  components: [
+			{name: "item", kind: "enyo.Image", src: "../assets/album.png", classes: "moon-photo-slideshow-control-item", ontap: "itemSelectHandler"}
 		]}
 
 	],
@@ -97,27 +95,8 @@ enyo.kind({
 		var wh = enyo.dom.getWindowHeight();
 		var controlH = 280;
 		this.setBounds({top: wh - controlH, height: controlH}, "px");
-
-		// mockup data
-/*		this.results = [{width: "500", height: "300", url: "../assets/breaking_bad.png"},
-						{width: "400", height: "400", url: "../assets/south_park.png"},
-						{width: "500", height: "500", url: "../assets/paulie.png"},
-						{width: "300", height: "300", url: "../assets/breaking_bad.png"},
-						{width: "400", height: "200", url: "../assets/south_park.png"},
-						{width: "200", height: "200", url: "../assets/paulie.png"},
-						{width: "500", height: "300", url: "../assets/breaking_bad.png"},
-						{width: "250", height: "400", url: "../assets/south_park.png"},
-						{width: "500", height: "500", url: "../assets/paulie.png"},
-						{width: "300", height: "300", url: "../assets/breaking_bad.png"},
-						{width: "400", height: "200", url: "../assets/south_park.png"},
-						{width: "200", height: "200", url: "../assets/paulie.png"},
-						{width: "300", height: "250", url: "../assets/breaking_bad.png"},
-						{width: "250", height: "200", url: "../assets/south_park.png"},
-						{width: "200", height: "200", url: "../assets/paulie.png"}
-					];
-*/
+		
 		this.$.list.applyStyle("height", (this.listHeight + 16 * 2 + 10) + "px");
-		//this.$.list.setCount(this.results.length);
 	},
 	setupItem: function(inSender, inEvent) {
 		// this is the row we're setting up
@@ -140,7 +119,7 @@ enyo.kind({
 	prevHandler: function(inSender, inEvent) {
 		if (this.index > 0) {
 			this.index--;
-			this.doChangeSlide({index: this.index});
+			this.doChangeSlide({index: this.index, direction: "prev"});
 		}		
 		return true;
 	},
@@ -150,8 +129,18 @@ enyo.kind({
 	nextHandler: function(inSender, inEvent) {
 		if (this.index < (this.results.length-1)) {
 			this.index++;
-			this.doChangeSlide({index: this.index});
+			this.doChangeSlide({index: this.index, direction: "next"});
 		}	
+		return true;
+	},
+	itemSelectHandler: function(inSender, inEvent) {
+		if (typeof inSender._nCurrentSpotlightItem != 'undefined') {
+			this.index = inSender._nCurrentSpotlightItem;
+		}
+		else {
+			this.index = inEvent.index;
+		}
+		this.doChangeSlide({index: this.index, direction: "none"});
 		return true;
 	}
 });
