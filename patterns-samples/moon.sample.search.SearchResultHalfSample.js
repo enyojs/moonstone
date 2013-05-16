98539ca59545aa72a813a8dd2f972e0c6f924557
kunmyon.choi <dilly97@gmail.com>
2013-05-17 06:34:15 +0900

    GF-4283 In App Search: Search Result Page (half)
    Initial implementation is done with Spotlight support.
    
    Todo:
    - moon.Panel needs interface to resize header, body, tools area
    - moon.DataGrid needs to replace enyo.DataList
    
    Enyo-DCO-1.1-Signed-off-by: Kunmyon Choi (kunmyon.choi@lge.com)

---
+++ patterns-samples/moon.sample.search.SearchResultHalfSample.js
@@ -0,0 +1,136 @@
+// Sample view
+
+enyo.kind({
+    name: "moon.SearchResultImageItem",
+    classes: "moon-search-result-item",
+    spotlight: true,
+    published: {
+        option: {
+            src: "",
+            caption: ""    
+        }
+    },
+    handlers: {
+        onSpotlightFocused: "focused",
+        onSpotlightBlur: "released"
+    },
+    components: [
+        {name: "caption", classes: "moon-search-result-item-text"}
+    ],
+    create: function() {
+        this.inherited(arguments);
+        this.optionChanged();
+    },
+    getCaption: function() {
+        return this.$.caption;
+    },
+    optionChanged: function(inOld) {
+        this.applyStyle("background-image", 'url(' + this.option.src + ')');
+        this.$.caption.setContent(this.option.caption);
+    },
+    focused: function() {
+        this.$.caption.addClass("spotlight");
+        return true;
+    },
+    released: function() {
+        this.$.caption.removeClass("spotlight");
+        return true;
+    }
+});
+
+enyo.kind({
+    name: "moon.sample.search.RecentSearchHalfSample",
+    kind: "moon.Panel",
+    titleAbove: "02",
+    title: "SEARCH",
+    titleBelow: "",
+    headerComponents: [
+        /* Fixme: moon.Panel needs interface to change Header, Body, Tools area size */
+         {kind: "FittableColumns", components: [
+            {kind: "moon.InputDecorator", fit: true, components: [
+                {kind: "moon.Input", placeholder: "Search term", onchange: "inputChanged"},
+                {kind: "Image", src: "../samples/assets/search-input-search.png"}
+            ]},
+        ]}
+    ],
+    components: [
+        {
+            /* Fixme: enyo.DataList should be changed to moon.DataGrid to support scroll */
+            kind: "enyo.DataList",
+            name: "resultInfo",
+            components: [
+                {
+                    kind: "moon.SearchResultImageItem", 
+                    bindFrom: "option", 
+                    bindTo: "option",
+                    ontap: "changeName"
+                }
+            ]
+        }
+    ],
+    bindings: [
+    {from: ".controller.result", to: "$.resultInfo.controller"}
+    ]
+});
+
+// Sample model
+
+enyo.ready(function(){
+    var sampleModel = new enyo.Model({
+        result: new enyo.Collection([
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}},
+            {option: {src: "./assets/default-movie.png", caption: "Person name"}}
+        ])
+    });
+ 
+//  Application to render sample
+
+    new enyo.Application({
+        view: {
+            classes: "enyo-unselectable moon",
+            components: [
+                {kind: "enyo.Spotlight"},
+                {
+                    kind: "moon.sample.search.RecentSearchHalfSample",
+                    controller: ".app.controllers.searchController",
+                    classes: "enyo-fit"
+                }
+            ]
+        },
+        controllers: [
+            {
+                name: "searchController",
+                kind: "enyo.ModelController",
+                model: sampleModel,
+                changeName: function(inSender, inEvent) {
+                    inSender.getCaption().setContent("Selected");
+                }
+            }
+        ]
+    });
+});
