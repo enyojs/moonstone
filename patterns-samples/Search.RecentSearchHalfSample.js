00fdcf0d97240b6433f6f6fe7498292e4f408b41
David.Um <joonho.um.snu@gmail.com>
2013-05-22 10:44:24 +0900

    GF-4284: Implement Recent Search Half Sample
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

---
+++ patterns-samples/Search.RecentSearchHalfSample.js
@@ -0,0 +1,60 @@
+enyo.kind({
+    name: "moon.sample.search.RecentSearchHalfSample",
+    kind: "moon.Panel",
+	classes: "enyo-unselectable moon moon-search",
+    fit: true,
+    titleAbove: "02",
+    title: "SEARCH",
+
+    headerComponents: [
+         {kind: "FittableColumns", components: [
+            {kind: "moon.InputDecorator", fit: true, components: [
+                {kind: "moon.Input", placeholder: "Search term", onchange: "inputChanged"},
+                {kind: "Image", src: "../samples/assets/search-input-search.png"}
+            ]},
+        ]}
+    ],
+
+    components: [
+        {kind: "enyo.Spotlight"},
+        {
+            layoutKind: "FittableRowsLayout",
+            fit: true,
+            components: [
+                {
+                    kind: "moon.RadioButtonGroup", 
+                    onActivate: "", 
+                    components: [
+                        {classes : "radio-button", content: "SUGGESTED SEARCH"},
+                        {classes : "radio-button", content: "RECENT SEARCHES"},
+                        {kind: "moon.IconButton", src: "assets/icon-round-delete.png", classes: "icon-button-right"}
+                    ],
+                },
+                { kind : "moon.Divider", classes : "divider" },
+                {
+                    kind: "moon.List",
+                    count: 10,
+                    fit: true,
+                    onSetupItem: "setupItem",
+                    components: [
+                        {
+                            name: "item",
+                            layoutKind: "FittableColumnsLayout",
+                            fit: true,
+                            classes: "moon-search-recent-list",
+                            components: [
+                                {kind: "moon.Item"},
+                                {kind: 'enyo.Image', classes: "moon-search-images", src: "assets/album.png"},
+                                {kind: 'enyo.Image', classes: "moon-search-images", src: "assets/album.png"},
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+
+    setupItem: function(inSender, inEvent) {
+        this.$.item.controls[0].setContent("RECENT SEARCH");
+    }
+});
