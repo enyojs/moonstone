b99c7e2dcbe7b225a92c956bf16517d0ad701879
SoonGil Choi <otisnate@empal.com>
2013-05-24 06:30:42 +0900

    apply enyo-mvc to moon.sample.video.BrowserMoviesWideSample

--- patterns-samples/Video.BrowserMoviesWideSample.js
+++ patterns-samples/Video.BrowserMoviesWideSample.js
@@ -1,58 +1,113 @@
 enyo.kind({
+    name: "moon.MovieImageItem",
+    /* TODO: moon-1v should be inserted at classes after fixing moon-1v value 
+        from "width: 170px;" to "height: 170px;" */
+    classes: "moon-movie-list-item moon-3h",
+    style: "height: 170px;",
+    spotlight: true,
+    published: {
+        option: {
+            src: "",
+            caption: ""    
+        }
+    },
+    handlers: {
+        onSpotlightFocused: "focused",
+        onSpotlightBlur: "released"
+    },
+    components: [
+        {name: "caption", classes: "moon-movie-list-item-text"}
+    ],
+    create: function() {
+        this.inherited(arguments);
+        this.optionChanged();
+    },
+    getCaption: function() {
+        return this.$.caption;
+    },
+    optionChanged: function(inOld) {
+        this.applyStyle("background-image", 'url(' + this.option.src + ')');
+        this.$.caption.setContent(this.option.caption);
+    },
+    focused: function() {
+        this.$.caption.addClass("spotlight");
+        return true;
+    },
+    released: function() {
+        this.$.caption.removeClass("spotlight");
+        return true;
+    }
+});
+
+enyo.kind({
     name: "moon.sample.video.BrowserMoviesWideSample",
     kind: "moon.Panel",
-    classes: "enyo-unselectable moon moon-video-browsermovies",
-    fit: true,
     titleAbove: "02",
     title: "Browser Movies",
-    imgList: [
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},
-        {src: "assets/album.png", name: "MOVIE NAME"},                
-    ],    
+    headerComponents: [
+        {kind: "moon.IconButton", src: "assets/icon-list.png"}
+    ],
     components: [
         {kind: "enyo.Spotlight"},
         {
-            classes: "moon-video-browsermovies-container",
-            name: "gridlist",
-            kind: "moon.GridList",
-            fit: true,
-            count: 12,
-            onSetupItem: "setupGridItem",
-            touch: true,
-            itemWidth: 230,
-            itemHeight: 130,
-            itemSpacing: 30,
+            name: "movieList",
+            /* TODO: enyo.DataGridList should be changed to moon.DataGridList to support scroll */
+            kind: "enyo.DataGridList",
             components: [
-                {name: "gridItem", kind: "moon.GridList.ImageItem"},
-                {name: "gridCaption", classes: "moon-video-browsermovies-item-label"}
+                {
+                    kind: "moon.MovieImageItem", 
+                    bindFrom: "option",
+                    bindTo: "option",
+                    ontap: "changeName"
+                }
             ]
         }
-    ],
+    ],   
+    bindings: [
+        {from: ".controller.albums", to: "$.movieList.controller"},
+    ]
+});
 
-    headerComponents: [
-        {classes: "moon-video-browsermovie-header-button", components: [
-            {kind: "moon.IconButton", src: "assets/icon-list.png"}
-        ]}
-    ],    
+// Sample model
 
-    setupGridItem: function(inSender, inEvent) {
-        var i = inEvent.index;
-        var gridItem = this.imgList[i];
-        if (!gridItem.src) {
-            return;
-        }
-        this.$.gridItem.setSource(gridItem.src);
-        this.$.gridCaption.setContent(gridItem.name);
-        this.$.gridItem.setSelected(this.$.gridlist.isSelected(i));
-    },
+enyo.ready(function(){
+    var sampleModel = new enyo.Model({
+        albums: new enyo.Collection([
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}},
+            {option: {src: "assets/default-music.png", caption: "Movie Name"}}
+        ]),
+    });
+
+//  Application to render sample
+
+    new enyo.Application({
+        view: {
+            classes: "enyo-unselectable moon",
+            components: [
+                {
+                    kind: "moon.sample.video.BrowserMoviesWideSample",
+                    controller: ".app.controllers.movieController",
+                    classes: "enyo-fit"
+                }
+            ]
+        },
+        controllers: [
+            {
+                name: "movieController",
+                kind: "enyo.ModelController",
+                model: sampleModel,
+                changeName: function(inSender, inEvent) {
+                    inSender.getCaption().setContent("Result");
+                }
+            }
+        ]
+    });
 });
