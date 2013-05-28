672ce9eb09d54b29e73525b9df46ff06d4f64aa0
DAJUNG CHUNG <dajung.chung@lge.com>
2013-05-28 14:01:23 +0900

    GF-5126: Create In App Setting - Account Setting
    Enyo-DCO-1.1-Signed-off-by: Dajung Chung <dajung.chung@lge.com>

---
+++ patterns-samples/Setting.AccountSettingSample.js
@@ -0,0 +1,88 @@
+//Account Settings Menu View
+enyo.kind({
+	//* @public
+    name: "moon.sample.setting.AccountSettingSample",
+    kind: "moon.Panel",
+    //* @protected
+    title: "ACCOUNT SETTINGS",
+    titleBelow: "Description of Settings Category",
+    components: [
+        {
+            kind: "FittableColumns",
+            fit: true, 
+            components: [
+                {
+                    name: "eMail",
+                    classes: "moon-cetner-item"
+                },
+                {
+                    name: "signOutButton",
+                    kind: "moon.Button",
+                    bindFrom: "signOutButton",
+                    classes: "moon-light-gray"
+                }
+            ]
+        },
+    	{
+    		kind: "FittableColumns",
+            fit:true,
+    		components: [
+    			{name: "autoSign"},
+    			{
+    				name: "toggleButton",
+                    kind: "moon.ToggleButton"
+    			}
+    		]
+    	},
+    	{name: "help", classes: "moon-settings-content"},
+    	{
+    		name: "createAccountButton",
+    		kind: "moon.Button",
+    		bindFrom: "createAccountButton",
+    		classes: "moon-light-gray"
+    	}
+    ],
+    bindings: [
+        {from: ".controller.eMail", to: "$.eMail.content"},
+        {from: ".controller.signOutButton", to: "$.signOutButton.content"},
+        {from: ".controller.autoSign", to: "$.autoSign.content"},     
+        {from: ".controller.help", to: "$.help.content"},
+        {from: ".controller.createAccountButton", to: "$.createAccountButton.content"}
+    ]
+});
+
+//Account Settings Menu Model
+enyo.ready(function(){
+	var accountModel = new enyo.Model({
+		eMail: "hancuna@naver.com",
+		signOutButton: "Sign out",
+		autoSign: "Auto Sign in",		
+		help: "Create your account to: " +
+			  "Keep up with your favorite channels" +
+			  "Save videos  to watch later, watch recommendations just for you, or" +
+			  "subscribe to get updates from your favorite channels",
+		createAccountButton: "Create New Account"
+	});
+
+//  Application to render sample
+	new enyo.Application({
+        view: {
+            classes: "enyo-unselectable moon",
+            components: [
+                {kind: "enyo.Spotlight"},
+                {
+                	kind: "moon.sample.setting.AccountSettingSample",
+                    controller: ".app.controllers.settingAccountController",
+                    classes: "enyo-fit"
+                }
+            ]
+        },
+        controllers: [
+            {
+                name: "settingAccountController",
+                kind: "enyo.ModelController",
+                model: accountModel
+            }
+        ]
+    });
+});
