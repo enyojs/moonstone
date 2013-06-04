4c0aae1aa6112db7ded13ee57b57678e6f6c08d5
Kevin Schaaf <kevin.schaaf@lge.com>
2013-06-04 02:37:39 -0700

    Fix comment. Enyo-DCO-1.1-Signed-off-by: Kevin Schaaf <kevin.schaaf@lge.com>

--- source/Item.js
+++ source/Item.js
@@ -10,7 +10,7 @@ enyo.kind({
 		//* events
 		disabled: false,
 		//* The relative position of the spotlight;
-		//* valid values are "locale-specific", "left", "right", "top", and "bottom"
+		//* valid values are "locale-default", "left", "right", "top", and "bottom"
 		//* The locale-specific setting selects either left or right, depending on the
 		//* default text-direction of the current locale, when enyo-ilib is loaded
 		//* (defaults to left if enyo-ilib is not loaded)
