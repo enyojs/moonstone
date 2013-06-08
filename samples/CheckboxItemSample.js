f239791f62ce7ce9a3ba22d0efb74a660b64da3f
Kevin Schaaf <kevin.schaaf@lge.com>
2013-06-08 05:01:44 -0700

    Sample cleanup: Separated CheckboxItems from ToggleItems.  Renamed samples to follow widget names.  Documented onActivate event properties for CheckboxItem.  Removed dark items.  Fixed up layout.
    Enyo-DCO-1.1-Signed-off-by: Kevin Schaaf kevin.schaaf@lge.com

---
+++ samples/CheckboxItemSample.js
@@ -0,0 +1,43 @@
+enyo.kind({
+	name: "moon.sample.CheckboxItemSample",
+	kind:"FittableRows",
+	classes: "moon enyo-unselectable enyo-fit",
+	components: [
+		{kind: "enyo.Spotlight"},
+		{name: 'scroller', kind: 'moon.Scroller', fit: true, touch: true, components: [
+			{controlClasses:"moon-checkbox-sample-wrapper moon-4h", components: [
+				{components: [
+					{kind: "moon.Divider", content: "Checkbox Items"},
+					{kind: "moon.CheckboxItem", content: "Option 1", checked: true, onchange: "itemChanged"},
+					{kind: "moon.CheckboxItem", content: "Option 2", onchange: "itemChanged"},
+					{kind: "moon.CheckboxItem", disabled: true, content: "Disabled", onchange: "itemChanged"},
+					{kind: "moon.CheckboxItem", content: "Option 4", checked: true, onchange: "itemChanged"},
+					{kind: "moon.CheckboxItem", content: "This is a verrry long option 5", onchange: "itemChanged"}
+				]},
+				{components: [
+					{kind: "moon.Divider", content: "Checkbox Item Group"},
+					{kind: "Group", onActivate: "groupChanged", components: [
+						{kind: "moon.CheckboxItem", content: "Group Option 1"},
+						{kind: "moon.CheckboxItem", content: "Group Option 2", checked: true},
+						{kind: "moon.CheckboxItem", disabled: true, content: "Disabled"},
+						{kind: "moon.CheckboxItem", content: "Group Option 4"},
+						{kind: "moon.CheckboxItem", content: "Group Option 5"}
+					]}
+				]}
+			]}
+		]},
+		{components: [
+			{kind:"moon.Divider", content:"Result"},
+			{name:"result", content:"Nothing selected"}
+		]}
+	],
+	itemChanged: function(inSender, inEvent) {
+		this.$.result.setContent(inSender.getContent() + " was " + (inSender.getChecked() ? " selected." : "deselected."));
+	},
+	groupChanged: function(inSender, inEvent) {
+		if (inEvent.toggledControl.getChecked()) {
+			var selected = inEvent.toggledControl.getContent();
+			this.$.result.setContent(selected + " was selected.");
+		}
+	}
+});
