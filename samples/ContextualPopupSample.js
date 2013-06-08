155ac7ac269a746f3c5c00c559ebff0d1094b15d
KunmyonChoi <dilly97@gmail.com>
2013-06-08 15:49:37 -0700

    Modify ContextualPopupSample and fixing moon-grid-vunits
    Enyo-DCO-1.1-Signed-off-by: Kunmyon Choi kunmyon.choi@lge.com

--- samples/ContextualPopupSample.js
+++ samples/ContextualPopupSample.js
@@ -1,116 +1,98 @@
 enyo.kind({
 	name: "moon.sample.ContextualPopupSample",
-	kind: "FittableRows",
-	classes: "enyo enyo-fit",
+	classes: "moon enyo-fit",
 	components: [
 		{kind: "enyo.Spotlight"},
-		{kind: "Scroller", fit: true, thumb:false, components:[
-			{name:"buttonContainer", kind:"FittableRows", classes:"enyo-fit", components:[
-				//Top row of buttons
-				{components:[
-					{kind: "moon.ContextualPopupDecorator", style:"display:inline-block", components: [
-						{content: "Average"},
-						{kind: "moon.ContextualPopup", style:"width:200px;height:200px;",
-							components: [
-								{content:"Item 1"},
-								{content:"Item 2"},
-								{content:"Item 3"},
-								{content:"Item 4"},
-								{content:"Item 5"}
-							]
-						}
-					]},
-
-					{kind: "moon.ContextualPopupDecorator", style:"display:inline-block;float:right", components: [
-						{content:"Small"},
-						{kind: "moon.ContextualPopup"}
+		{kind: "moon.ContextualPopupDecorator", style:"position: absolute; left: 0px; top: 0px;", components: [
+			{content: "Average"},
+			{kind: "moon.ContextualPopup", classes: "moon-2h moon-1v",
+				components: [
+					{content:"Item 1"},
+					{content:"Item 2"},
+					{content:"Item 3"},
+					{content:"Item 4"},
+					{content:"Item 5"}
+				]
+			}
+		]},
+		{kind: "moon.ContextualPopupDecorator", style:"position: absolute; right: 0px; top: 0px;", components: [
+			{content:"Small"},
+			{kind: "moon.ContextualPopup"}
+		]},
+		{kind: "moon.ContextualPopupDecorator", style:"position: absolute; left: 0px; top: 45%;", components: [
+			{content: "Wide"},
+			{kind: "moon.ContextualPopup", classes: "moon-6h moon-1v",
+				components: [
+					{kind: "Scroller", horizontal:"auto",  touch:true, thumb:false, classes: "enyo-fill", components:[
+						{content:"testing 1"},
+						{content:"testing 2"}
 					]}
-				]},
-				//Center row of buttons
-				{fit:true, style:"padding-top:15%;padding-bottom:15%;", components:[
-					{kind: "moon.ContextualPopupDecorator", style:"display:inline-block;", components: [
-						{content: "Wide"},
-						{kind: "moon.ContextualPopup",
-							style:"width:500px",
-							components: [
-								{kind: "Scroller", style:"min-width:150px;", horizontal:"auto",  touch:true, thumb:false,  components:[
-									{content:"testing 1"},
-									{content:"testing 2"}
-								]}
-							]
-						}
-					]},
-					{kind: "moon.ContextualPopupDecorator", style:"display:inline-block;float:right", components: [
-						{content:"Long"},
-						{kind: "moon.ContextualPopup",
-							components: [
-								{content:"testing 1"},
-								{content:"testing 2"},
-								{content:"testing 3"},
-								{content:"testing 4"},
-								{content:"testing 5"},
-								{content:"testing 6"},
-								{content:"testing 7"},
-								{content:"testing 9"},
-								{content:"testing 10"},
-								{content:"testing 11"},
-								{content:"testing 12"},
-								{content:"testing 13"},
-								{content:"testing 14"},
-								{content:"testing 15"},
-								{content:"testing 16"},
-								{content:"testing 17"},
-								{content:"testing 18"},
-								{content:"testing 19"},
-								{content:"testing 20"},
-								{content:"testing 21"},
-								{content:"testing 22"},
-								{content:"testing 23"},
-								{content:"testing 24"},
-								{content:"testing 25"},
-								{content:"testing 26"},
-								{content:"testing 27"},
-								{content:"testing 28"},
-								{content:"testing 29"},
-								{content:"testing 30"}
-							]
-						}
-					]}
-				]},
-				//Bottom row of buttons
-				{components:[
-					{kind: "moon.ContextualPopupDecorator", style:"display:inline-block;", components: [
-						{content: "Press Me"},
-						{kind: "moon.ContextualPopup",
-							style:"width:200px",
-							components: [
-								{content:"testing 1"},
-								{content:"testing 2"},
-								{content:"testing 3"},
-								{content:"testing 4"},
-								{content:"testing 5"},
-								{content:"testing 6"},
-								{content:"testing 7"},
-								{content:"testing 9"},
-								{content:"testing 10"}
-							]
-						}
-					]},
-					{kind: "moon.ContextualPopupDecorator", style:"display:inline-block;float:right", components: [
-						{content:"Try Me"},
-						{kind: "moon.ContextualPopup",
-							style:"width:250px",
-							components: [
-								{content:"Item 1"},
-								{content:"Item 2"},
-								{content:"Item 3"},
-								{content:"Item 4"},
-								{content:"Item 5"}
-							]
-						}
-					]}
-				]}
-			]}
+				]
+			}
+		]},
+		{kind: "moon.ContextualPopupDecorator", style:"position: absolute; right: 0px; top: 45%;", components: [
+			{content:"Long"},
+			{kind: "moon.ContextualPopup",
+				components: [
+					{content:"testing 1"},
+					{content:"testing 2"},
+					{content:"testing 3"},
+					{content:"testing 4"},
+					{content:"testing 5"},
+					{content:"testing 6"},
+					{content:"testing 7"},
+					{content:"testing 9"},
+					{content:"testing 10"},
+					{content:"testing 11"},
+					{content:"testing 12"},
+					{content:"testing 13"},
+					{content:"testing 14"},
+					{content:"testing 15"},
+					{content:"testing 16"},
+					{content:"testing 17"},
+					{content:"testing 18"},
+					{content:"testing 19"},
+					{content:"testing 20"},
+					{content:"testing 21"},
+					{content:"testing 22"},
+					{content:"testing 23"},
+					{content:"testing 24"},
+					{content:"testing 25"},
+					{content:"testing 26"},
+					{content:"testing 27"},
+					{content:"testing 28"},
+					{content:"testing 29"},
+					{content:"testing 30"}
+				]
+			}
+		]},
+		{kind: "moon.ContextualPopupDecorator", style:"position: absolute; left: 0px; bottom: 0px;", components: [
+			{content: "Press Me"},
+			{kind: "moon.ContextualPopup", classes: "moon-2h",
+				components: [
+					{content:"testing 1"},
+					{content:"testing 2"},
+					{content:"testing 3"},
+					{content:"testing 4"},
+					{content:"testing 5"},
+					{content:"testing 6"},
+					{content:"testing 7"},
+					{content:"testing 9"},
+					{content:"testing 10"}
+				]
+			}
+		]},
+		{kind: "moon.ContextualPopupDecorator", style:"position: absolute; right: 0px; bottom: 0px;", components: [
+			{content:"Try Me"},
+			{kind: "moon.ContextualPopup", classes: "moon-3h",
+				components: [
+					{content:"Item 1"},
+					{content:"Item 2"},
+					{content:"Item 3"},
+					{content:"Item 4"},
+					{content:"Item 5"}
+				]
+			}
 		]}
 	]
 });
