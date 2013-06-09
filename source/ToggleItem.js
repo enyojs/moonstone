db35eb07dda6a1c3b152e48c09a8e50297013312
Kevin Schaaf <kevin.schaaf@lge.com>
2013-06-09 11:41:14 -0700

    Revert "Merge pull request #178 from enyojs/GF-5022-jungchae"
    This reverts commit b6a900ebf0fd9439614a402f27b7582aaddc4ad5, reversing
    changes made to ec0630602f7663794b329735feffcf07bc665ff5.

--- source/ToggleItem.js
+++ source/ToggleItem.js
@@ -1,15 +1,15 @@
 /**
 	_moon.ToggleItem_ is a control that combines a
-	<a href="#moon.ToggleText">moon.ToggleText</a> with a text label.
+	<a href="#moon.ToggleButton">moon.ToggleButton</a> with a text label.
 */
 enyo.kind({
 	name: "moon.ToggleItem",
 	kind: "moon.CheckboxItem",
 	//* @protected
-	classes: "moon-toggle-item",
+	classes: "moon-labeled-toggle-button",
 	components: [
-		{classes: "moon-toggle-item-label-wrapper", components: [
-			{name: "label", classes: "moon-toggle-item-label"}
+		{classes: "moon-labeled-toggle-button-label-wrapper", components: [
+			{name: "label", classes: "moon-labeled-toggle-button-label"}
 		]},
 		{name: "input", kind: "moon.ToggleText", spotlight: false}
 	]
--- source/ToggleText.js
+++ source/ToggleText.js
@@ -1,7 +1,7 @@
 /**
-	_moon.ToggleText_, which extends <a href="#moon.Checkbox">moon.Checkbox</a>,
+	_moon.ToggleButton_, which extends <a href="#moon.Checkbox">moon.Checkbox</a>,
 	is a control that looks like a switch with labels for two states, an "on"
-	state and an "off" state.  When the ToggleText is tapped, it switches its
+	state and an "off" state.  When the ToggleButton is tapped, it switches its
 	state and fires an _onChange_ event.
 */
 
@@ -9,15 +9,15 @@ enyo.kind({
 	name: "moon.ToggleText",
 	kind: "moon.Checkbox",
 	published: {
-		//* Label for toggle text's "on" state
+		//* Label for toggle button's "on" state
 		onContent: $L("on"),
-		//* Label for toggle text's "off" state
+		//* Label for toggle button's "off" state
 		offContent: $L("off")
 	},
 	//* @protected
-	classes: "moon-toggle-text",
+	classes: "moon-toggle-button",
 	components: [
-		{name: "label", classes: "moon-toggle-text-text"}
+		{name: "label", classes: "moon-toggle-button-text"}
 	],
 	create: function() {
 		this.inherited(arguments);
