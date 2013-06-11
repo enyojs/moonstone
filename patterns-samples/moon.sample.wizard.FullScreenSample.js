548c641adb68bbaa7b4f3885cbe9d4f984f55956
Jungchae Kim <jungchae.kim@lge.com>
2013-06-11 16:18:58 -0700

    GF-5504 : Wizard Action Menu revision #1
    Code update based on revision from GF-5503 : Wizard Full Screen
    (Reviewer : Chris Patalano)
    
    Enyo-DCO-1.1-Singed-off-by: Jungchae Kim jungchae.kim@lge.com

--- patterns-samples/moon.sample.wizard.FullScreenSample.js
+++ patterns-samples/moon.sample.wizard.FullScreenSample.js
@@ -1,17 +1,69 @@
 enyo.kind({
-    name: "moon.sample.wizard.FullScreenSample",
-    kind: "moon.Panels",
-    arrangerKind: "CarouselArranger",
-    classes: "moon enyo-unselectable enyo-fit",
+	name: "Sample.Wizard.Panels",
+	kind: "moon.Panels",
+	defaultKind: "Sample.Wizard.Panel",
+	arrangerKind: "CarouselArranger",
+	classes: "moon enyo-unselectable enyo-fit",
     handlers: {
-        onWizardStepChanged: "wizardStepChanged",
+		onNext: "next",
+		onPrevious: "previous",
+		onCancel: "goCancel",
     },
+	goCancel: function() {
+		this.log("Cancel");
+		return true;
+	}
+})
+
+enyo.kind({
+	name: "Sample.Wizard.Panel",
+	kind: "moon.Panel",
+	classes: "moon-wizard-sample enyo-fit",
+    published: {
+        selectedText: "",
+        processed: false,
+    },
+    events: {
+		onNext: "",
+		onPrevious: "",
+		onCancel: "",
+    },
+    headerComponents: [
+        {name: "wizListAction", ontap:"doListAct", kind: "moon.ListActions", classes: "wizard-listaction", iconSrc:"./assets/icon-list.png",
+            listActions: [
+            {                    
+                components: [
+                    {kind: "moon.Divider", content:"Category"},
+                    {name: "wizList", kind: "moon.DataList", components: [
+                        {kind:"moon.Item", classes: "wizard-listaction-item", bindFrom: "step", bindTo: "content"},
+                        {classes: "wizard-listaction-text", bindFrom: "processed", bindTo: "content"}
+                    ]}
+                ]
+            }]
+        }    
+    ],
+    bindings: [
+        {from: ".controller.title", to: ".title"},
+        {from: ".controller.wizResults", to: ".$.header.$.wizListAction.$.listActionsContainer.$.wizList.controller"},
+    ],
+    rendered: function() {
+        this.inherited(arguments);
+        this.initialSetting();
+    },
+    initialSetting: function() {
+    	// Stub
+    }
+});
+
+enyo.kind({
+    name: "moon.sample.wizard.FullScreenSample",
+    kind: "Sample.Wizard.Panels",
     components: [
-        {name: "introPage", kind: "moon.sample.wizard.FullScreenIntroSample"},
-        {name: "stepPage1", kind: "moon.sample.wizard.FullScreenStepSample"},
-        {name: "stepPage2", kind: "moon.sample.wizard.FullScreenStepSample"},
-        {name: "stepPage3", kind: "moon.sample.wizard.FullScreenStepSample"},
-        {name: "stepPage4", kind: "moon.sample.wizard.FullScreenStepSample"},
+        {name: "introPage",   kind: "moon.sample.wizard.FullScreenIntroSample"},
+        {name: "stepPage1",   kind: "moon.sample.wizard.FullScreenStepSample"},
+        {name: "stepPage2",   kind: "moon.sample.wizard.FullScreenStepSample"},
+        {name: "stepPage3",   kind: "moon.sample.wizard.FullScreenStepSample"},
+        {name: "stepPage4",   kind: "moon.sample.wizard.FullScreenStepSample"},
         {name: "confirmPage", kind: "moon.sample.wizard.FullScreenConfirmationSample"}  
     ],
     bindings: [
@@ -22,53 +74,48 @@ enyo.kind({
         {from: ".controller", to: ".$.stepPage4.controller"},
         {from: ".controller", to: ".$.confirmPage.controller"}
     ],
-    wizardStepChanged: function(iSender, iEvent) {
-        var cmd = iEvent.cmd;
-        switch(cmd) {
-            case "previous":
-                this.previous();
-                break; 
-            case "next":
-                this.next();
-                break;
-            case "done":
-                // TODO : done routine
-                break;
-            case "cancle":
-                // TODO : cancle routine
-                break;
-            case "STEP TITLE 01":
-            case "STEP TITLE 02":
-            case "STEP TITLE 03":
-            case "STEP TITLE 04":
-            case "STEP TITLE 05":
-            case "STEP TITLE 06":
+    onTap: function(oSender, oEvent) {
+        //* override from panels
+        // no action for Carosel Arranger using button
+        var target = oEvent.dispatchTarget.kind;
+        switch(target)
+        {
+            case "moon.Item":
                 var collection = this.controller.get("wizResults");
-                for (var index in collection) {
-                    if(cmd == collection.at(index).get("step"))
+                var cmd = oEvent.dispatchTarget.content;
+                for (var idx in collection) {
+                    if(cmd == collection.at(idx).get("step"))
                     {
-                        this.setIndex(index);
-                        break;
+                        this.setIndex(idx);
+                        this.closeListAction(oSender.name);
+                        return true;
                     }
                 }
                 break;
-            default:
+            case "moon.Button":
+                this.closeListAction(oSender.name);
+                break;
+
         }
         return true;
     },
-    onTap: function(oSender, oEvent) {
-        //* override from panels
-        // no action for Carosel Arranger using button        
-        return true;
+    closeListAction: function(sender) {
+        var p = this.$[sender].$.header.$.wizListAction;
+        if(p.getOpen())
+        {
+            p.setOpen(false);
+            enyo.Spotlight.spot(p.$.activator);
+        }    
     }
 });
+
 enyo.ready(function(){
     var wizardModel = new enyo.Model({
             title: "Wizard Title",
             wizContainer: new enyo.Collection([
                 {
                     id: "01",
-                    subtitle: "STEP TITLE 01",
+                    subtitle: "Step Title 1",
                     imgsrc: "assets/album.PNG",
                     instruction: "INSTRUCTIONAL TEXT GOES HERE",
                     detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet ipsum tortor,a       \
@@ -78,35 +125,35 @@ enyo.ready(function(){
                 },
                 {   
                     id: "02",
-                    subtitle: "STEP TITLE 02",
+                    subtitle: "Step Title 2",
                     imgsrc: "",
                     instruction: "INSTRUCTIONAL TEXT GOES HERE 2",
                     detail: "INPUT FIELD DESCRIPTION GOES HERE 2"
                 },
                 {   
                     id: "03",
-                    subtitle: "STEP TITLE 03",
+                    subtitle: "Step Title 3",
                     imgsrc: "",
                     instruction: "INSTRUCTIONAL TEXT GOES HERE 3",
                     detail: "INPUT FIELD DESCRIPTION GOES HERE 3"
                 },
                 {   
                     id: "04",
-                    subtitle: "STEP TITLE 04",
+                    subtitle: "Step Title 4",
                     imgsrc: "",
                     instruction: "INSTRUCTIONAL TEXT GOES HERE 4",
                     detail: "INPUT FIELD DESCRIPTION GOES HERE 4"
                 },
                 {   
                     id: "05",
-                    subtitle: "STEP TITLE 05",
+                    subtitle: "Step Title 5",
                     imgsrc: "",
                     instruction: "INSTRUCTIONAL TEXT GOES HERE 5",
                     detail: "INPUT FIELD DESCRIPTION GOES HERE 5"
                 },
                 {   
                     id: "06",
-                    subtitle: "STEP TITLE 06",
+                    subtitle: "Step Title 6",
                     imgsrc: "",
                     instruction: "Confirm your selections before complete",
                     detail: ""
@@ -122,7 +169,8 @@ enyo.ready(function(){
             ])
         }
     );
-    app = new enyo.Application({
+	
+    var app = new enyo.Application({
         view: {
             classes: "enyo-unselectable moon",
             components: [
