441414ed66281b8c170d2a0b47a67d87916124d4
Jim Tang <jim.tang@lge.com>
2013-06-14 11:45:23 -0700

    API comment cleanup

--- source/BoxIconButton.js
+++ source/BoxIconButton.js
@@ -1,8 +1,9 @@
 /**
-	_moon.BoxIconButton_ is a icon control that has 80x80 size.
+	_moon.BoxIconButton_, which extends <a href="#moon.IconButton>moon.IconButton</a>,
+	is an icon control 80x80 pixels in size. It is typically used inside the
+	_components_ block of a _moon.VideoPlayer_.
 
 		{kind: "moon.BoxIconButton", src: "assets/icon.png"}
-
 */
 enyo.kind({
 	name: "moon.BoxIconButton",
@@ -13,4 +14,3 @@ enyo.kind({
 		this.removeClass("moon-icon-button");
 	}
 });
-
