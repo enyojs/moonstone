64ac167eea9bcb7c13a2935e5616ffad55230f4b
Jungchae Kim <jungchae.kim@lge.com>
2013-06-18 10:22:41 -0700

    GF-6674 control video feedback
    Video feedbak information is displayed by this component by interacting
    with VideoPlayer.
    
    Enyo-DCO-1.1-Singed-off-by: Jungchae Kim jungchae.kim@lge.com

--- source/VideoFeedback.js
+++ source/VideoFeedback.js
@@ -1,11 +1,7 @@
 enyo.kind({
 	name: "moon.VideoFeedback",
 	kind: "enyo.Control",
-	classes: "moon-video-player-feedback", 
-	handlers: {
-		onFeedback: "feedback",
-		ontimeupdate: "timeInfo"
-	},
+	classes: "moon-video-player-feedback",
 	published: {
 		autoTimeout: 3,
 		showFeedback: false
@@ -13,19 +9,18 @@ enyo.kind({
 	//* @protected
 	_autoTimer: null,
 	components: [
-		{name: "feedbackDecorator", style: "margin: 5px; padding-top: 50px;", components: [
-			{name: "feedback", classes: "moon-header-font", 
-			style: "height: 65px; margin-top: 12px; text-align: center; font-size: 50px;"},
-			{name: "feedIcon", spotlight: false, kind: "moon.BoxIconButton"}
+		{classes: "moon-video-feedback-wrapper", components: [
+			{name: "feedtext", classes: "moon-video-feedback-text"},
+			{name: "feedIcon", kind: "moon.BoxIconButton", classes: "moon-video-feedback-icon", spotlight: false}
 		]}
 	],
-	//* @protected
+	//* @public
 	feedback: function(cmd, param, src) {
-		var msg = cmd;
-		if(msg=="time") {
+		if(cmd=="time") {
 			this.cmdTimeInfo(param);
 			return true;
 		}
+		var msg = cmd;
 		switch(msg)
 		{
 			case "play":
@@ -50,22 +45,20 @@ enyo.kind({
 				break;
 			case "jumpBack":
 				msg = 30 + " sec";
-				this.configuration(src, 0, 0, "right");
+				this.configuration(src, 0, 10, "right");
 				break;
 			case "jumpForward":
 				msg = 30 + " sec";
 				this.configuration(src, 0, 0, "left");
 				break;
 			case "rewindpaused":
-				msg = "";
-				// TODO
+				msg = "N/A";
 				break;
 			case "forwardpaused":
-				msg = "";
-				// TODO
+				msg = "N/A";
 				break;
 		}
-		this.$.feedback.setContent(msg);
+		this.$.feedtext.setContent(msg);
 		if(!this.$.feedIcon.getShowing()) {
 			this.$.feedIcon.setShowing(true);
 		}
@@ -73,25 +66,29 @@ enyo.kind({
 
 		return true;
 	},
+	//* @protected
 	configuration: function(src, left, right, option) {
 		this.$.feedIcon.setSrc(src);
-		this.$.feedback.applyStyle("padding-left", left + "px");
-		this.$.feedback.applyStyle("padding-right", right + "px");
-		this.$.feedback.applyStyle("float", option);
 		this.$.feedIcon.applyStyle("float", option);
+		this.$.feedtext.applyStyle("float", option);
+		this.$.feedtext.applyStyle("padding-left", left + "px");
+		this.$.feedtext.applyStyle("padding-right", right + "px");
 	},
+
 	cmdTimeInfo: function(param) {
 		if(!this.getShowFeedback()) {
 			this.configuration(null, 50, 0, null);
-			this.$.feedback.setContent(param);
+			this.$.feedtext.setContent(param);
 			if(this.$.feedIcon.getShowing())
 				this.$.feedIcon.setShowing(false);
 		}	
 		return true;	
 	},
+
 	_timeoutHandler: function() {
 		this.setShowFeedback(false);
 	},
+
 	_setAutoTimer: function() {
 		this._resetAutoTimer();
 		this._autoTimer = setTimeout(enyo.bind(this, 
@@ -101,6 +98,7 @@ enyo.kind({
 		this.setShowFeedback(true);
 		this.log();
 	},
+	
 	_resetAutoTimer: function() {
 		if (this._autoTimer != null) {
 			clearTimeout(this._autoTimer);
