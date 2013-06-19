f9d46bd343a1a774c539d5fb1f4ee11423ceb650
Yunbum Sung <yb.sung@lge.com>
2013-06-19 15:46:13 +0900

    GF-7159: Impliment new "SearchHeader"
    Enyo-DCO-1.1-Signed-Off-By: Yunbum Sung <yb.sung@lge.com>

--- source/SearchHeader.js
+++ source/SearchHeader.js
@@ -1,35 +1,43 @@
 /**
-	_moon.SearchHeader_ extends <a href="#moon.Header">moon.Header</a>, combining
-	a search box with a narrow title.
+	_moon.SearchHeader_ extends <a href="#moon.Header">moon.Header</a>
+	using moon.Input instead of enyo.Control for a title.
 */
 enyo.kind({
-    //* @public
-    name: "moon.SearchHeader",
-    kind: "moon.Header",
-    events: {
-        "onSearch": ""
-    },
-    //* @protected
-    classes: "moon-searchheader",
-    components: [
-        {name: "titleAbove", classes: "moon-searchheader-title-above"},
-        {name: "title", classes: "moon-searchheader-title"},
-        {name: "titleBelow", kind: "moon.Item", spotlight: false, classes: "moon-searchheader-title-below"},
-        {name: "searchInputContainer", kind: "FittableColumns", classes: "moon-searchheader-container", style: "width: 100%;", components: [
-            {classes: "moon-searchheader-search-left"},
-            {kind: "moon.InputDecorator", layoutKind: "FittableColumnsLayout", fit: true, components: [
-                {name: "searchInput", kind: "moon.Input", oninput: "search", fit: true, placeholder: "Search term"},
-                {kind: "Image", src: "../samples/assets/search-input-search.png", style: "width: 20px;height:20px;margin-left:10px;"}
-            ]},
-            {classes: "moon-searchheader-search-right"},
-        ]},
-        {name: "client", classes: "moon-header-client"},
-        {name: "animator", kind: "StyleAnimator", onComplete: "animationComplete"}
-    ],
+	//* @public
+	name: "moon.SearchHeader",
+	kind: "moon.Header",
+	events: {
+		/** 
+			Fires when typing a text in a moon.Input for a title
 
-    //* @public
+			_inEvent.keyword_ contains the title of moon.SearchHeader
+			which is the text in a moon.Input
+		*/
+		"onSearch": ""
+	},
+	//* @protected
+	classes: "moon-header moon-search-header",
+	components: [
+		{name: "titleAbove", classes: "moon-header-title-above"},
+		{kind: "moon.InputDecorator", classes: 'moon-search-input-decorator', components: [
+			{name: "title", kind: "moon.Input", oninput: "search", placeholder: "Search", classes: "moon-header-title"}
+		]},
+		{name: "titleBelow", kind: "moon.Item", spotlight: false, classes: "moon-header-title-below"},
+		{name: "client", classes: "moon-header-client"},
+		{name: "animator", kind: "StyleAnimator", onComplete: "animationComplete"}
+	],
 
-    search: function() {
-        this.doSearch({"keyword": this.$.searchInput.getValue()});
-    }
+	//* @public
+
+	//* If _this.tiitle_ changed, the placeHolder value of a moon.Input will be updated
+	titleChanged: function() {
+		var placeHolder = this.title || this.content;
+		if(placeHolder != "") {
+			this.$.title.setPlaceholder(placeHolder);
+		}
+	},
+	//* If user type a text in moon.Input, "onSearch" evnet  will be fired.
+	search: function() {
+		this.doSearch({"keyword": this.$.title.getValue()});
+	}
 });
--- source/SearchPanel.js
+++ source/SearchPanel.js
@@ -1,26 +1,17 @@
 /**
 	_moon.SearchPanel_ extends <a href="#moon.Panel">moon.Panel</a>, providing a
 	convenient, ready-made panel for use in search scenarios. Its features include
-	a search header and a	FittableRows layout for the main body content.
+	a search header and a FittableRows layout for the main body content.
 */
 
 enyo.kind({
 	//* @public
 	name: "moon.SearchPanel",
 	kind: "moon.Panel",
-	handlers: {
-        "onSearch": "search"
-    },
-    //* @protected
+	//* @protected
 	panelTools : [
 		{name: "header", kind: "moon.SearchHeader"},
 		{name: "panelBody", kind: "FittableRows", fit: true, classes: "moon-panel-body"},
 		{name: "animator", kind: "StyleAnimator", onComplete: "animationComplete"}
-	],
-
-	//* @public
-
-	search: function(inSender, inEvent) {
-		// console.log("[moon.SearchPanel] onSearch Event - search keyword : " + inEvent.keyword);
-	}
+	]
 });
