01f01999f6f06416e7db975fe8ea3bd2aa20760e
Yunbum Sung <yb.sung@lge.com>
2013-06-19 17:19:43 +0900

    GF-7159: Spotlight bug fixed
    The Spotlight behavior of the moon.InputDecorator modified in Search
    Header didn't work well
    
    Enyo-DCO-1.1-Signed-Off-By: Yunbum Sung <yb.sung@lge.com>

--- samples/SearchHeaderSample.js
+++ samples/SearchHeaderSample.js
@@ -2,6 +2,7 @@ enyo.kind({
 	name: "moon.sample.SearchHeaderSample",
 	classes: "moon enyo-unselectable enyo-fit moon-hspacing",
 	components: [
+		{kind: "enyo.Spotlight"},
 		{kind: "moon.SearchHeader", classes:"moon-10h"},
 		{kind: "moon.SearchHeader", titleAbove: "02", titleBelow: "Sub Header", classes:"moon-10h"},
 		{kind: "moon.SearchHeader", content: "Captioned Header", titleAbove: "02", titleBelow: "Sub Header", classes:"moon-10h"}
