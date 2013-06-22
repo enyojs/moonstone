ac1f10563501b3e453e727ea8bc00255ebcb23b5
KunmyonChoi <dilly97@gmail.com>
2013-06-22 09:45:26 -0700

    Add transitionFinished event for panels transition
    Enyo-DCO-1.1-Signed-off-by: Kunmyon Choi kunmyon.choi@lge.com

--- source/Panels.js
+++ source/Panels.js
@@ -22,6 +22,11 @@ enyo.kind({
 		*/
 		pattern: "activity"		
 	},
+	events: {
+		onShowFinished: "",
+		onHideFinished: "",
+		onHideLeftFinished: ""
+	},
 	handlers: {
 		onSpotlightFocused			: 'onSpotlightFocused',
 		onSpotlightContainerEnter	: 'onSpotlightPanelEnter',
@@ -29,7 +34,8 @@ enyo.kind({
 		ontap						: 'onTap',
 		onTransitionFinish			: 'transitionFinish',
 		onPreTransitionComplete		: 'panelPreTransitionComplete',
-		onPostTransitionComplete	: 'panelPostTransitionComplete'
+		onPostTransitionComplete	: 'panelPostTransitionComplete',
+		ontransitionend				: 'onTransitionFinish'
 	},
 	defaultKind: "moon.Panel",
 	draggable: false,
@@ -58,63 +64,6 @@ enyo.kind({
 		enyo.Spotlight.Util.dispatchEvent(s5WayEventType, null, this);
 	},
 
-	//* @public
-	/**
-		Returns an array of contained panels.
-		Subclasses can override this if they don't want the arranger to layout all of their children
-	*/
-	getPanels: function() {
-		return this.getClientControls();
-	},
-	//* Creates a panel on top of the stack and increments index to select that
-	//* component.
-	pushPanel: function(inInfo, inMoreInfo) { // added
-		var lastIndex = this.getPanels().length - 1,
-			oPanel = this.createComponent(inInfo, inMoreInfo);
-
-		oPanel.render();
-		this.resized();
-		this.setIndex(lastIndex+1);
-		return oPanel;
-	},
-	//* Creates multiple panels on top of the stack and updates index to select
-	//* the last one created.
-	pushPanels: function(inInfos, inCommonInfo) { // added
-		var lastIndex = this.getPanels().length - 1,
-			oPanels = this.createComponents(inInfos, inCommonInfo),
-			nPanel;
-
-		for (nPanel in oPanels) {
-			oPanels[nPanel].render();
-		}
-
-		this.resized();
-		this.setIndex(lastIndex+1);
-		return oPanels;
-	},
-	//* Destroys panels whose index is greater than or equal to _inIndex_.
-	popPanels: function(inIndex) {
-		var panels = this.getPanels();
-		inIndex = inIndex || panels.length - 1;
-
-		while (panels.length > inIndex && inIndex >= 0) {
-			panels[panels.length - 1].destroy();
-		}
-	},
-	replacePanel: function(index, inInfo, inMoreInfo) {
-		var panels = this.getPanels(),
-			oPanel = null;
-
-		if (panels.length > index) {
-			panels[index].destroy();
-			if (panels.length > index) {
-				inMoreInfo = enyo.mixin({addBefore: panels[index]}, inMoreInfo);
-			}
-		}
-		oPanel = this.createComponent(inInfo, inMoreInfo);
-		oPanel.render();
-		this.resized();
-	},
 	onTap: function(oSender, oEvent) {
 		var n = this.getPanelIndex(oEvent.originator);
 
@@ -126,12 +75,156 @@ enyo.kind({
 		}
 		return false;
 	},
-	next: function() {
-		var nextIndex = this.clamp(this.index+1);
-		if (nextIndex != this.getIndex()) {
-			this.setIndex(nextIndex);
+	/**
+		If any panel has a pre-transition, pushes the panel's index to
+		_preTransitionWaitList_.
+	*/
+	triggerPanelPreTransitions: function() {
+		var panels = this.getPanels(),
+			options = {};
+
+		this.preTransitionWaitlist = [];
+		for(var i = 0, panel; (panel = panels[i]); i++) {
+			options = this.getTransitionOptions(i, this.toIndex);
+			if (panel.preTransition && panel.preTransition(this.fromIndex, this.toIndex, options)) {
+				this.preTransitionWaitlist.push(i);
+			}
+		}
+
+		if (this.preTransitionWaitlist.length === 0) {
+			this.transitionReady = true;
+			this.setIndex(this.transitionIndex);
 		}
 	},
+	panelPreTransitionComplete: function(inSender, inEvent) {
+		var index = this.getPanels().indexOf(inEvent.originator);
+
+		for (var i = 0; i < this.preTransitionWaitlist.length; i++) {
+			if (this.preTransitionWaitlist[i] === index) {
+				this.preTransitionWaitlist.splice(i,1);
+				break;
+			}
+		}
+
+		if (this.preTransitionWaitlist.length === 0) {
+			this.preTransitionComplete();
+		}
+	},
+	preTransitionComplete: function() {
+		this.transitionReady = true;
+		this.setIndex(this.transitionIndex);
+	},
+	triggerPanelPostTransitions: function() {
+		var panels = this.getPanels(),
+			options = {};
+		this.postTransitionWaitlist = [];
+		for(var i = 0, panel; (panel = panels[i]); i++) {
+			options = this.getTransitionOptions(i, this.toIndex);
+			if (panel.postTransition && panel.postTransition(this.fromIndex, this.toIndex, options)) {
+				this.postTransitionWaitlist.push(i);
+			}
+		}
+
+		if (this.postTransitionWaitlist.length === 0) {
+			this.postTransitionComplete();
+		}
+	},
+	panelPostTransitionComplete: function(inSender, inEvent) {
+		var index = this.getPanels().indexOf(inEvent.originator);
+
+		for (var i = 0; i < this.postTransitionWaitlist.length; i++) {
+			if (this.postTransitionWaitlist[i] === index) {
+				this.postTransitionWaitlist.splice(i,1);
+				break;
+			}
+		}
+
+		if (this.postTransitionWaitlist.length === 0) {
+			this.postTransitionComplete();
+		}
+	},
+	postTransitionComplete: function() {
+		// TODO - something here?
+	},
+	getTransitionOptions: function(fromIndex, toIndex) {
+		if (this.layout.getTransitionOptions) {
+			return this.layout.getTransitionOptions(fromIndex, toIndex);
+		} else {
+			return {};
+		}
+	},
+
+	_applyPattern: function() {
+		switch (this.pattern) {
+		case "none":
+			break;
+		case "alwaysviewing":
+			this.createChrome([
+				{
+					name: "backgroundScrim",
+					kind: enyo.Control,
+					classes: "moon-panels-background-scrim",
+					components: [
+						{
+							name: "spotable",
+							kind: enyo.Control,
+							classes: "moon-panels-spot",
+							isChrome: true,
+							onenter: "show"
+						}
+					]
+				},
+				{
+					name: "client",
+					kind: enyo.Control,
+					classes: "enyo-fill enyo-arranger moon-panels-client",
+					ontap: "hide",
+					components: [
+						{
+							name: "panelScrim",
+							kind: enyo.Control,
+							classes: "moon-panels-panel-scrim",
+							isChrome: true
+						}
+					]
+				}
+			]);
+			this.panelCoverRatio = 0.5;
+			break;
+		case "activity":
+			/* falls through */
+		default:
+			this.showFirstBreadcrumb = true;
+		}
+	},
+
+	onTransitionFinish: function(inSender, inEvent) {
+		if (inSender === this.$.client) {
+			switch (this._transitionCommand) {
+			case "show":
+				this.doShowFinished(inEvent);
+				break;
+			case "hide":
+				this.doHideFinished(inEvent);
+				break;
+			case "hideLeft":
+				this.doHideLeftFinished(inEvent);
+				break;
+			default:
+				// not handled
+			}
+		}
+	},
+	_clearTransition: function() {
+		this.$.backgroundScrim.addRemoveClass("on", false);
+		this.$.client.addRemoveClass("left", false);
+		this.$.client.addRemoveClass("right", false);
+		this.$.client.addRemoveClass("show", false);
+	},
+	_show: function() {
+		this.$.client.addClass("show");
+	},
+
 	// Called when focus leaves one of the panels.
 	onSpotlightPanelLeave: function(oSender, oEvent) {
 		if (oEvent.originator != this.getActive())	{ return false; }
@@ -180,7 +273,73 @@ enyo.kind({
 		return false;
 	},
 
-	// Gets index of a panel by its reference.
+	//* @public
+	/**
+		Returns an array of contained panels.
+		Subclasses can override this if they don't want the arranger to layout all of their children
+	*/
+	getPanels: function() {
+		return this.getClientControls();
+	},
+	//* Creates a panel on top of the stack and increments index to select that
+	//* component.
+	pushPanel: function(inInfo, inMoreInfo) { // added
+		var lastIndex = this.getPanels().length - 1,
+			oPanel = this.createComponent(inInfo, inMoreInfo);
+
+		oPanel.render();
+		this.resized();
+		this.setIndex(lastIndex+1);
+		return oPanel;
+	},
+	//* Creates multiple panels on top of the stack and updates index to select
+	//* the last one created.
+	pushPanels: function(inInfos, inCommonInfo) { // added
+		var lastIndex = this.getPanels().length - 1,
+			oPanels = this.createComponents(inInfos, inCommonInfo),
+			nPanel;
+
+		for (nPanel in oPanels) {
+			oPanels[nPanel].render();
+		}
+
+		this.resized();
+		this.setIndex(lastIndex+1);
+		return oPanels;
+	},
+	//* Destroys panels whose index is greater than or equal to _inIndex_.
+	popPanels: function(inIndex) {
+		var panels = this.getPanels();
+		inIndex = inIndex || panels.length - 1;
+
+		while (panels.length > inIndex && inIndex >= 0) {
+			panels[panels.length - 1].destroy();
+		}
+	},
+	//* Destroy right panel and create panel without transition effect. */
+	replacePanel: function(index, inInfo, inMoreInfo) {
+		var panels = this.getPanels(),
+			oPanel = null;
+
+		if (panels.length > index) {
+			panels[index].destroy();
+			if (panels.length > index) {
+				inMoreInfo = enyo.mixin({addBefore: panels[index]}, inMoreInfo);
+			}
+		}
+		oPanel = this.createComponent(inInfo, inMoreInfo);
+		oPanel.render();
+		this.resized();
+	},
+	//* Advance panel index by 1 until index is reached to the last. */
+	next: function() {
+		var nextIndex = this.clamp(this.index+1);
+		if (nextIndex != this.getIndex()) {
+			this.setIndex(nextIndex);
+		}
+	},
+
+	/** Gets index of a panel by its reference. */
 	getPanelIndex: function(oControl) {
 		var oPanel = null;
 
@@ -221,150 +380,25 @@ enyo.kind({
 			this.triggerPanelPreTransitions();
 		}
 	},
-	/**
-		If any panel has a pre-transition, pushes the panel's index to
-		_preTransitionWaitList_.
-	*/
-	triggerPanelPreTransitions: function() {
-		var panels = this.getPanels(),
-			options = {};
-
-		this.preTransitionWaitlist = [];
-		for(var i = 0, panel; (panel = panels[i]); i++) {
-			options = this.getTransitionOptions(i, this.toIndex);
-			if (panel.preTransition && panel.preTransition(this.fromIndex, this.toIndex, options)) {
-				this.preTransitionWaitlist.push(i);
-			}
-		}
-
-		if (this.preTransitionWaitlist.length === 0) {
-			this.transitionReady = true;
-			this.setIndex(this.transitionIndex);
-		}
-	},
-	panelPreTransitionComplete: function(inSender, inEvent) {
-		var index = this.getPanels().indexOf(inEvent.originator);
-
-		for (var i = 0; i < this.preTransitionWaitlist.length; i++) {
-			if (this.preTransitionWaitlist[i] === index) {
-				this.preTransitionWaitlist.splice(i,1);
-				break;
-			}
-		}
-
-		if (this.preTransitionWaitlist.length === 0) {
-			this.preTransitionComplete();
-		}
-	},
-	preTransitionComplete: function() {
-		this.transitionReady = true;
-		this.setIndex(this.transitionIndex);
-	},
-
-	triggerPanelPostTransitions: function() {
-		var panels = this.getPanels(),
-			options = {};
-		this.postTransitionWaitlist = [];
-		for(var i = 0, panel; (panel = panels[i]); i++) {
-			options = this.getTransitionOptions(i, this.toIndex);
-			if (panel.postTransition && panel.postTransition(this.fromIndex, this.toIndex, options)) {
-				this.postTransitionWaitlist.push(i);
-			}
-		}
-
-		if (this.postTransitionWaitlist.length === 0) {
-			this.postTransitionComplete();
-		}
-	},
-	panelPostTransitionComplete: function(inSender, inEvent) {
-		var index = this.getPanels().indexOf(inEvent.originator);
-
-		for (var i = 0; i < this.postTransitionWaitlist.length; i++) {
-			if (this.postTransitionWaitlist[i] === index) {
-				this.postTransitionWaitlist.splice(i,1);
-				break;
-			}
-		}
-
-		if (this.postTransitionWaitlist.length === 0) {
-			this.postTransitionComplete();
-		}
-	},
-	postTransitionComplete: function() {
-		// TODO - something here?
-	},
-	getTransitionOptions: function(fromIndex, toIndex) {
-		if (this.layout.getTransitionOptions) {
-			return this.layout.getTransitionOptions(fromIndex, toIndex);
-		} else {
-			return {};
-		}
-	},
-	_applyPattern: function() {
-		switch (this.pattern) {
-		case "none":
-			break;
-		case "alwaysviewing":
-			this.createChrome([
-				{
-					name: "backgroundScrim",
-					kind: enyo.Control,
-					classes: "moon-panels-background-scrim",
-					components: [
-						{
-							name: "spotable",
-							kind: enyo.Control,
-							classes: "moon-panels-spot",
-							isChrome: true,
-							onenter: "show"
-						}
-					]
-				},
-				{
-					name: "client",
-					kind: enyo.Control,
-					classes: "enyo-fill enyo-arranger moon-panels-client",
-					ontap: "hide",
-					components: [
-						{
-							name: "panelScrim",
-							kind: enyo.Control,
-							classes: "moon-panels-panel-scrim",
-							isChrome: true
-						}
-					]
-				}
-			]);
-			this.panelCoverRatio = 0.5;
-			break;
-		case "activity":
-			/* falls through */
-		default:
-			this.showFirstBreadcrumb = true;
-		}
-	},
+	//* Show panals with transition from right */
 	show: function() {
+		this._transitionCommand = "show";
 		this._clearTransition();
 		this.$.backgroundScrim.addRemoveClass("on", true);
 		enyo.job(this.id + "hide", this.bindSafely("_show"), 0.3);
 		return true;
 	},
-	_show: function() {
-		this.$.client.addClass("show");
-	},
+	//* Hide panals with transition to right */
 	hide: function() {
+		this._transitionCommand = "hide";
 		this._clearTransition();
 		this.$.client.addRemoveClass("right", true);
 		return true;
 	},
+	//* Hide panals with transition to left */
 	hideToLeft: function() {
+		this._transitionCommand = "hideLeft";
 		this._clearTransition();
 		this.$.client.addRemoveClass("left", true);
-	},
-	_clearTransition: function() {
-		this.$.backgroundScrim.addRemoveClass("on", false);
-		this.$.client.addRemoveClass("left", false);
-		this.$.client.addRemoveClass("right", false);
-		this.$.client.addRemoveClass("show", false);
 	}
 });
