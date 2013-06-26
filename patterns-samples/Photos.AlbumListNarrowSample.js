e2cae6b7cc82efc00e793bef2a293abb4fd186c8
David.Um <joonho.um.snu@gmail.com>
2013-06-26 16:54:02 +0900

    GF-7754: Applying MVC and code clean up
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

--- patterns-samples/Photos.AlbumListNarrowSample.js
+++ patterns-samples/Photos.AlbumListNarrowSample.js
@@ -1,106 +1,118 @@
 enyo.kind({
-    name: "moon.sample.photos.AlbumListNarrowSample",
-    kind : "moon.Panel",
-    classes : "moon enyo-fit",
-    title : "ALBUMS",
-    titleAbove : "01",
-    titleBelow : "",
-    components: [
-        {kind : "enyo.Spotlight"},
-        {
-
-            kind : "moon.Scroller",
-            classes: "enyo-fill",
-            horizontal : "hidden",
-            touch : true,
-            components : [
-                {
-                    kind : "sample.photo.AlbumListItem",
-                    source: "./assets/default-movie.png",
-                    bgSource : "./assets/bg-movie.png",
-                },
-                {
-                    kind : "sample.photo.AlbumListItem",
-                    source: "./assets/default-movie.png",
-                    bgSource : "./assets/bg-movie.png"
-                },
-                {
-                    kind : "sample.photo.AlbumListItem",
-                    source: "./assets/default-movie.png",
-                    bgSource : "./assets/bg-movie.png"
-                },
-                {
-                    kind : "sample.photo.AlbumListItem",
-                    source: "./assets/default-movie.png",
-                    bgSource : "./assets/bg-movie.png"
-                },
-                {
-                    kind : "sample.photo.AlbumListItem",
-                    source: "./assets/default-movie.png",
-                    bgSource : "./assets/bg-movie.png"
-                }
-            ]
-        }
-    ]
-});
-
-enyo.kind({
-    name: "sample.photo.AlbumListItem",
+    name: "moon.sample.photos.AlbumListItem",
     classes: "sample-album-list-item",
     kind: "moon.Item",
+
+    published: {
+        option: {
+            source: "",
+            bgSource: "",
+            title: ""    
+        }        
+    },
+
     components:[
         {
-            kind : "FittableColumns",
-            components : [
+            kind: "FittableColumns",
+            components: [
                 {
-                    components : [
+                    components: [
                         {
-                            name : "bgImage",
-                            kind : "enyo.Image",
-                            classes : "front-image",
-                            components : [
+                            name: "bgImage",
+                            kind: "enyo.Image",
+                            classes: "front-image",
+                            components: [
                                 {
-                                    name : "image",
-                                    kind : "enyo.Image",
-                                    classes : "back-image",
+                                    name: "image",
+                                    kind: "enyo.Image",
+                                    classes: "back-image"
                                 }
                             ]
-                        },
+                        }
                     ]
                 },
                 {
-                    name : "title",
-                    classes : "title-text",
-                    style : "margin-left : 10px",
+                    name: "title",
+                    classes: "title-text",
+                    style: "margin-left: 10px"
                 }
             ]
         }
     ],
 
-    published: {
-        source: '',
-        bgSource : '',
-        title : '',
-    },
-
     create: function() {
         this.inherited(arguments);
-        this.sourceChanged();
-        this.textChanged();
+        this.optionChanged();
     },
 
-    sourceChanged: function() {
-        if (!this.source || this.source === '') {
+    optionChanged: function() {
+        if (!this.option.source || this.source === '') {
             return;
         }
-        this.$.image.setAttribute('src', this.source);
-        this.$.bgImage.setAttribute('src', this.bgSource);
-    },
-
-    textChanged : function(){
-        if (!this.title || this.title === '') {
-            return;
-        }
-        this.$.title.content = this.title;
+        this.$.image.setAttribute('src', this.option.source);
+        this.$.bgImage.setAttribute('src', this.option.bgSource);
+        this.$.title.content = this.option.title;
     }
+});
+
+enyo.kind({
+    name: "moon.sample.photos.AlbumListNarrowSample",
+    kind: "moon.Panel",
+    titleAbove: "01",
+    title: "ALBUMS",
+    titleBelow: "",
+    components: [
+        {
+            name: "menuList",
+            kind: "enyo.DataList",
+            scrollerOptions: {horizontal: "hidden", thumb: false},
+            components: [
+                {
+                    kind: "moon.sample.photos.AlbumListItem",
+                    bindFrom: "option",
+                    bindTo: "option"
+                }
+            ]
+        }
+    ],
+
+    bindings: [
+        {from: ".controller.menus", to: "$.menuList.controller"}
+    ]
+});
+
+// Sample model
+
+enyo.ready(function(){
+    var sampleModel = new enyo.Model({
+        menus: new enyo.Collection([
+            {option: {source: "./assets/default-movie.png", bgSource: "./assets/bg-movie.png", title: "Album Name"}},
+            {option: {source: "./assets/default-movie.png", bgSource: "./assets/bg-movie.png", title: "Album Name"}},
+            {option: {source: "./assets/default-movie.png", bgSource: "./assets/bg-movie.png", title: "Album Name"}},
+            {option: {source: "./assets/default-movie.png", bgSource: "./assets/bg-movie.png", title: "Album Name"}}            
+        ])
+    });
+
+//  Application to render sample
+
+    new enyo.Application({
+        view: {
+            classes: "enyo-unselectable moon",
+            components: [
+                {kind: "enyo.Spotlight"},
+                {
+                    kind: "moon.sample.photos.AlbumListNarrowSample",
+                    controller: ".app.controllers.photoController",
+                    classes: "enyo-fit"
+                }
+            ]
+        },
+        controllers: [
+            {
+                name: "photoController",
+                kind: "enyo.ModelController",
+                model: sampleModel
+            }
+        ]
+    });
 });
