e9ea14aebe62c7edc3d43fc0572bfdca947463ba
Jungchae Kim <jungchae.kim@lge.com>
2013-06-28 14:42:33 -0700

    Video Player Sample in Panel Updates
    Enyo-DCO-1.1-Singed-off-by: Jungchae Kim jungchae.kim@lge.com

---
+++ samples/PanelsVideoPlayerSample/PanelsVideoPlayerSample.js
@@ -0,0 +1,96 @@
+enyo.kind({
+	name: "moon.sample.PanelsVideoPlayerSample",
+	classes: "moon enyo-fit enyo-unselectable",
+	published: {
+		server: "http://media.w3.org/2010/05/sintel/",
+		fileName: "trailer",
+		fileExt: "mp4"
+	},
+	components: [
+		{kind: "enyo.Spotlight"},
+		{name: "panels", kind: "moon.Panels", pattern: "activity", classes: "enyo-fit", components: [
+			{kind: "moon.Panel", title: "Intro", components: [
+				{kind: "moon.Divider", content: "Select video content"},
+				{kind: "Group", style: "margin-top: 20px;", onActivate: "groupChanged", components: [
+					{kind: "moon.SelectableItem", content: "Counter", onchange: "webCounter", checked: true}, 
+					{kind: "moon.SelectableItem", content: "Bunny", onchange: "webMovieBunny"},
+					{kind: "moon.SelectableItem", content: "Sintel", onchange: "webMovieSintel"}
+				]}
+			]},
+
+			{kind: "moon.Panel", joinToPrev: true, title: "Workspace", layoutKind: "FittableColumnsLayout", components: [
+
+				{name:"videoSampler", kind: "moon.sample.VideoPlayerSample", ontap: "requestFullScreen", style: "width: 720px; margin-top: 40px;z-index:10;"},
+				{fit: true, layoutKind: "FittableRowsLayout", components: [
+					{kind: "moon.Item", style: "float: right;", content: "Capture", ontap: "next"},
+					{kind: "moon.Divider", style: "margin-top:40px", content:"Select Video Format"},
+					{name:"pickerExt", kind:"moon.SimplePicker", onChange:"changed", components: [
+						{content:"mp4"},
+						{content:"webm"},
+						{content:"ogv"}
+					]}
+				]}
+			]},
+
+			{kind: "moon.Panel", title: "Result", components: [
+				{tag: "canvas", name: "capture"}
+			]}
+		]}
+	],
+	create: function() {
+		this.inherited(arguments);
+		this.$.videoSampler.$.player.$.video.applyStyle("width", "720px");
+	},
+	next: function(inSender, inEvent) {
+		this.updateCanvas(this.$.videoSampler.$.player.$.video.hasNode());
+		this.$.panels.next();
+		return true;
+	},
+	webCounter: function(inSender, inEvent) {
+		this.setServer("http://media.w3.org/2010/05/video/");
+		this.setFileName("movie_300");
+		this.setFileExt("mp4");
+		this.$.pickerExt.setSelectedIndex(0);
+		this.movieChange();
+		return true;
+	},
+	webMovieBunny: function(inSender, inEvent) {
+		this.setServer("http://media.w3.org/2010/05/bunny/");
+		this.setFileName("movie");
+		this.setFileExt("mp4");
+		this.$.pickerExt.setSelectedIndex(0);
+		this.movieChange();
+		return true;
+	},
+	webMovieSintel: function(inSender, inEvent) {
+		this.setServer("http://media.w3.org/2010/05/sintel/");
+		this.setFileName("trailer");
+		this.setFileExt("mp4");
+		this.$.pickerExt.setSelectedIndex(0);
+		this.movieChange();
+		return true;
+	},
+	changed: function(inSender, inEvent) {
+		this.setFileExt(inEvent.content);
+		this.movieChange();
+	},
+	movieChange: function() {
+		this.$.videoSampler.$.player.$.video.setSrc(this.server + this.fileName + "." + this.fileExt);
+		this.$.videoSampler.$.player.$.video.applyStyle("width", "720px");
+		this.$.videoSampler.$.player.$.video.setAutoplay(true);
+	},
+	requestFullScreen: function(inSender, inEvent) {
+		if(inEvent.originator.ontap=="toggleFullscreen" && !this.resizeFix) {
+			this.$.videoSampler.$.player.$.video.applyStyle("width", "auto");
+		}
+	},
+	updateCanvas: function(inNode) {
+		var drawingNode = this.$.capture.hasNode();
+		var videoNode = this.$.videoSampler.$.player.$.video.hasNode();
+		var ctx = drawingNode.getContext("2d");
+		var vdb = videoNode.getBoundingClientRect();
+		this.$.capture.applyStyle("width", vdb.width+"px");
+		this.$.capture.applyStyle("height", vdb.height+"px");
+		ctx.drawImage(inNode, 0, 0, 300, 150);
+	}
+}); 
---
+++ samples/PanelsVideoPlayerSample/VideoPlayerSample.js
@@ -0,0 +1,41 @@
+
+enyo.kind({
+	name: "moon.sample.VideoPlayerSample",
+	classes: "enyo-unselectable moon-video-player-sample",
+	components: [
+		{name: "player", kind: "moon.VideoPlayer",
+			sourceComponents: [
+			{src: "http://media.w3.org/2010/05/sintel/trailer.mp4",  type: "video/mp4"},
+			{src: "http://media.w3.org/2010/05/sintel/trailer.webm", type: "video/webm"},
+			{src: "http://media.w3.org/2010/05/sintel/trailer.ogv",  type: "video/ogg"}
+			],
+			infoComponents: [
+				{classes: "moon-video-player-description-info", components: [
+					{name: "videoDateTime", classes: "moon-header-font moon-video-player-info-datetime", content: new Date()},
+					{name: "videoTitle", classes: "moon-header-font moon-video-player-info-showname", content: "Breaking Bad - Live Free Or Die"},
+					{name: "videoChannel", classes: "moon-video-player-info-channel", content: "AMC (301) 7:00 PM - 8:00 PM"},
+					{name: "videoDescription", classes: "moon-video-player-info-synopsys", 
+					content: "As Walt deals with the aftermath of the Casa Tranquila explosion, Hank works to wrap up his investigation of Gus' empire."}
+				]},
+				{classes: "moon-video-player-settings-info", components: [
+					{content: "3D", classes: "moon-video-player-info-icon"},
+					{content: "Live", classes: "moon-video-player-info-icon"},					
+					{content: "REC 08:22", classes: "moon-video-player-info-icon moon-video-player-info-redicon"}
+				]},
+				{classes: "moon-video-player-rating-info", allowHtml: true, content: "RTV-14<br>VL SC"}
+			],
+			components: [
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"}
+			]
+		}]
+	}
+);
