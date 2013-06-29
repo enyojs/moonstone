90956cf35e698fc4c29d948d05368be2523135f9
Jungchae Kim <jungchae.kim@lge.com>
2013-06-28 19:38:58 -0700

    video player sample in panel updates
    Enyo-DCO-1.1-Singed-off-by: Jungchae Kim jungchae.kim@lge.com

--- samples/PanelsVideoPlayerSample/PanelsVideoPlayerSample.js
+++ samples/PanelsVideoPlayerSample/PanelsVideoPlayerSample.js
@@ -1,11 +1,6 @@
 enyo.kind({
 	name: "moon.sample.PanelsVideoPlayerSample",
 	classes: "moon enyo-fit enyo-unselectable",
-	published: {
-		server: "http://media.w3.org/2010/05/sintel/",
-		fileName: "trailer",
-		fileExt: "mp4"
-	},
 	components: [
 		{kind: "enyo.Spotlight"},
 		{name: "panels", kind: "moon.Panels", pattern: "activity", classes: "enyo-fit", components: [
@@ -17,55 +12,59 @@ enyo.kind({
 					{kind: "moon.SelectableItem", content: "Sintel", onchange: "webMovieSintel"}
 				]}
 			]},
-
 			{kind: "moon.Panel", joinToPrev: true, title: "Workspace", layoutKind: "FittableColumnsLayout", components: [
-				{name: "player", kind: "moon.VideoPlayer", style:"width:720px",
-					sourceComponents: [
-						{src: "http://media.w3.org/2010/05/video/movie_300.mp4",  type: "video/mp4"},
-						{src: "http://media.w3.org/2010/05/video/movie_300.webm", type: "video/webm"},
-						{src: "http://media.w3.org/2010/05/video/movie_300.ogv",  type: "video/ogg"},
-						{src: "http://media.w3.org/2010/05/bunny/movie.mp4",  type: "video/mp4"},
-						{src: "http://media.w3.org/2010/05/bunny/movie.webm", type: "video/webm"},
-						{src: "http://media.w3.org/2010/05/bunny/movie.ogv",  type: "video/ogg"},
-						{src: "http://media.w3.org/2010/05/sintel/trailer.mp4",  type: "video/mp4"},
-						{src: "http://media.w3.org/2010/05/sintel/trailer.webm", type: "video/webm"},
-						{src: "http://media.w3.org/2010/05/sintel/trailer.ogv",  type: "video/ogg"}
-					],
-					infoComponents: [
-						{classes: "moon-video-player-description-info", components: [
-							{name: "videoDateTime", classes: "moon-header-font moon-video-player-info-datetime", content: new Date()},
-							{name: "videoTitle", classes: "moon-header-font moon-video-player-info-showname", content: "Breaking Bad - Live Free Or Die"},
-							{name: "videoChannel", classes: "moon-video-player-info-channel", content: "AMC (301) 7:00 PM - 8:00 PM"},
-							{name: "videoDescription", classes: "moon-video-player-info-synopsys", 
-							content: "As Walt deals with the aftermath of the Casa Tranquila explosion, Hank works to wrap up his investigation of Gus' empire."}
-						]},
-						{classes: "moon-video-player-settings-info", components: [
-							{content: "3D", classes: "moon-video-player-info-icon"},
-							{content: "Live", classes: "moon-video-player-info-icon"},					
-							{content: "REC 08:22", classes: "moon-video-player-info-icon moon-video-player-info-redicon"}
-						]},
-						{classes: "moon-video-player-rating-info", allowHtml: true, content: "RTV-14<br>VL SC"}
-					],
+				{
 					components: [
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
-						{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-fullscreenbutton.png", ontap: "closeFullscreen"}
-					]},
-					{fit: true, layoutKind: "FittableRowsLayout", components: [
-						{kind: "moon.Item", style: "float: right;", content: "Capture", ontap: "next"},
-						{kind: "moon.Divider", style: "margin-top:40px", content:"Select Video Format"},
-						{name:"pickerExt", kind:"moon.SimplePicker", onChange:"changed", components: [
-							{content:"mp4"},
-							{content:"webm"},
-							{content:"ogv"}
-						]}
+						{name: "player", kind: "moon.VideoPlayer", style: "width: 720px;",
+							sourceComponents: [
+								{src: "http://10.195.248.113:8080/files/movie_300.mp4",  type: "video/mp4"},
+								{src: "http://10.195.248.113:8080/files/movie_300.webm", type: "video/webm"},
+								{src: "http://10.195.248.113:8080/files/movie_300.ogv",  type: "video/ogg"},
+								{src: "http://10.195.248.113:8080/files/movie.mp4",  type: "video/mp4"},
+								{src: "http://10.195.248.113:8080/files/movie.webm", type: "video/webm"},
+								{src: "http://10.195.248.113:8080/files/movie.ogv",  type: "video/ogg"},
+								{src: "http://10.195.248.113:8080/files/trailer.mp4",  type: "video/mp4"},
+								{src: "http://10.195.248.113:8080/files/trailer.webm", type: "video/webm"},
+								{src: "http://10.195.248.113:8080/files/trailer.ogv",  type: "video/ogg"}
+							],
+							infoComponents: [
+								{
+									kind: "moon.VideoInfoHeader",
+									aboveTitle: new Date(),
+									title: "Breaking Bad - Live Free Or Die",
+									subTitle: "AMC (301) 7:00 PM - 8:00 PM",
+									description: "As Walt deals with the aftermath of the Casa Tranquila explosion, Hank works to wrap up his investigation of Gus' empire.",
+									components: [
+										{content: "3D"},
+										{content: "Live"},					
+										{content: "REC 08:22", classes: "moon-video-player-info-redicon"}
+									]
+								}
+							],
+							components: [
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-placeholder.png"},
+								{kind: "moon.IconButton", src: "$lib/moonstone/images/icon-fullscreenbutton.png", ontap: "closeFullscreen"}
+							]
+						}
+					]
+				},
+				{fit: true, layoutKind: "FittableRowsLayout", components: [
+					{kind: "moon.Item", style: "float: right;", content: "Capture", ontap: "next"},
+					{kind: "moon.Divider", style: "margin-top:40px", content:"Select Video Format"},
+					{name:"pickerExt", kind:"moon.SimplePicker", onChange:"changed", components: [
+						{content:"mp4"},
+						{content:"webm"},
+						{content:"ogv"}
 					]}
+				]}
 				]},
 				{kind: "moon.Panel", title: "Result", components: [
 					{tag: "canvas", name: "capture"}
@@ -78,34 +77,19 @@ enyo.kind({
 			return true;
 		},
 		webCounter: function(inSender, inEvent) {
-			this.setFileName("movie_300");
-			this.setFileExt("mp4");
 			this.$.pickerExt.setSelectedIndex(0);
-			this.movieChange();
 			return true;
 		},
 		webMovieBunny: function(inSender, inEvent) {
-			this.setFileName("movie");
-			this.setFileExt("mp4");
 			this.$.pickerExt.setSelectedIndex(0);
-			this.movieChange();
 			return true;
 		},
 		webMovieSintel: function(inSender, inEvent) {
-			this.setFileName("trailer");
-			this.setFileExt("mp4");
 			this.$.pickerExt.setSelectedIndex(0);
-			this.movieChange();
 			return true;
 		},
 		changed: function(inSender, inEvent) {
-			this.setFileExt(inEvent.content);
-			this.movieChange();
-		},
-		movieChange: function() {
-			this.$.player.$.video.setSrc(this.server + this.fileName + "." + this.fileExt);
-			this.$.player.$.video.applyStyle("width", "720px");
-			this.$.player.$.video.setAutoplay(true);
+			// this.$.player.setSrc(this.$.player.sources[7]);
 		},
 		closeFullscreen: function(inSender, inEvent) {
 			this.$.player.toggleFullscreen(inSender, inEvent);
