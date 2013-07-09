9738ab84b04ec3cb925d2f2105b2a72f84254794
Jungchae Kim <jungchae.kim@lge.com>
2013-07-09 15:02:02 -0700

    GF-7535 moonstone UI component : moon.HighlightText
    Enyo-DCO-1.1-Singed-off-by: Jungchae Kim jungchae.kim@lge.com

--- source/HighlightText.js
+++ source/HighlightText.js
@@ -12,11 +12,15 @@
 
 	_moon.HighlightText_ get event like below, then the highlighted text is disapeared.
 		this.waterfall("offHighlight");
+
+    Not only can highlight directly using highlightOver highlightBlur function but also
+    published variable for highlight.
 */
 enyo.kind({
     name: "moon.HighlightText",
     published: {
         content: "",
+        highlight: "",
         highlightClasses: "moon-highlight-text-highlighted"
     },
     handlers: {
@@ -28,27 +32,43 @@ enyo.kind({
         this.inherited(arguments);
         this._content = this.content;
     },
-    setContent:function(inValue) {
+    setContent: function(inValue) {
         this.inherited(arguments);
         this._content = inValue;
     },
-    _setContent:function(inValue) {
+    _setContent: function(inValue) {
         this.content = inValue;
         this.contentChanged();
     },
     getContent:function() {
         return this._content;   
     },
+    highlightChanged: function(inValue) {
+        if(inValue==="") {
+            this.highlightBlur();
+        } else {
+            this.highlightOver(inValue);
+        }
+    },
     onHighlightHandler: function(inSender, inEvent) {
+        this.highlightOver(inEvent.highlight);
+        return true;
+    },
+    offHighlightHandler: function(inSender, inEvent) {
+        this.highlightBlur();
+        return true;
+    },
+    //* @public
+    highlightOver: function(inText) {
         this.allowHtml = true;
         var output = "",
-            hlt = inEvent.highlight,
+            hlt = inText,
             ort = this.getContent(),
-            eIdx = ort.length,
-            sIdx = 0
+            sIdx = 0,
+            eIdx = ort.length
         ;
         while(sIdx !== -1) {
-            sIdx = ort.search(hlt);
+            sIdx = ort.search(hlt);           
             if(sIdx !== -1) {
                 output += ort.slice(0, sIdx) + "<span class=" + this.highlightClasses + ">";
                 output += ort.slice(sIdx, sIdx+hlt.length) + "</span>";
@@ -58,13 +78,10 @@ enyo.kind({
         }
         output += ort;
         this._setContent(output);
-        
-        return true;
     },
-    offHighlightHandler: function(inSender, inEvent) {
+    highlightBlur: function() {
         this.allowHtml = false;
         this.setContent(this._content);
         this.render();
-        return true;
     }
 });
