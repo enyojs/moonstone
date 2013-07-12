6c1064552883d0eb912879465f5ade86afb037b3
patalano <christopher.patalano@lge.com>
2013-07-12 12:27:02 -0700

    Added _scrollToControl_ function for moon.Scroller.
    Enyo-DCO-1.1-Signed-off-by: Chris Patalano <christopher.patalano@lge.com>

--- source/Scroller.js
+++ source/Scroller.js
@@ -34,6 +34,16 @@ enyo.kind({
 	//* If true, scroll events are not allowed to propagate
 	preventScrollPropagation: false,
 	//* Default to moon.ScrollStrategy
-	strategyKind: "moon.ScrollStrategy"
+	strategyKind: "moon.ScrollStrategy",
+	
+	/**
+		Scrolls until _inControl_ is in view. If _inScrollFullPage_ is set, scrolls
+		until the edge of _inControl_ is aligned with the edge of the visible scroll
+		area.
+	*/
+	scrollToControl: function(inControl, inScrollFullPage) {
+		this.$.strategy.animateToControl(inControl, inScrollFullPage);
+	}
+	
 	//* @protected
 });
