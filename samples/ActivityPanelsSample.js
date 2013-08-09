3210c267ed6b4468ed8eed973862fe37b8fa2388
Ben Combee <ben.combee@lge.com>
2013-08-09 02:51:46 -0700

    moon.Panel: Make miniHeader act as marquee instead of shrunken original header, move inline style to class
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@lge.com)

--- samples/ActivityPanelsSample.js
+++ samples/ActivityPanelsSample.js
@@ -1,66 +1,66 @@
 enyo.kind({
-    name: "moon.sample.ActivityPanelsSample",
-    classes: "moon enyo-fit enyo-unselectable",
+	name: "moon.sample.ActivityPanelsSample",
+	classes: "moon enyo-fit enyo-unselectable",
 	style: "background: #fff url(https://secure.gravatar.com/avatar/70103c13ada6801c095816e50198045c?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png) repeat;",
-    components: [
-        {kind: "enyo.Spotlight"},
-        {name: "panels", kind: "moon.Panels", pattern: "activity", classes: "enyo-fit", components: [
-            {title: "First Panel", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
+	components: [
+		{kind: "enyo.Spotlight"},
+		{name: "panels", kind: "moon.Panels", pattern: "activity", classes: "enyo-fit", components: [
+			{title: "First Panel", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-            {title: "Second Panel", classes: "moon-6h",
-            	titleBelow: "Lorem ipsum dolor",
-            	subTitleBelow: "Ut enim ad minim veniam",
-            	joinToPrev: false, components: [
+			{title: "Second Panel with extra long title", classes: "moon-6h",
+				titleBelow: "Lorem ipsum dolor",
+				subTitleBelow: "Ut enim ad minim veniam",
+				joinToPrev: false, components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-            {title: "Third Panel", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
+			{title: "Third Panel with long title", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-            {title: "Fourth", joinToPrev: false, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
+			{title: "Fourth", joinToPrev: false, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-            {title: "Fifth", joinToPrev: false, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
+			{title: "Fifth", joinToPrev: false, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-            {title: "Sixth", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
+			{title: "Sixth", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-            {title: "Seventh", joinToPrev: false, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
+			{title: "Seventh", joinToPrev: false, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]}
-        ]}
-    ],
-    next: function(inSender, inEvent) {
-    	this.$.panels.next();
-    	return true;
-    }
+		]}
+	],
+	next: function(inSender, inEvent) {
+		this.$.panels.next();
+		return true;
+	}
 });
