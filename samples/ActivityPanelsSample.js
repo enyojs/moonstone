b097d204d9c4221a4c3d80587f83ec795cb9b66e
Ben Combee <ben.combee@lge.com>
2013-08-09 12:54:21 -0700

    Modify ActivityPanelsSample.js to not have quirks with switching due to active panel being not-the-last on screen
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@lge.com)

--- samples/ActivityPanelsSample.js
+++ samples/ActivityPanelsSample.js
@@ -5,59 +5,77 @@ enyo.kind({
 		{kind: "enyo.Spotlight"},
 		{name: "panels", kind: "moon.Panels", pattern: "activity", classes: "enyo-fit", components: [
 			{title: "First Panel", classes: "moon-7h", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
-				{kind: "moon.Item", content: "Item One", ontap: "next"},
-				{kind: "moon.Item", content: "Item Two", ontap: "next"},
-				{kind: "moon.Item", content: "Item Three", ontap: "next"},
-				{kind: "moon.Item", content: "Item Four", ontap: "next"},
-				{kind: "moon.Item", content: "Item Five", ontap: "next"}
+				{kind: "moon.Item", content: "Item One", ontap: "next1"},
+				{kind: "moon.Item", content: "Item Two", ontap: "next1"},
+				{kind: "moon.Item", content: "Item Three", ontap: "next1"},
+				{kind: "moon.Item", content: "Item Four", ontap: "next1"},
+				{kind: "moon.Item", content: "Item Five", ontap: "next1"}
 			]},
 			{title: "Second Panel", classes: "moon-7h",
 				joinToPrev: true, components: [
-				{kind: "moon.Item", content: "Item One", ontap: "next"},
-				{kind: "moon.Item", content: "Item Two", ontap: "next"},
-				{kind: "moon.Item", content: "Item Three", ontap: "next"},
-				{kind: "moon.Item", content: "Item Four", ontap: "next"},
-				{kind: "moon.Item", content: "Item Five", ontap: "next"}
+				{kind: "moon.Item", content: "Item One", ontap: "next2"},
+				{kind: "moon.Item", content: "Item Two", ontap: "next2"},
+				{kind: "moon.Item", content: "Item Three", ontap: "next2"},
+				{kind: "moon.Item", content: "Item Four", ontap: "next2"},
+				{kind: "moon.Item", content: "Item Five", ontap: "next2"}
 			]},
 			{title: "Third Panel", classes: "moon-7h", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
-				{kind: "moon.Item", content: "Item One", ontap: "next"},
-				{kind: "moon.Item", content: "Item Two", ontap: "next"},
-				{kind: "moon.Item", content: "Item Three", ontap: "next"},
-				{kind: "moon.Item", content: "Item Four", ontap: "next"},
-				{kind: "moon.Item", content: "Item Five", ontap: "next"}
+				{kind: "moon.Item", content: "Item One", ontap: "next3"},
+				{kind: "moon.Item", content: "Item Two", ontap: "next3"},
+				{kind: "moon.Item", content: "Item Three", ontap: "next3"},
+				{kind: "moon.Item", content: "Item Four", ontap: "next3"},
+				{kind: "moon.Item", content: "Item Five", ontap: "next3"}
 			]},
 			{title: "Fourth", classes: "moon-7h", joinToPrev: true, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
-				{kind: "moon.Item", content: "Item One", ontap: "next"},
-				{kind: "moon.Item", content: "Item Two", ontap: "next"},
-				{kind: "moon.Item", content: "Item Three", ontap: "next"},
-				{kind: "moon.Item", content: "Item Four", ontap: "next"},
-				{kind: "moon.Item", content: "Item Five", ontap: "next"}
+				{kind: "moon.Item", content: "Item One", ontap: "next4"},
+				{kind: "moon.Item", content: "Item Two", ontap: "next4"},
+				{kind: "moon.Item", content: "Item Three", ontap: "next4"},
+				{kind: "moon.Item", content: "Item Four", ontap: "next4"},
+				{kind: "moon.Item", content: "Item Five", ontap: "next4"}
 			]},
 			{title: "Fifth", classes: "moon-7h", joinToPrev: true, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
-				{kind: "moon.Item", content: "Item One", ontap: "next"},
-				{kind: "moon.Item", content: "Item Two", ontap: "next"},
-				{kind: "moon.Item", content: "Item Three", ontap: "next"},
-				{kind: "moon.Item", content: "Item Four", ontap: "next"},
-				{kind: "moon.Item", content: "Item Five", ontap: "next"}
+				{kind: "moon.Item", content: "Item One", ontap: "next5"},
+				{kind: "moon.Item", content: "Item Two", ontap: "next5"},
+				{kind: "moon.Item", content: "Item Three", ontap: "next5"},
+				{kind: "moon.Item", content: "Item Four", ontap: "next5"},
+				{kind: "moon.Item", content: "Item Five", ontap: "next5"}
 			]},
 			{title: "Sixth", classes: "moon-7h", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
-				{kind: "moon.Item", content: "Item One", ontap: "next"},
-				{kind: "moon.Item", content: "Item Two", ontap: "next"},
-				{kind: "moon.Item", content: "Item Three", ontap: "next"},
-				{kind: "moon.Item", content: "Item Four", ontap: "next"},
-				{kind: "moon.Item", content: "Item Five", ontap: "next"}
+				{kind: "moon.Item", content: "Item One"},
+				{kind: "moon.Item", content: "Item Two"},
+				{kind: "moon.Item", content: "Item Three"},
+				{kind: "moon.Item", content: "Item Four"},
+				{kind: "moon.Item", content: "Item Five"}
 			]},
 			{title: "Seventh", classes: "moon-7h", joinToPrev: true, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", components: [
-				{kind: "moon.Item", content: "Item One", ontap: "next"},
-				{kind: "moon.Item", content: "Item Two", ontap: "next"},
-				{kind: "moon.Item", content: "Item Three", ontap: "next"},
-				{kind: "moon.Item", content: "Item Four", ontap: "next"},
-				{kind: "moon.Item", content: "Item Five", ontap: "next"}
+				{kind: "moon.Item", content: "Item One"},
+				{kind: "moon.Item", content: "Item Two"},
+				{kind: "moon.Item", content: "Item Three"},
+				{kind: "moon.Item", content: "Item Four"},
+				{kind: "moon.Item", content: "Item Five"}
 			]}
 		]}
 	],
-	next: function(inSender, inEvent) {
-		this.$.panels.next();
+	// custom next handler for each panel to avoid switching from one active panel
+	// to another with no visible change for demo
+	next1: function(inSender, inEvent) {
+		this.$.panels.setIndex(2);
 		return true;
-	}
+	},
+	next2: function(inSender, inEvent) {
+		this.$.panels.setIndex(2);
+		return true;
+	},
+	next3: function(inSender, inEvent) {
+		this.$.panels.setIndex(4);
+		return true;
+	},
+	next4: function(inSender, inEvent) {
+		this.$.panels.setIndex(5);
+		return true;
+	},
+	next5: function(inSender, inEvent) {
+		this.$.panels.setIndex(5);
+		return true;
+	},
 });
