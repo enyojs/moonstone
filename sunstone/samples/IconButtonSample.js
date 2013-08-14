aa33ef6d24b4fdd2b21da2d7979bef20efbb3452
bumbae.kim <bumbae@gmail.com>
2013-08-14 09:09:32 +0900

    THEWEBOS-42: apply icon button gui for mobile
    Enyo-DCO-1.1-Signed-off-by: Bumbae Kim bumbae.kim@lge.com

--- sunstone/samples/IconButtonSample.js
+++ sunstone/samples/IconButtonSample.js
@@ -1,21 +1,21 @@
 enyo.kind({
 	name: "sun.sample.IconButtonSample",
 	kind: "FittableRows",
-	classes: "moon enyo-unselectable enyo-fit",
+	classes: "sun moon enyo-unselectable enyo-fit",
 	components: [
 		{kind: "enyo.Spotlight"},
 		{fit:true, components: [
 			{kind: "moon.Divider", content: "Icon Buttons:"},
 			{classes: "moon-hspacing", components: [
-				{kind: "sun.IconButton", src: "assets/1080x1920/icon-list.png", ontap: "buttonTapped"},
-				{kind: "sun.IconButton", src: "assets/1080x1920/icon-list.png", ontap: "buttonTapped"}
+				{kind: "sun.IconButton", src: "assets/1080x1920/icon-close-button.png", ontap: "buttonTapped"},
+				{kind: "sun.IconButton", src: "assets/1080x1920/icon-close-button.png", ontap: "buttonTapped"}
 			]},
 			{tag: "br"},
 			{tag: "br"},
 			{kind: "moon.Divider", content: "Deactivated Icon Buttons:"},
 			{classes: "moon-hspacing", components: [
-				{kind: "sun.IconButton", src: "assets/icon-list.png", ontap: "buttonTapped", disabled: true},
-				{kind: "sun.IconButton", src: "assets/icon-list.png", ontap: "buttonTapped", disabled: true}
+				{kind: "sun.IconButton", src: "assets/1080x1920/icon-close-button.png", ontap: "buttonTapped", disabled: true},
+				{kind: "sun.IconButton", src: "assets/1080x1920/icon-close-button.png", ontap: "buttonTapped", disabled: true}
 			]}
 		]},
 		{kind: "moon.Divider", content: "Result"},
