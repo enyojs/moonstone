c7712d1fd61fa30776e84e5692c3b7cd35379f58
yunshok.ahn <yunshok.ahn@lge.com>
2013-08-19 13:08:37 +0900

    THEWEBOS-507: List Item
    Implement 3 kind of list item.
    Enyo-DCO-1.1-Signed-Off-By: YunShok.Ahn (yunshok.ahn@lge.com)

--- sunstone/samples/ListItemSample.js
+++ sunstone/samples/ListItemSample.js
@@ -1,67 +1,254 @@
-// enyo.kind({
-//  name: "moon.sample.ListVerticalSample",
-//  classes: "moon enyo-unselectable enyo-fit",
-//  published: {
-//      index: 0,
-//      pageSize: 3
-//  },
-//  components: [
-//      {kind: 'enyo.Spotlight'},
-//      {name: "list", kind: "moon.List", spotlight: false, orient:"v", count: 2000, multiSelect: false, 
-//          classes: "enyo-fit list-vertical-controls-sample-list moon-list-vertical-sample",
-//          onSetupItem: "setupItem", components: [
-//          {name: "item", classes: "list-vertical-sample-item enyo-border-box", components: [
-//              {name: "index", classes: "list-sample-index"},
-//              {name: "name"}
-//          ]}
-//      ]}
-//  ],
-//  names: [],
-//  setupItem: function(inSender, inEvent) {
-//      /* global makeName */
-//      // this is the row we're setting up
-//      var i = inEvent.index;
-//      // make some mock data if we have none for this row
-//      if (!this.names[i]) {
-//          this.names[i] = makeName(5, 10, '', '');
-//      }
-//      var n = this.names[i];
-//      var ni = ("00000000" + i).slice(-7);
-//      // apply selection style if inSender (the list) indicates that this row is selected.
-//      this.$.item.addRemoveClass("list-sample-selected", inSender.isSelected(i));
-//      this.$.name.setContent(n);
-//      this.$.index.setContent(ni);
-//  }
-// });
-
-enyo.kind({
-    name: "sun.IconSmall",
-    kind: "sun.IconButton",
-    style: "height: 107px; width: 107px;"
-})
-
 enyo.kind({
     name: "moon.sample.ListItemSample",
-    classes: "moon sun enyo-unselectable enyo-fit",
-    published: {
-        index: 0,
-        pageSize: 3
-    },
+    classes: "sun moon enyo-unselectable enyo-fit",
+    listItems: [
+        {
+            subject: "Subject only"
+        },
+        {
+            subject: "Subject+ExtraInfo", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            subject: "Subject+Command", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            subject: "Subject+Command+ExtraInfo", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject only"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject+ExtraInfo", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject+Command", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject+Command+ExtraInfo", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject only"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+ExtraInfo", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+Command", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+Command+ExtraInfo", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            subject: "Subject only", 
+            item1: "Item 01"
+        },
+        {
+            subject: "Subject+ExtraInfo", 
+            item1: "Item 01", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            subject: "Subject+Command", 
+            item1: "Item 01", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            subject: "Subject+Command+ExtraInfo", 
+            item1: "Item 01", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject only", 
+            item1: "Item 01"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject+ExtraInfo", 
+            item1: "Item 01", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject+Command", 
+            item1: "Item 01", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject+Command+ExtraInfo", 
+            item1: "Item 01", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject only", 
+            item1: "Item 01"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+ExtraInfo", 
+            item1: "Item 01", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+Command", 
+            item1: "Item 01", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+Command+ExtraInfo", 
+            item1: "Item 01", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            subject: "Subject only", 
+            item1: "Item 01", 
+            item2: "Item 02"
+        },
+        {
+            subject: "Subject+ExtraInfo", 
+            item1: "Item 01", 
+            item2: "Item 02", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            subject: "Subject+Command", 
+            item1: "Item 01", 
+            item2: "Item 02", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            subject: "Subject+Command+ExtraInfo", 
+            item1: "Item 01", 
+            item2: "Item 02", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject only", 
+            item1: "Item 01", 
+            item2: "Item 02"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png",
+            subject: "Subject+ExtraInfo",
+            item1: "Item 01",
+            item2: "Item 02",
+            extraInfo: "ExtraInfo"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png",
+            subject: "Subject+Command",
+            item1: "Item 01",
+            item2: "Item 02",
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            icon: "assets/1080x1920/small-icon-close-button.png", 
+            subject: "Subject+Command+ExtraInfo", 
+            item1: "Item 01", 
+            item2: "Item 02", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject only", 
+            item1: "Item 01", 
+            item2: "Item 02"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+ExtraInfo", 
+            item1: "Item 01", 
+            item2: "Item 02", 
+            extraInfo: "ExtraInfo"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+Command", 
+            item1: "Item 01", 
+            item2: "Item 02", 
+            command: "assets/1080x1920/small-icon-close-button.png"
+        },
+        {
+            thumbnail: "assets/image-sample.png", 
+            subject: "Subject+Command+ExtraInfo", 
+            item1: "Item 01", 
+            item2: "Item 02", 
+            command: "assets/1080x1920/small-icon-close-button.png", 
+            extraInfo: "ExtraInfo"
+        },
+    ],
+
     components: [
         {kind: 'enyo.Spotlight'},
-        {name: "list", kind: "moon.List", spotlight: false, orient:"v", count: 3, multiSelect: false, 
+        {name: "list", kind: "moon.List", spotlight: false, orient:"v", count: 36, multiSelect: false, 
             classes: "enyo-fit list-vertical-controls-sample-list moon-list-vertical-sample",
             onSetupItem: "setupItem", touch: true, components: [
-            {name: "item", kind: "sun.ListItemSingle"}
-        ]}
+            {
+                name: "item", 
+                kind: "sun.ListItem", 
+                onIconTap: "itemIconTapped",
+                onThumbnailTap: "itemThumbnailTapped",
+                onCommandTap: "itemCommandTapped",
+                onListItemTap: "listItemTapped"
+            }
+        ]},
+        {name: "popup", kind: "sun.Popup", content: "Popup..."}
     ],
+
     setupItem: function(inSender, inEvent) {
-        var i = inEvent.index;
-        var ni = ("00000000" + i).slice(-8);
-        this.$.item.setIcon("assets/1080x1920/icon-close-button.png");
-        this.$.item.setLabel(ni);
-        this.$.item.setExtraInfo("extra info "+ni.slice(-2));
-        this.$.item.setExtraInfoIcon("assets/1080x1920/icon-close-button.png");
-        this.$.item.setSelected(inSender.isSelected(i));
+        this.$.item.setItem(this.listItems[inEvent.index]);
+        this.$.item.setSelected(inSender.isSelected(inEvent.index));
+    },
+
+    itemIconTapped: function(inSender, inEvent) {
+        this.preTapped = "Icon tapped first, ";
+        return true;
+    },
+
+    itemThumbnailTapped: function(inSender, inEvent) {
+        this.preTapped = "Thumbnail tapped first, ";
+        return true;
+    },
+
+    itemCommandTapped: function(inSender, inEvent) {
+        this.preTapped = "Command tapped first, ";
+        return true;
+    },
+
+    listItemTapped: function(inSender, inEvent) {
+        this.$.popup.setContent((this.preTapped || " ") + "List Item tapped (index "+inEvent.index+")");
+        this.preTapped = "";
+        this.$.popup.show();
     }
+
 });
--- sunstone/source/ListItem.js
+++ sunstone/source/ListItem.js
@@ -1,49 +1,110 @@
 enyo.kind({
-    name: "sun.ListItemSingle",
+    name: "sun.ListItem",
+    classes: "sun-listitem-item",
     published: {
         selected: false,
-        icon: "",
-        label: "",
-        text: "",
-        extraInfo: "",
-        extraInfoIcon: "",
+        item: ""
     },
-    classes: "debug sun-listitem-item",
+    events: {
+        onIconTap: "",
+        onThumbnailTap: "",
+        onCommandTap: "",
+        onListItemTap: ""
+    },
+    handlers: {
+        ontap: "listItemTapped"
+    },
     components: [
-        {name: "icon", kind: "sun.IconButton", classes: "debug sun-listitem-icon", showing: false},
-        {name: "label", classes: "debug sun-listitem-subject"},
-        {classes: "debug sun-listitem-extracontainer", components: [
-            {name: "extraInfoIcon", kind: "sun.IconButton", classes: "debug sun-listitem-extrainfoicon", showing: false},
-            {name: "extraInfo", classes: "debug sun-listitem-extrainfo"},
-        ]}
+        {name: "icon", kind: "sun.IconButton", classes: "sun-listitem-icon", small: true, ontap: "iconTapped"},
+        {name: "thumbnail", kind: "enyo.Image", classes: "sun-listitem-thumbnail", ontap: "thumbailTapped"},
+
+        {name: "subject", classes: "sun-listitem-subject"},
+        {name: "textItem1", classes: "sun-listitem-textitem1"},
+        {name: "textItem2", classes: "sun-listitem-textitem2"},
+
+        {name: "command", kind: "sun.IconButton", classes: "sun-listitem-command", small: true, ontap: "commandTapped"},
+        {name: "extraInfo", classes: "sun-listitem-extrainfo"}
     ],
 
-    labelChanged: function() {
-        this.$.label.setContent(this.label);
+    itemChanged: function() {
+        var item = this.item;
+
+        this.removeClass("sun-listitem-icon-attached");
+        this.removeClass("sun-listitem-thumbnail-attached");
+        this.$.icon.hide();
+        this.$.thumbnail.hide();
+        if (item.icon && item.icon != "") {
+            this.$.icon.addStyles("background-image:url(" + item.icon + ");");
+            this.$.icon.show();
+            this.addClass("sun-listitem-icon-attached");
+        }
+        else if (item.thumbnail && item.thumbnail != "") {
+            this.$.thumbnail.setAttribute('src', enyo.path.rewrite(item.thumbnail));
+            this.$.thumbnail.show();
+            this.addClass("sun-listitem-thumbnail-attached");
+        }
+
+        this.$.textItem1.hide();
+        this.$.textItem2.hide();
+        if (item.subject && item.subject != "") {
+            this.$.subject.setContent(item.subject);
+        }
+        if (item.item1 && item.item1 != "") {
+            this.$.textItem1.setContent(item.item1);
+            this.$.textItem1.show();
+        }
+        if (item.item2 && item.item2 != "") {
+            this.$.textItem2.setContent(item.item2);
+            this.$.textItem2.show();
+        }
+
+        this.removeClass("sun-listitem-command-attached");
+        this.$.command.hide();
+        if (item.command && item.command != "") {
+            this.$.command.addStyles("background-image:url(" + item.command + ");");
+            this.$.command.show();
+            this.addClass("sun-listitem-command-attached");
+        }
+
+        this.removeClass("sun-listitem-extrainfo-attached");
+        this.$.extraInfo.hide();
+        if (item.extraInfo && item.extraInfo != "") {
+            this.$.extraInfo.setContent(item.extraInfo);
+            this.$.extraInfo.show();
+            this.addClass("sun-listitem-extrainfo-attached");
+        }
+
+        this.removeClass("sun-listitem-singlelined");
+        this.removeClass("sun-listitem-doublelined");
+        this.removeClass("sun-listitem-triplelined");
+        if (item.item2) {
+            this.addClass("sun-listitem-triplelined");
+        }
+        else if (item.item1) {
+            this.addClass("sun-listitem-doublelined");
+        }
+        else {
+            this.addClass("sun-listitem-singlelined");
+        }
     },
+
     selectedChanged: function() {
         this.addRemoveClass("sun-listitem-selected", this.selected);
     },
-    extraInfoChanged: function() {
-        this.$.extraInfo.setContent(this.extraInfo);
-    },
-    iconChanged: function() {
-        if (!this.icon || this.icon === '') {
-           this.$.icon.hide();
-        }
-        else {
-           this.$.icon.addStyles("background-image:url(" + this.icon + ");");
-           this.$.icon.show();
-        }
-    },
-    extraInfoIconChanged: function() {
-        if (!this.extraInfoIcon || this.extraInfoIcon === '') {
-           this.$.extraInfoIcon.hide();
-        }
-        else {
-           this.$.extraInfoIcon.addStyles("background-image:url(" + this.extraInfoIcon + ");");
-           this.$.extraInfoIcon.show();
-        }
+
+    iconTapped: function(inSender, inEvent) {
+        this.doIconTap();
     },
 
+    thumbailTapped: function(inSender, inEvent) {
+        this.doThumbnailTap();
+    },
+
+    commandTapped: function(inSender, inEvent) {
+        this.doCommandTap();
+    },
+
+    listItemTapped: function(inSender, inEvent) {
+        this.doListItemTap();
+    }
 });
