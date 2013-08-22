d9e4cdd32eacff9658eb7c084f0088bba38db76c
yb.sung <yb.sung@lge.com>
2013-08-22 21:48:54 +0900

    THEWEBOS-531-ybsung: update Header Sample
    Enyo-DCO-1.1-Signed-Off-By: Yunbum Sung <yb.sung@lge.com>

--- sunstone/samples/HeaderSample.js
+++ sunstone/samples/HeaderSample.js
@@ -1,7 +1,7 @@
 enyo.kind({
 	name: "sun.sample.HeaderSample",
 	kind: "FittableRows",
-	handlers: {onHeaderLeftTapped: "onHeaderLeftTappedFunc"},
+	handlers: {onHeaderLeftTapped: "arrowTapped"},
 	classes: "sun moon enyo-unselectable enyo-fit sun-header-sample",
 	components: [
 		{kind: "enyo.Spotlight"},
@@ -14,7 +14,7 @@ enyo.kind({
 				{tag: "br"},
 				
 				{kind: "sun.Divider", content: "Optional Header"},
-				{kind: "sun.Header", content: "Header", arrowIcon: true, loading: true, progress: "33", components: [
+				{kind: "sun.Header", Toast: "buttonToast", content: "Header", arrowIcon: true, loading: true, progress: "33", components: [
 					{kind: "sun.IconButton", src: "assets/1080x1920/small-icon-close-button.png", small: true},
 					{kind: "sun.IconButton", src: "assets/1080x1920/small-icon-close-button.png", small: true},
 					{name: "loading", kind: "sun.IconButton", src: "assets/1080x1920/small-icon-close-button.png", small: true, loading: true, ontap: "loadingButtonTapped"}
@@ -35,12 +35,14 @@ enyo.kind({
 					{kind: "sun.IconButton", src: "assets/1080x1920/small-icon-close-button.png", small: true}
 				]}
 			]}
-		]}
+		]},
+		{name: "arrowToast", kind: "sun.Toast", content: "Arrow Icon Tapped!!!"}
 	],
-	onHeaderLeftTappedFunc: function(inSender, inEvent) {
-		console.log("onHeaderLeftTapped");
-	},
 	loadingButtonTapped: function(inSender, inEvent) {
 		this.$.loading.setLoading(!this.$.loading.getLoading());
+	},
+	arrowTapped: function(inSender, inEvent) {
+		this.$.arrowToast.hide();
+		this.$.arrowToast.show();
 	}
 });
