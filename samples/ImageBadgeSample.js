a9d1ec9ffc05b033db30cf85ccc48be9cbe065ae
patalano <christopher.patalano@lge.com>
2013-08-29 09:22:28 -0700

    Remove extra images.
    Enyo-DCO-1.1-Signed-off-by: Chris Patalano <christopher.patalano@lge.com>

--- samples/ImageBadgeSample.js
+++ samples/ImageBadgeSample.js
@@ -6,24 +6,24 @@ enyo.kind({
 		{kind: "enyo.Spotlight"},
 		{kind: "moon.Scroller", fit: true, components: [
 			{kind: "moon.Divider", content: "Image Badges:"},
-			{kind: "moon.Image", src: "assets/hoth.jpg", alt: "Hoth!", components: [
-				{src: "assets/commentIcon.png"},
-				{src: "assets/rottenIcon.png", classes: "float-right"}
+			{kind: "moon.Image", src: "http://placehold.it/240x360&text=Image+One", alt: "Image One", components: [
+				{src: "http://placehold.it/42x42&text=Badge"},
+				{src: "http://placehold.it/42x42&text=Badge", classes: "float-right"}
 			]},
-			{kind: "moon.Image", src: "assets/tatooine.jpg", alt: "Tatooine!", components: [
-				{src: "assets/enyoIcon.png"}
+			{kind: "moon.Image", src: "http://placehold.it/180x240&text=Image+Two", alt: "Image Two", components: [
+				{src: "http://placehold.it/42x42&text=Badge"},
 			]},
 			
 			{kind: "moon.Divider", classes: "image-badge-sample-divider", content: "Image Badges - Show on Spotlight:"},
 			{kind: "moon.Item", components: [
-				{kind: "moon.Image", src: "assets/hoth.jpg", alt: "Hoth!", showBadgesOnSpotlight: true, components: [
-					{src: "assets/commentIcon.png"},
-					{src: "assets/rottenIcon.png", classes: "float-right"}
+				{kind: "moon.Image", src: "http://placehold.it/240x360&text=Image+One", alt: "Image One", showBadgesOnSpotlight: true, components: [
+					{src: "http://placehold.it/42x42&text=Badge"},
+					{src: "http://placehold.it/42x42&text=Badge", classes: "float-right"}
 				]}
 			]},
 			{kind: "moon.Item", components: [
-				{kind: "moon.Image", src: "assets/tatooine.jpg", alt: "Tatooine!", showBadgesOnSpotlight: true, components: [
-					{src: "assets/enyoIcon.png"}
+				{kind: "moon.Image", src: "http://placehold.it/180x240&text=Image+Two", alt: "Image Two", showBadgesOnSpotlight: true, components: [
+					{src: "http://placehold.it/42x42&text=Badge"},
 				]}
 			]}
 		]}
