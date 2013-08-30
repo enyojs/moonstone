0523957c6530be16a7c3447235ff697680104d3c
patalano <christopher.patalano@lge.com>
2013-08-30 16:08:53 -0700

    Merging manually from pull 464 fixing GF-12756.
    Updated to match UX styles. General clean up.
    
    Enyo-DCO-1.1-Signed-off-by: Chris Patalano <christopher.patalano@lge.com>
    
    Conflicts:
    	css/moonstone-dark.css
    	css/moonstone-light.css

--- samples/ExpandableTextSample.js
+++ samples/ExpandableTextSample.js
@@ -3,21 +3,26 @@ enyo.kind({
 	kind: "FittableRows",
 	classes: "moon enyo-unselectable enyo-fit",
 	components: [
-		{kind: "enyo.Spotlight"},
-		{fit:true,  components : [
-			{kind: "moon.Divider", content: "#1: Basic"},
-			{kind: "moon.ExpandableText", onChange: "collapsedChanged", content: "Pixar genius reigns in this funny romantic comedy, which stars a robot who says absolutely nothing for a full 25 minutes yet somehow completely transfixes and endears himself to the audience within the first few minutes of the film. As the last robot left on earth, Wall-E (voiced by Ben Burtt) is one small robot--with a big, big heart--who holds the future of earth and mankind squarely in the palm of his metal hand. He's outlasted all the \"Waste Allocation Load Lifter Earth-Class\" robots that were assigned some 700 years ago to clean up the environmental mess that man made of earth while man vacationed aboard the luxury spaceship Axiom."},
+		{kind: "moon.Scroller", fit: true,  components: [
+			{kind: "moon.Divider", content: "#1: Basic, No Expansion Needed"},
+			{kind: "moon.ExpandableText", onExpandCollapse: "collapsedChanged", content: "Pixar genius reigns in this funny romantic comedy."},
 			{tag: "br"},
-			{kind: "moon.Divider", content: "#2: With 'collapsed: false'"},
-			{kind: "moon.ExpandableText", onChange: "collapsedChanged", collapsed: false, content: "Pixar genius reigns in this funny romantic comedy, which stars a robot who says absolutely nothing for a full 25 minutes yet somehow completely transfixes and endears himself to the audience within the first few minutes of the film. As the last robot left on earth, Wall-E (voiced by Ben Burtt) is one small robot--with a big, big heart--who holds the future of earth and mankind squarely in the palm of his metal hand. He's outlasted all the \"Waste Allocation Load Lifter Earth-Class\" robots that were assigned some 700 years ago to clean up the environmental mess that man made of earth while man vacationed aboard the luxury spaceship Axiom."},
+			
+			{kind: "moon.Divider", content: "#2: Basic"},
+			{kind: "moon.ExpandableText", onExpandCollapse: "collapsedChanged", content: "Pixar genius reigns in this funny romantic comedy, which stars a robot who says absolutely nothing for a full 25 minutes yet somehow completely transfixes and endears himself to the audience within the first few minutes of the film. As the last robot left on earth, Wall-E (voiced by Ben Burtt) is one small robot--with a big, big heart--who holds the future of earth and mankind squarely in the palm of his metal hand. He's outlasted all the \"Waste Allocation Load Lifter Earth-Class\" robots that were assigned some 700 years ago to clean up the environmental mess that man made of earth while man vacationed aboard the luxury spaceship Axiom."},
 			{tag: "br"},
-			{kind: "moon.Divider", content: "#3: With 'maxLines: 1'"},
-			{kind: "moon.ExpandableText", onChange: "collapsedChanged", maxLines: 1, content: "Pixar genius reigns in this funny romantic comedy, which stars a robot who says absolutely nothing for a full 25 minutes yet somehow completely transfixes and endears himself to the audience within the first few minutes of the film. As the last robot left on earth, Wall-E (voiced by Ben Burtt) is one small robot--with a big, big heart--who holds the future of earth and mankind squarely in the palm of his metal hand. He's outlasted all the \"Waste Allocation Load Lifter Earth-Class\" robots that were assigned some 700 years ago to clean up the environmental mess that man made of earth while man vacationed aboard the luxury spaceship Axiom."}
+			
+			{kind: "moon.Divider", content: "#3: Collapsed: false"},
+			{kind: "moon.ExpandableText", onExpandCollapse: "collapsedChanged", collapsed: false, content: "Pixar genius reigns in this funny romantic comedy, which stars a robot who says absolutely nothing for a full 25 minutes yet somehow completely transfixes and endears himself to the audience within the first few minutes of the film. As the last robot left on earth, Wall-E (voiced by Ben Burtt) is one small robot--with a big, big heart--who holds the future of earth and mankind squarely in the palm of his metal hand. He's outlasted all the \"Waste Allocation Load Lifter Earth-Class\" robots that were assigned some 700 years ago to clean up the environmental mess that man made of earth while man vacationed aboard the luxury spaceship Axiom."},
+			{tag: "br"},
+			
+			{kind: "moon.Divider", content: "#4: MaxLines: 1"},
+			{kind: "moon.ExpandableText", onExpandCollapse: "collapsedChanged", maxLines: 1, content: "Pixar genius reigns in this funny romantic comedy, which stars a robot who says absolutely nothing for a full 25 minutes yet somehow completely transfixes and endears himself to the audience within the first few minutes of the film. As the last robot left on earth, Wall-E (voiced by Ben Burtt) is one small robot--with a big, big heart--who holds the future of earth and mankind squarely in the palm of his metal hand. He's outlasted all the \"Waste Allocation Load Lifter Earth-Class\" robots that were assigned some 700 years ago to clean up the environmental mess that man made of earth while man vacationed aboard the luxury spaceship Axiom."}
 		]},
 		{kind: "moon.Divider", content: "Result"},
-		{name: "console", classes: "moon-input-sample-console", content: "Collapsed: ", allowHtml: true}
+		{name: "console", classes: "moon-input-sample-console", content: "No Change Yet"}
 	],
 	collapsedChanged: function(inSender, inEvent) {
-		this.$.console.setContent("Collapsed of " + inSender.name + " : " + inSender.collapsed);
+		this.$.console.setContent(inSender.name + (inSender.collapsed ? " Collapsed" : " Expanded"));
 	}
 });
