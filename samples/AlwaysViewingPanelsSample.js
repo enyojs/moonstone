9ed906e7f35bcdb15ad5c214dea950c855596d44
KunmyonChoi <dilly97@gmail.com>
2013-08-30 03:20:40 -0700

    Cherry pick number one.
    GF-11375 Panels over VideoPlayer pattern
    - Adding Signal and handlers on VideoPlayer to show/hide controls
    - Forcing panel to have spotlight container logic is moved to createComponents on moon.Panels
    - Adding onShowFinished, onHideFinished event on moon.Panels
    - Adding ontransitionend event handler to handle moon.Panels show/hide transition on moon.Panels
    - Adding patternTools component block to dynamically create components based on pattern of moon.Panels
    - Adding fullscreen background and 50% scrim
    - Adding right positioned show/hide handle on moon.Panels
    - Modifying onTap handler to hide when bg taped
    - getPanels is overridden to  get Client controls (Panel) only
    - Modifying moon.Panel to have proper index on Header
    - Modifying BreadcrumbArranger to get joinToPrev index
    - Change Activity and AlwaysViewing Panels Samples to have both Video Player and Panels in Fullscreen
    - Adding dark theme with caret assets, needs light theme
    
    This pull request requires modified Spotlight decorator for Container.
    
    Enyo-DCO-1.1-Signed-off-by: Kunmyon Choi <kunmyon.choi@lge.com>
    
    Conflicts:
    	css/moonstone-variables-dark.less
    	css/moonstone-variables-light.less
    	samples/ActivityPanelsSample.js
    	samples/AlwaysViewingPanelsSample.js

--- samples/AlwaysViewingPanelsSample.js
+++ samples/AlwaysViewingPanelsSample.js
@@ -2,6 +2,45 @@ enyo.kind({
     name: "moon.sample.AlwaysViewingPanelsSample",
     classes: "moon enyo-fit enyo-unselectable",
     components: [
+        {name: "player", kind: "moon.VideoPlayer", src: "http://media.w3.org/2010/05/bunny/movie.mp4", autoplay: true,
+			infoComponents: [
+				{kind: "moon.VideoInfoBackground", orient: "left", background: true, fit: true, components: [
+					{
+						kind: "moon.ChannelInfo",
+						channelNo: "13",
+						channelName: "AMC",
+						classes: "moon-2h", 
+						components: [
+							{content: "3D"},
+							{content: "Live"},
+							{content: "REC 08:22", classes: "moon-video-player-info-redicon "}
+						]
+					},
+					{
+						kind: "moon.VideoInfoHeader",
+						title: "Downton Abbey - Extra Title",
+						subTitle: "Mon June 21, 7:00 - 8:00pm",
+						subSubTitle: "R - TV 14, V, L, SC",
+						description: "The series, set in the Youkshire country estate of Downton Abbey, depicts the lives of the aristocratic Crawley famiry and"
+					}
+				]},
+				{kind: "moon.VideoInfoBackground", orient: "right", background: true, components: [
+					{kind:"moon.Clock"}
+				]}
+			],
+			components: [
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-placeholder.png"}
+			]
+		},
         {name: "panels", kind: "moon.Panels", pattern: "alwaysviewing", classes: "enyo-fit", components: [
             {title: "First Panel", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", classes: "moon-7h", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
@@ -10,15 +49,15 @@ enyo.kind({
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-			{title: "Second Panel",
-				components: [
+            {title: "Second Panel",
+            	components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-			{title: "Third Panel", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", classes: "moon-7h", components: [
+            {title: "Third Panel", titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", classes: "moon-7h", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
@@ -32,7 +71,7 @@ enyo.kind({
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-			{title: "Fifth", joinToPrev: true, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", classes: "moon-7h", components: [
+            {title: "Fifth", joinToPrev: true, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", classes: "moon-7h", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
@@ -46,7 +85,7 @@ enyo.kind({
 				{kind: "moon.Item", content: "Item Four", ontap: "next"},
 				{kind: "moon.Item", content: "Item Five", ontap: "next"}
 			]},
-			{title: "Seventh", joinToPrev: true, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", classes: "moon-7h", components: [
+            {title: "Seventh", joinToPrev: true, titleBelow:"Sub-title", subTitleBelow:"Sub-sub title", classes: "moon-7h", components: [
 				{kind: "moon.Item", content: "Item One", ontap: "next"},
 				{kind: "moon.Item", content: "Item Two", ontap: "next"},
 				{kind: "moon.Item", content: "Item Three", ontap: "next"},
@@ -56,6 +95,7 @@ enyo.kind({
 		]}
 	],
 	next: function(inSender, inEvent) {
+		console.log("== GO NEXT:", inSender.name);
 		this.$.panels.next();
 		return true;
 	}
