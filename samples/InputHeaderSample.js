b748b735f504894c284e038a507f5144b97e7758
patalano <christopher.patalano@lge.com>
2013-08-30 12:45:16 -0700

    Added custom events for input header input/change events.
    Removed placeholder property from input header - we need to preserve the header api because header is often used in panel(s).
    
    Enyo-DCO-1.1-Signed-off-by: Chris Patalano <christopher.patalano@lge.com>
    
    Conflicts:
    	samples/InputHeaderSample.js

--- samples/InputHeaderSample.js
+++ samples/InputHeaderSample.js
@@ -2,30 +2,46 @@ enyo.kind({
 	name: "moon.sample.InputHeaderSample",
 	kind: "FittableRows",
 	classes: "moon enyo-unselectable enyo-fit moon-input-header-sample",
+	handlers: {
+		onInputHeaderInput: "handleInput",
+		onInputHeaderChange: "handleChange"
+	},
 	components: [
-		{kind: "moon.Scroller", fit: true, components: [
-			{classes:"moon-hspacing", components: [
-				{kind: "moon.InputHeader", title:"Input Header", titleAbove: "02", titleBelow: "Sub Header", 
-				subTitleBelow: "Sub-sub Header", classes:"moon-10h", oninput:"handleInput", onchange:"handleChange", 
-				components: [
+		{kind:"moon.Panels", pattern:"activity", fit:true, components: [
+			{
+				kind: "moon.Panel",
+				classes: "moon-8h",
+				headerOptions: {kind: "moon.InputHeader", components: [
 					{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-like.png"},
 					{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-next.png"}
 				]},
-				{kind: "moon.InputHeader", title: "Small Header", small: true, titleAbove: "02", titleBelow: "Sub Header", 
-				subTitleBelow: "Sub-sub Header", classes:"moon-10h", oninput:"handleInput", onchange:"handleChange", 
-				components: [
+				title:"Input Header",
+				titleAbove: "01",
+				titleBelow: "Sub Header",
+				subTitleBelow: "Sub-sub Header"
+			},
+			{
+				kind: "moon.Panel",
+				classes: "moon-8h",
+				joinToPrev: true,
+				headerOptions: {kind: "moon.InputHeader", components: [
 					{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-like.png"},
 					{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-next.png"}
-				]}
-			]}
+				]},
+				smallHeader: true,
+				title:"Small Input Header",
+				titleAbove: "02",
+				titleBelow: "Sub Header",
+				subTitleBelow: "Sub-sub Header"
+			}
 		]},
 		{kind: "moon.Divider", content: "Result", classes: "moon-input-header-sample-result"},
 		{name: "console", classes: "moon-input-header-sample-console", content: "Input: "}
 	],
 	handleInput: function(inSender, inEvent) {
-		this.$.console.setContent("Input: " + inSender.getValue());
+		this.$.console.setContent("Input: " + inEvent.originator.getValue());
 	},
 	handleChange: function(inSender, inEvent) {
-		this.$.console.setContent("Change: " + inSender.getValue());
+		this.$.console.setContent("Change: " + inEvent.originator.getValue());
 	}
 });
