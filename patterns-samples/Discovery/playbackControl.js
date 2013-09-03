b04d5633fa7cd780b735bf790c4b2072dff862f3
eunjoungcho <eunjoung.cho@lge.com>
2013-09-03 18:04:02 +0900

    - Overall Design: Full Screen Video Player (Discovery)
    Enyo-DCO-1.1-Singed-off-by: Eunjoung Cho eunjoung.cho@lge.com

--- patterns-samples/Discovery/playbackControl.js
+++ patterns-samples/Discovery/playbackControl.js
@@ -1,4 +1,29 @@
 enyo.kind({
-	name: "Discovery.components.PlaybackControl",
-	kind: "moon.VideoPlayer"
+	name: "Discovery.Components.PlaybackControl",
+	classes: "moon enyo-fit enyo-unselectable moon-video-player-sample",
+	fit: true,
+	components: [
+		{
+			name: "player",
+			kind: "moon.VideoPlayer",
+			rewindIcon: "$lib/moonstone/images/video-player/icon_skipbackward.png", 
+			fastForwardIcon: "$lib/moonstone/images/video-player/icon_skipforward.png",
+			jumpBackIcon: "hidden",
+			jumpForwardIcon: "hidden",
+			src: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+			autoplay:true,
+			infoComponents: [
+				{kind: "moon.VideoInfoBackground", orient: "left", background: true, fit: true, components: [
+					{
+						kind: "moon.VideoInfoHeader",
+						title: "Downton Abbey",
+					}
+				]},
+			],
+			components: [
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-fullscreenbutton.png"},
+				{kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon_shrink.png"}
+			]
+		}
+	]
 });
