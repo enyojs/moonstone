5bd00c5db5663151a891f7edc8cbe3361b838d5f
Jungchae Kim <jungchae.kim@lge.com>
2013-09-04 10:13:21 +0900

    Refactoring - Seperate kind from moon.VideoPlayer - Add kind for image slider - Make it sample - Apply discovery's playback control UI design
    Enyo-DCO-1.1-Singed-off-by: Jungchae Kim jungchae.kim@lge.com

--- patterns-samples/Discovery/playbackControl.js
+++ patterns-samples/Discovery/playbackControl.js
@@ -1,21 +1,19 @@
 enyo.kind({
-	name: "Discovery.Components.PlaybackControl",
+	name: "Discovery.Sample.Playback",
 	classes: "moon enyo-fit enyo-unselectable moon-video-player-sample",
 	fit: true,
 	components: [
 		{
 			name: "player",
-			kind: "moon.VideoPlayer",
-			showPlaybackControls: false,
+			kind: "Discovery.Components.VideoPlaybackControl",
 			src: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-			autoplay:true,
 			infoComponents: [
 				{kind: "moon.VideoInfoBackground", orient: "left", background: true, fit: true, components: [
 					{
 						kind: "moon.VideoInfoHeader",
-						title: "Downton Abbey",
+						title: "Downton Abbey"
 					}
-				]},
+				]}
 			],
 			components: [
 				{name: "sendBackButton", kind: "moon.IconButton", src: "$lib/moonstone/images/video-player/icon-fullscreenbutton.png", ontap: "buttonBack"},
@@ -23,13 +21,40 @@ enyo.kind({
 			]
 		}
 	],
-
 	buttonBack: function() {
-        window.open("http://www.google.com", "width=500, height=500", true);
-    },
+		window.open("http://www.google.com", "width=500, height=500", true);
+	},
 
-    buttonThumb: function() {
-    	window.open("http://www.naver.com", "width=500, height=500", true);
-    }
+	buttonThumb: function() {
+		window.open("http://www.naver.com", "width=500, height=500", true);
+	}
 });
 
+enyo.kind({
+	name: "Discovery.Components.VideoPlaybackControl",
+	kind: "moon.VideoPlayer",
+	autoplay:true,
+	create: function() {
+		this.inherited(arguments);		
+		// ICON 변경
+		this.setRewindIcon("$lib/moonstone/images/video-player/icon_skipbackward.png");
+		this.setFastForwardIcon("$lib/moonstone/images/video-player/icon_skipforward.png");
+		this.$.jumpBack.addRemoveClass("hide", true);
+		this.$.jumpForward.addRemoveClass("hide", true);
+	},
+	// override parent's function
+	rewind: function() {
+		// this.inherited(arguments);
+		this.log("Child call only");
+	},
+	// override parent's function
+	fastForward: function() {
+		// this.inherited(arguments);
+		this.log("Child call only");
+	}
+});
+
+enyo.kind({
+	name: "Discovery.Components.ImagePlaybackControl",
+	// kind: "moon.SlideShow" ???
+});
