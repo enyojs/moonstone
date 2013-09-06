608fdbc0de93c0d483e303a98c5711684a116c00
anugrahsaxena <anugrah.saxena@lge.com>
2013-09-06 16:57:41 +0530

    GF-6539 Music.SimilarTracksNarrowSample Update
    Issue: Music Similar Tracks Narrow Sample - sample includes view
    switchers but UX design does not.
    Fixes:
    1) Added "moon-hspacing" class to component extending 'moon.Item'.
    2) Added class "moon-header-font" to 'moon.Item' child components.
    3) Added class "moon-large-button-text" to 'imageTrack'.
    4) Relocated and Updated "changeName" method from Controllers to Kind to
    make "changeName" work properly.
    
    Enyo-DCO-1.1-Signed-off-by: Anugrah Saxena <anugrah.saxena@lge.com>

--- patterns-samples/Music/Music.SimilarTracksNarrowSample.js
+++ patterns-samples/Music/Music.SimilarTracksNarrowSample.js
@@ -29,15 +29,18 @@ enyo.kind({
 								{from: ".model.time", to: ".$.imageTime.content"}
 							],
 							components: [                    
-								{kind: "moon.Item", ontap: "changeName", layoutKind: "FittableColumnsLayout", components: [
+								{kind: "moon.Item", ontap: "changeName", layoutKind: "FittableColumnsLayout", classes: "moon-hspacing", components: [
 									{name: "enyoImage", kind: "enyo.Image", style: "width: 126px; height: 126px;"},
-									{components: [
-										{name: "imageTrack"},
+									{classes: "moon-header-font", components: [
+										{name: "imageTrack", classes: "moon-large-button-text"},
 										{name: "imageArtist", classes: "moon-superscript"},
 										{name: "imageTime", classes: "moon-superscript"}
 									]}                            
 								]}
-							]
+							],
+							changeName: function(inSender, inEvent) {
+								inSender.parent.model.set("track", "Good track");
+							}
 						}
 					]
 				}
@@ -84,10 +87,7 @@ enyo.ready(function (){
             {
                 name: "trackController",
                 kind: "enyo.ModelController",
-                model: sampleModel,
-                changeName: function(inSender, inEvent) {
-                    inSender.parent.controller.set("track", "Good track");
-                }
+                model: sampleModel
             }
         ]
     });
