a57b696fa20c0a54bffc014232788155cc194566
ashwinir <ashwini13.r@lge.com>
2013-09-16 17:00:27 +0900

    GF-32629 - Defect fix
    Fix:
    Growing width animation is not required, as breadcrum arranger takes
    care of width of panels. Thus, performing animation for growing the
    height of panels.
    
    Enyo-DCO-1.1-Signed-off-by: Ashwini R <ashwini13.r@lge.com>

--- source/Panel.js
+++ source/Panel.js
@@ -124,8 +124,7 @@ enyo.kind({
 		this.updateViewportSize();
 		this.shrinkWidthAnimation = this.createShrinkingWidthAnimation();
 		this.shrinkHeightAnimation = this.createShrinkingHeightAnimation();
-		this.growWidthAnimation = this.createGrowingWidthAnimation();
-		this.growHeightAnimation = this.createGrowingHeightAnimation();
+		this.growAnimation = this.createGrowingAnimation();
 	},
 	//* Update _this.$.contentWrapper_ to have the height/width of _this_
 	updateViewportSize: function() {
@@ -218,7 +217,7 @@ enyo.kind({
 		this.growing = true;
 		this.shrinking = false;
 		this.showingSmallHeader = true;
-		this.growingWidthAnimation();
+		this.growingAnimation();
 	},
 	//* @protected
 	getInitAnimationValues: function() {
@@ -234,21 +233,13 @@ enyo.kind({
 		this.haltAnimations();
 		this.preTransitionComplete();
 	},
-	growingHeightAnimation: function() {
+	growingAnimation: function() {
 		this.haltAnimations();
-		this.$.animator.play(this.growHeightAnimation.name);
-	},
-	growingWidthAnimation: function() {
-		this.haltAnimations();
-		
-		this.growingHeightAnimation();
-		// NOTE - Skipping width grow animation
-		// this.$.animator.play(this.growWidthAnimation.name);
+		this.$.animator.play(this.growAnimation.name);
 	},
 	haltAnimations: function() {
 		this.$.animator.stop();
-		this.$.animator.pause(this.growWidthAnimation.name);
-		this.$.animator.pause(this.growHeightAnimation.name);
+		this.$.animator.pause(this.growAnimation.name);
 		this.$.animator.pause(this.shrinkWidthAnimation.name);
 		this.$.animator.pause(this.shrinkHeightAnimation.name);
 	},
@@ -302,10 +293,7 @@ enyo.kind({
 		case "shrinkWidth":
 			this.preTransitionComplete();
 			return true;
-		case "growWidth":
-			this.growingHeightAnimation();
-			return true;
-		case "growHeight":
+		case "grow":
 			this.postTransitionComplete();
 			return true;
 		}
@@ -336,9 +324,9 @@ enyo.kind({
 			}
 		});
 	},
-	createGrowingHeightAnimation: function() {
+	createGrowingAnimation: function() {
 		return this.$.animator.newAnimation({
-			name: "growHeight",
+			name: "grow",
 			duration: 400,
 			timingFunction: "cubic-bezier(.25,.1,.25,1)",
 			keyframes: {
