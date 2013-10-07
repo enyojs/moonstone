1db573b7ba0dcb99a281823ed4d5326048fc045d
ashwinir <ashwini13.r@lge.com>
2013-10-07 22:43:39 +0900

    GF- 40517 - changes related to Spotlight when play/pause is hidden.
    As per Comments, changed the code to ensure at least one of the controls
    will be active when play/pause buttons are hidden.
    Tested the sample, works Fine.
    
    Enyo-DCO-1.1-Signed-off-by: Ashwini R <ashwini13.r@lge.com>

--- source/VideoPlayer.js
+++ source/VideoPlayer.js
@@ -128,7 +128,7 @@ enyo.kind({
 		onSpotlightRight: 'spotlightLeftRightHandler',
 		onresize: 'resizeHandler'
 	},
-    bindings: [
+	bindings: [
 		{from: ".sourceComponents",			to:".$.video.sourceComponents"},
 		{from: ".playbackRateHash",			to:".$.video.playbackRateHash"},
 		{from: ".jumpBackIcon",				to:".$.jumpBack.src"},
@@ -144,7 +144,7 @@ enyo.kind({
 		{from: ".showFFRewindControls",		to:".$.rewind.showing"},
 		{from: ".showPlayPauseControl",		to:".$.fsPlayPause.showing"},
 		{from: ".showVideo",				to:".$.videoContainer.showing"}
-    ],
+	],
 	
 	//* @protected
 
