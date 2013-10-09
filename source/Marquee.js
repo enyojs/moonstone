759bffe526f56c011ab84db5598e10aaf179464a
Kevin Schaaf <kevin.schaaf@lge.com>
2013-10-09 06:02:36 -0700

    Only take actions on resize if marquee was actually running.  Also apply same handlers treatment to MarqueeItem mixing as in MarqueeSupport. DCO-1.1-Signed-Off-By: Kevin Schaaf (kevin.schaaf@lge.com)

--- source/Marquee.js
+++ source/Marquee.js
@@ -124,7 +124,8 @@ moon.MarqueeSupport = {
 		return true;
 	},
 	_marquee_resize: function(inSender, inEvent) {
-		if (this.marqueeOnSpotlight) {
+		if (this.marqueeOnSpotlight && this.marqueeActive) {
+			this.marqueeActive = false;
 			this._marquee_startHold();
 		}
 	},
@@ -134,7 +135,7 @@ moon.MarqueeSupport = {
 	//* Start timer to waterfall an _onRequestMarqueeStart_ event that kicks off marquee animation on all child marquees
 	startMarquee: function() {
 		this._marquee_buildWaitList();
-		
+
 		if (this.marqueeWaitList.length === 0) {
 			return;
 		}
@@ -186,7 +187,7 @@ moon.MarqueeItem = {
 		onMarqueeEnded:""
 	},
 	//* @protected
-	handlers: {
+	_marqueeItem_Handlers: {
 		onRequestMarquee: "_marquee_requestMarquee",
 		onRequestMarqueeStart: "_marquee_startAnimation",
 		onRequestMarqueeStop: "_marquee_stopAnimation",
@@ -196,6 +197,17 @@ moon.MarqueeItem = {
 		_marquee_contentChanged: ["content"]
 	},
 	classes: "moon-marquee",
+	dispatchEvent: enyo.inherit(function (sup) {
+		return function(sEventName, oEvent, oSender) {
+			if (!oEvent.delegate) {
+				var handler = this._marqueeItem_Handlers[sEventName];
+				if (handler && this[handler](oSender, oEvent)) {
+					return true;
+				}
+			}
+			return sup.apply(this, arguments);
+		};
+	}),
 	//* When the content of this control changes, update the content of _this.$.marqueeText_ (if it exists)
 	_marquee_contentChanged: function() {
 		if (this.$.marqueeText) {
