402993aca95aff90dd10499bebc0290d1ee31fd7
Joohyun Yoon <joohyun.yoon@lge.com>
2013-10-13 14:08:50 +0900

    revise comment
    Enyo-DCO-1.1-Signed-off-by: Joohyun Yoon joohyun.yoon@lgepartner.com

--- source/RemotePlayerControl.js
+++ source/RemotePlayerControl.js
@@ -4,7 +4,7 @@ enyo.kind({
 	published: {
 		//* Keycode map (keycode : function)
 		keyMap : {
-			39 : "pause", //should be removed: it is for test on chrome browser : <left arrow key>
+			39 : "pause", //should be removed: it is for test on chrome browser : <right arrow key>
 			415 : "play",
 			413 : "stop",
 			19  : "pause",
@@ -31,7 +31,7 @@ enyo.kind({
 		enyo.dispatcher.features.push(enyo.bind(this, this.handleEvents));
 	},
 	handleEvents: function(inEvent) {
-		if (this.handleKeyEvents && (inEvent.type === "keydown" || inEvent.type === "keyup")) {
+		if (this.handleKeyEvents && inEvent.type === "keydown") {
 			var fn;
 			if((fn = this[this.keyMap[inEvent.keyCode]]) && enyo.isFunction(fn)) {
 				fn.call(this, inEvent);
@@ -54,30 +54,3 @@ enyo.kind({
 		this.doFastForward(inEvent);
 	}
 });
-
-
-///*******************************************************************************************///
-// Should be removed!!!
-// Test code for key dispather. 
-
-
-//* @protected
-/*enyo.dispatcher.features.push(
-	function(e) {
-		// NOTE: beware of properties in enyo.gesture inadvertently mapped to event types
-		if (e.type === "keydown") {
-			if(enyo.dispatcher.registerKeyMap[e.keycode]) {
-				e.keyIdentifier = enyo.dispatcher.registerKeyMap[e.keycode];
-				return enyo.dispatch(e);
-		}
-	}
-);
-
-enyo.dispatcher.registerKeyMap = {
-	415 : "play",
-	413 : "stop",
-	19  : "pause",
-	412 : "rewind",
-	417 : "fastForward"
-}*/
-///**************************************************************************************///
