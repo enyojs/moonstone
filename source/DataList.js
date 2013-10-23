1538f229f038b788f7af48a08940233196cf64dc
Cole Davis <cole.davis@lge.com>
2013-10-23 07:59:57 -0700

    fix for GF-40422, adjustment for moon delegates
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@lge.com>

--- source/DataList.js
+++ source/DataList.js
@@ -22,6 +22,30 @@ enyo.kind.extendMethods(moon.DataList.delegates.vertical, {
 			list.$.scroller.resized();
 		};
 	}),
+	/**
+		Attempts to scroll to the given index.
+	*/
+	scrollToIndex: function (list, i) {
+			// first see if the child is already available to scroll to
+		var c = this.childForIndex(list, i),
+			// but we also need the page so we can find its position
+			p = this.pageForIndex(list, i);
+		// if there is no page then the index is bad
+		if (p < 0 || p > this.pageCount(list)) { return; }
+		// if there isn't one, then we know we need to go ahead and
+		// update, otherwise we should be able to use the scroller's
+		// own methods to find it
+		if (c) {
+			c.bubble("onRequestScrollIntoView");
+		} else {
+			list.$.page1.index = p;
+			list.$.page2.index = (p+1);
+			this.refresh(list);
+			enyo.asyncMethod(function () {
+				list.scrollToIndex(i);
+			});
+		}
+	},
 	reset: enyo.inherit(function (sup) {
 		return function (list) {
 			sup.apply(this, arguments);
