6ea3bf9319b44966a49dd69f07dd25016fb071ec
ym1980.kim <ym1980.kim@lge.com>
2013-10-23 14:41:40 +0900

    GF-41275: Update to slide up/down Dialog popup. Enyo-DCO-1.1-Signed-off-by: YoungMok Kim <ym1980.kim@lge.com>

--- source/Dialog.js
+++ source/Dialog.js
@@ -10,15 +10,23 @@ enyo.kind({
 		title: "",
 		message: ""
 	},
+	bounds: null,
 	tools: [
-		{name: "title", classes: "moon-header-font moon-popup-header-text moon-dialog-title"},
 		{
-			layoutKind: "FittableColumnsLayout",
+			name: "dialogwrapper",
+			classes: "moon-neutral moon-dialog-wrapper",
 			components: [
-				{name: "message", kind:"moon.BodyText", fit: true},
-				{name: "client"}
+				{name: "title", classes: "moon-header-font moon-popup-header-text moon-dialog-title"},
+				{
+					layoutKind: "FittableColumnsLayout",
+					components: [
+						{name: "message", kind:"moon.BodyText", fit: true},
+						{name: "client"}
+					]
+				}
 			]
-		}
+		},
+		{name: "showHideAnimator", kind: "StyleAnimator", onComplete: "animationComplete"}
 	],
 	create: function() {
 		this.inherited(arguments);
@@ -30,5 +38,56 @@ enyo.kind({
 	},
 	messageChanged: function() {
 		this.$.message.setContent(this.message);
+	},
+	show: function () {
+		this.inherited(arguments);
+		if (!this.bounds) {
+			this.bounds = this.getBounds();
+		}
+		this.$.showHideAnimator.play(this.createShowAnimation().name);
+	},
+	hide: function () {
+		this.showHideScrim(false);
+		this.$.showHideAnimator.play(this.createHideAnimation().name);
+		this.aniCompleteCallback = function () {
+			this.setShowing(false);
+		}
+	},
+	animationComplete: function (inSender, inEvent) {
+		if (this.aniCompleteCallback) {
+			this.aniCompleteCallback();
+		}
+		this.aniCompleteCallback = null;
+		return true;
+	},
+	createShowAnimation: function() {
+		return this.$.showHideAnimator.newAnimation({
+			name: "show",
+			duration: 200,
+			timingFunction: "cubic-bezier(0.25, 0.1, 0.25, 1)",
+			keyframes: {
+				0: [
+					{control: this.$.dialogwrapper, properties: { "-webkit-transform": "translateY(" + this.bounds.height + "px)" }}
+				],
+				100: [
+					{control: this.$.dialogwrapper, properties: { "-webkit-transform": "translateY(0)" }}
+				]
+			}
+		});
+	},
+	createHideAnimation: function() {
+		return this.$.showHideAnimator.newAnimation({
+			name: "hide",
+			duration: 200,
+			timingFunction: "cubic-bezier(0.25, 0.1, 0.25, 1)",
+			keyframes: {
+				0: [
+					{control: this.$.dialogwrapper, properties: { "-webkit-transform": "current" }}
+				],
+				100: [
+					{control: this.$.dialogwrapper, properties: { "-webkit-transform": "translateY(" + this.bounds.height + "px)" }}
+				]
+			}
+		});
 	}
 });
