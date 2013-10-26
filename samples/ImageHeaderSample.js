6fc0f397b1820e8092ffb0200473d2ee97676bc5
Brooke Peterson <brooke.peterson@lge.com>
2013-10-25 18:47:14 -0700

    GF-41398: Production: moon.Header: Indent header contents 20px when full-width background image is used. Enyo-DCO-1.1-Signed-off-by: Brooke Peterson <brooke.peterson@lge.com>

--- samples/ImageHeaderSample.js
+++ samples/ImageHeaderSample.js
@@ -5,7 +5,7 @@ enyo.kind({
 	components: [
 			{kind: "moon.Header", name:"imageHeader", content: "Image Header", titleAbove: "02", titleBelow: "Sub Header", subTitleBelow:"Sub-sub Header", backgroundSrc: "./assets/imageheader.jpg", classes:"moon-image-header-sample", components: [
 				{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-like.png", ontap:"likeBig"},
-				{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-next.png", ontap:"shareBig"},
+				{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-next.png", ontap:"shareBig"}
 			]}
 	],
 	likeBig: function(inSender, inEvent) {
@@ -13,6 +13,6 @@ enyo.kind({
 	},
 	shareBig: function(inSender, inEvent) {
 		this.$.imageHeader.setSubTitleBelow("Please share Enyo.");
-	},
+	}
 
 });
