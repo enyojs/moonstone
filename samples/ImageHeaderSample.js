db2475e52d900a7b303ac8d59bd538f638587929
Brooke Peterson <brooke.peterson@lge.com>
2013-10-25 19:24:02 -0700

    GF-41398: Production: moon.Header: Indent header contents 20px when full-width background image is used. Enyo-DCO-1.1-Signed-off-by: Brooke Peterson <brooke.peterson@lge.com>

--- samples/ImageHeaderSample.js
+++ samples/ImageHeaderSample.js
@@ -2,16 +2,29 @@ enyo.kind({
 	name: "moon.sample.ImageHeaderSample",
 	kind: "FittableRows",
 	classes: "moon enyo-unselectable enyo-fit moon-header-sample",
-	components: [
-			{kind: "moon.Header", name:"imageHeader", content: "Image Header", titleAbove: "02", titleBelow: "Sub Header", subTitleBelow:"Sub-sub Header", backgroundSrc: "./assets/imageheader.jpg", classes:"moon-image-header-sample", components: [
-				{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-like.png", ontap:"likeBig"},
-				{kind: "moon.IconButton", src: "../patterns-samples/assets/icon-next.png", ontap:"shareBig"}
-			]}
-	    ],
-	    likeBig: function(inSender, inEvent) {
+	components: [{
+		kind: "moon.Header",
+		name: "imageHeader",
+		content: "Image Header",
+		titleAbove: "02",
+		titleBelow: "Sub Header",
+		subTitleBelow: "Sub-sub Header",
+		backgroundSrc: "./assets/imageheader.jpg",
+		classes: "moon-image-header-sample",
+		components: [{
+			kind: "moon.IconButton",
+			src: "../patterns-samples/assets/icon-like.png",
+			ontap: "likeBig"
+		}, {
+			kind: "moon.IconButton",
+			src: "../patterns-samples/assets/icon-next.png",
+			ontap: "shareBig"
+		}]
+	}],
+	likeBig: function(inSender, inEvent) {
 		this.$.imageHeader.setSubTitleBelow("Thanks for liking Enyo.");
-	    },
-	    shareBig: function(inSender, inEvent) {
+	},
+	shareBig: function(inSender, inEvent) {
 		this.$.imageHeader.setSubTitleBelow("Please share Enyo.");
-	    }
-    });
+	}
+});
