2b77b960f1c91b7cdb526a5d9f36d8834c978735
Cole Davis <cole.davis@lge.com>
2013-10-29 13:47:00 -0700

    add initial support for scrollToIndex
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@lge.com>

--- source/DataGridList.js
+++ source/DataGridList.js
@@ -23,6 +23,27 @@ enyo.kind({
 				list.$.scroller.resized();
 			};
 		}),
+		scrollToIndex: function (list, i) {
+				// first see if the child is already available to scroll to
+			var c = this.childForIndex(list, i),
+				// but we also need the page so we can find its position
+				p = this.pageForIndex(list, i);
+			// if there is no page then the index is bad
+			if (p < 0 || p > this.pageCount(list)) { return; }
+			// if there isn't one, then we know we need to go ahead and
+			// update, otherwise we should be able to use the scroller's
+			// own methods to find it
+			if (c) {
+				list.$.scroller.scrollToControl(c, true);
+			} else {
+				// we do this to ensure we trigger the paging event when necessary
+				this.resetToPosition(list, this.pagePosition(list, p));
+				// now retry the original logic until we have this right
+				enyo.asyncMethod(function () {
+					list.scrollToIndex(i);
+				});
+			}
+		},
 		reset: enyo.inherit(function (sup) {
 			return function (list) {
 				sup.apply(this, arguments);
