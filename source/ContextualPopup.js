6e4df4c7dc819b73b2b261d03d4dccd1b1d4bebb
ym1980.kim <ym1980.kim@lge.com>
2013-11-01 14:17:32 +0900

    GF-44435: Add scrim to ContextualPopup to prevent losing spotlight by other components. Enyo-DCO-1.1-Signed-off-by: YoungMok Kim <ym1980.kim@lge.com>

--- source/ContextualPopup.js
+++ source/ContextualPopup.js
@@ -36,7 +36,11 @@ enyo.kind({
 	//* @protected
 	_spotlight: null,
 	floating:true,
-	
+	/**
+		Determines whether a scrim will appear when the popup is modal.
+		Note that modal scrims are transparent, so you won't see them.
+	*/
+	scrimWhenModal: true,
 	// Layout parameters
 	
 	//* Vertical flush layout margin
@@ -200,5 +204,47 @@ enyo.kind({
 	},
 	_preventEventBubble: function(inSender, inEvent) {
 		return true;
+	},
+	showHideScrim: function(inShow) {
+		if (this.floating && this.spotlightModal && this.modal) {
+			var scrim = this.getScrim();
+			if (inShow && this.scrimWhenModal) {
+				// move scrim to just under the popup to obscure rest of screen
+				var i = this.getScrimZIndex();
+				this._scrimZ = i;
+				scrim.showAtZIndex(i);
+			} else {
+				scrim.hideAtZIndex(this._scrimZ);
+			}
+			enyo.call(scrim, "addRemoveClass", [this.scrimClassName, scrim.showing]);
+		}
+	},
+	getScrimZIndex: function() {
+		// Position scrim directly below popup
+		return this.findZIndex()-1;
+	},
+	getScrim: function() {
+		// show a transparent scrim for modal popups if scrimWhenModal is true
+		// if scrim is true, then show a regular scrim.
+		if (this.modal && this.scrimWhenModal) {
+			return moon.scrimTransparent.make();
+		}
+		return moon.scrim.make();
+	},
+	findZIndex: function() {
+		// a default z value
+		var z = this.defaultZ;
+		if (this._zIndex) {
+			z = this._zIndex;
+		} else if (this.hasNode()) {
+			// Re-use existing zIndex if it has one
+			z = Number(enyo.dom.getComputedStyleValue(this.node, "z-index")) || z;
+		}
+		this._zIndex = z;
+		return this._zIndex;
+	},
+	showingChanged: function() {
+		this.inherited(arguments);
+		this.showHideScrim(this.showing);
 	}
 });
