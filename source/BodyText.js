2a8fcaa41921be34e065411b02959a1bf4b3bc66
Jim Tang <jim.tang@lge.com>
2013-11-04 15:02:16 -0800

    For GF-40370: Marking elements public/protected, plus general API comment cleanup. Enyo-DCO-1.1-Signed-Off-By: Jim Tang (jim.tang@lge.com)

--- source/BodyText.js
+++ source/BodyText.js
@@ -1,10 +1,13 @@
 /**
-	_moon.BodyText_ is a simple control for displaying body text in an app.  It is designed to align
-	with other text-based controls.
+	_moon.BodyText_ is a simple control for displaying body text in an app.  It is
+	designed to align with other text-based controls.
 */
 
 enyo.kind({
 	name: "moon.BodyText",
+	//*	@protected
 	classes: "moon-body-text moon-body-text-spacing",
+	//* @public
+	//* If true, HTML tags are allowed in the control's content 
 	allowHtml: true
 });
