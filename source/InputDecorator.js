3513a05601c400f1802851a0aab6f373b3534e86
Brooke Peterson <brooke.peterson@lge.com>
2013-11-12 16:57:50 -0800

    GF-45338: Inputs:fix outer stroke and small corner radius on Textarea Enyo-DCO-1.1-Signed-off-by: Brooke Peterson <brooke.peterson@lge.com>

--- source/InputDecorator.js
+++ source/InputDecorator.js
@@ -1,76 +1,71 @@
 /**
-	_moon.InputDecorator_ is a control that provides input styling. Any controls
-	in the InputDecorator will appear to be inside an area styled as an	input.
-	Usually, an InputDecorator surrounds a [moon.Input](#moon.Input):
+ _moon.InputDecorator_ is a control that provides input styling. Any controls
+ in the InputDecorator will appear to be inside an area styled as an	input.
+ Usually, an InputDecorator surrounds a [moon.Input](#moon.Input):
 
-		{kind: 'moon.InputDecorator', components: [
-			{kind: 'moon.Input'}
-		]}
+ {kind: 'moon.InputDecorator', components: [
+ {kind: 'moon.Input'}
+ ]}
 
-	Other controls, such as buttons, may be placed to the right or left of the
-	input control, e.g.:
+ Other controls, such as buttons, may be placed to the right or left of the
+ input control, e.g.:
 
-		{kind: 'moon.InputDecorator', components: [
-			{kind: 'moon.IconButton', src: 'search.png'},
-			{kind: 'moon.Input'},
-			{kind: 'moon.IconButton', src: 'cancel.png'}
-		]}
+ {kind: 'moon.InputDecorator', components: [
+ {kind: 'moon.IconButton', src: 'search.png'},
+ {kind: 'moon.Input'},
+ {kind: 'moon.IconButton', src: 'cancel.png'}
+ ]}
 
-	Note that the InputDecorator fits around the content inside it. If the
-	decorator is sized, then its contents will likely need to be sized as well.
+ Note that the InputDecorator fits around the content inside it. If the
+ decorator is sized, then its contents will likely need to be sized as well.
 
-		{kind: 'moon.InputDecorator', style: 'width: 500px;', components: [
-			{kind: 'moon.Input', style: 'width: 100%;'}
-		]}
-*/
+ {kind: 'moon.InputDecorator', style: 'width: 500px;', components: [
+ {kind: 'moon.Input', style: 'width: 100%;'}
+ ]}
+ */
 
 enyo.kind({
-	name              : 'moon.InputDecorator',
-	kind              : 'enyo.ToolDecorator',
+	name : 'moon.InputDecorator',
+	kind : 'enyo.ToolDecorator',
 	//* @protected
-	tag               : 'label',
-	classes           : 'moon-input-decorator',
-	spotlight         : true,
+	tag : 'label',
+	spotlight : true,
 	spotlightDecorate : false,
-
 	handlers : {
-		onDisabledChange  : 'onDisabledChange',
-		onfocus           : 'onFocus',
-		onblur            : 'onBlur',
-		onSpotlightFocus  : 'onSpotlightFocus',
+		onDisabledChange : 'onDisabledChange',
+		onfocus : 'onFocus',
+		onblur : 'onBlur',
+		onSpotlightFocus : 'onSpotlightFocus',
 		onSpotlightSelect : 'onSpotlightSelect',
-		onSpotlightBlur   : 'onSpotlightBlur',
-		onSpotlightLeft   : 'onSpotlightLeft',
-		onSpotlightRight  : 'onSpotlightRight',
-		onSpotlightUp     : 'onSpotlightUp',
-		onSpotlightDown   : 'onSpotlightDown'
+		onSpotlightBlur : 'onSpotlightBlur',
+		onSpotlightLeft : 'onSpotlightLeft',
+		onSpotlightRight : 'onSpotlightRight',
+		onSpotlightUp : 'onSpotlightUp',
+		onSpotlightDown : 'onSpotlightDown'
 	},
 
 	//* @protected
 	/**************************************************/
 
-	_oInputControl: null,
+	_oInputControl : null,
 
 	// Returns boolean indicating whether passed-in control is an input field.
-	_isInput: function(oControl) {
-		return (
-			oControl instanceof moon.Input		||
-			oControl instanceof moon.RichText	||
-			oControl instanceof moon.TextArea
+	_isInput : function(oControl) {
+		return ( oControl instanceof moon.Input || oControl instanceof moon.RichText || oControl instanceof moon.TextArea
 		);
 	},
 
 	// Traverses tree of children to find input control.
-	_findInputControl: function(oControl) {
+	_findInputControl : function(oControl) {
 		oControl = oControl || this;
 
 		var oInputControl = null;
 
-		for (var n=0; n<oControl.children.length; n++) {
+		for (var n = 0; n < oControl.children.length; n++) {
 			if (this._isInput(oControl.children[n])) {
 				return oControl.children[n];
 			}
-			if ((oInputControl = this._findInputControl(oControl.children[n]))) {
+			if (( oInputControl = this._findInputControl(oControl.children[n]))) {
 				return oInputControl;
 			}
 		}
@@ -79,59 +74,67 @@ enyo.kind({
 	//* @public
 	/**************************************************/
 
-	create: function() {
+	create : function() {
 		this.inherited(arguments);
 		this.updateFocus(false);
 		this._oInputControl = this._findInputControl();
+		if (this._oInputControl instanceof moon.Input || this._oInputControl instanceof moon.RichText) {
+			this.addClass("moon-input-decorator");
+		}
+		if (this._oInputControl instanceof moon.TextArea) {
+			this.addClass("moon-textarea-decorator");
+		}
 	},
 
-	createComponent: function() {
+	createComponent : function() {
 		this.inherited(arguments);
 		this._oInputControl = this._findInputControl();
 	},
 
-	createComponents: function() {
+	createComponents : function() {
 		this.inherited(arguments);
 		this._oInputControl = this._findInputControl();
 	},
 
-	updateFocus: function(bFocus) {
+	updateFocus : function(bFocus) {
 		this.focused = bFocus;
 		this.addRemoveClass('moon-focused', this.alwaysLooksFocused || this.focused);
 	},
 
-	getInputControl: function() {
+	getInputControl : function() {
 		return this._oInputControl;
 	},
 
 	//* @protected
 	// Event handlers:
 	/**************************************************/
-	onFocus: function(oSender, oEvent) {
+	onFocus : function(oSender, oEvent) {
 		enyo.Spotlight.spot(this);
 		// enyo.Spotlight.disablePointerMode();
 		enyo.Spotlight.freeze();
 		this.updateFocus(true);
 	},
 
-	onBlur: function() {
+	onBlur : function() {
 		// enyo.Spotlight.enablePointerMode();
 		enyo.Spotlight.unfreeze();
 		this.updateFocus(false);
 	},
 
-	onDisabledChange: function(oSender, oEvent) {
+	onDisabledChange : function(oSender, oEvent) {
 		this.addRemoveClass('moon-disabled', oEvent.originator.disabled);
 	},
 
 	// Spotlight Event handlers:
 	/**************************************************/
 
-	onSpotlightFocus: function() {
-		this.bubble("onRequestScrollIntoView", {side: "top"});
+	onSpotlightFocus : function() {
+		this.bubble("onRequestScrollIntoView", {
+			side : "top"
+		});
 	},
 
-	onSpotlightSelect: function(oSender, oEvent) {
+	onSpotlightSelect : function(oSender, oEvent) {
 		var oInput = this.getInputControl();
 		var textArea = oInput instanceof moon.TextArea;
 		if (textArea && !oInput.hasFocus() && oEvent && (oEvent.keyCode === 13)) {
@@ -140,21 +143,25 @@ enyo.kind({
 		}
 		this.focus();
 		if (oInput && oInput.hasFocus() && oEvent) {
-			oEvent.allowDomDefault();            // Allow keydown to bubble
-			return true;                         // Prevent onSpotlightLeft to bubble
+			oEvent.allowDomDefault();
+			// Allow keydown to bubble
+			return true;
+			// Prevent onSpotlightLeft to bubble
 		}
 	},
 
-	onSpotlightBlur: function(oSender, oEvent) {
+	onSpotlightBlur : function(oSender, oEvent) {
 		this.blur();
 	},
 
-	onSpotlightLeft: function(oSender, oEvent) {
+	onSpotlightLeft : function(oSender, oEvent) {
 		var oInput = this.getInputControl();
 		if (oInput && oInput.hasFocus() && oInput.left) {
 			if (oInput.left()) {
-				oEvent.allowDomDefault();       // Allow keydown to bubble
-				return true;                    // Prevent onSpotlightLeft to bubble
+				oEvent.allowDomDefault();
+				// Allow keydown to bubble
+				return true;
+				// Prevent onSpotlightLeft to bubble
 			} else {
 				this.blur();
 				oInput.blur();
@@ -162,12 +169,14 @@ enyo.kind({
 		}
 	},
 
-	onSpotlightRight: function(oSender, oEvent) {
+	onSpotlightRight : function(oSender, oEvent) {
 		var oInput = this.getInputControl();
 		if (oInput && oInput.hasFocus() && oInput.right) {
 			if (oInput.right()) {
-				oEvent.allowDomDefault();       // Allow keydown to bubble
-				return true;                    // Prevent onSpotlightLeft to bubble
+				oEvent.allowDomDefault();
+				// Allow keydown to bubble
+				return true;
+				// Prevent onSpotlightLeft to bubble
 			} else {
 				this.onBlur();
 				oInput.blur();
@@ -175,12 +184,14 @@ enyo.kind({
 		}
 	},
 
-	onSpotlightUp: function(oSender, oEvent) {
+	onSpotlightUp : function(oSender, oEvent) {
 		var oInput = this.getInputControl();
 		if (oInput && oInput.hasFocus() && oInput.up) {
 			if (oInput.up()) {
-				oEvent.allowDomDefault();       // Allow keydown to bubble
-				return true;                    // Prevent onSpotlightLeft to bubble
+				oEvent.allowDomDefault();
+				// Allow keydown to bubble
+				return true;
+				// Prevent onSpotlightLeft to bubble
 			} else {
 				this.blur();
 				oInput.blur();
@@ -188,12 +199,14 @@ enyo.kind({
 		}
 	},
 
-	onSpotlightDown: function(oSender, oEvent) {
+	onSpotlightDown : function(oSender, oEvent) {
 		var oInput = this.getInputControl();
 		if (oInput && oInput.hasFocus() && oInput.down) {
 			if (oInput.down()) {
-				oEvent.allowDomDefault();       // Allow keydown to bubble
-				return true;                    // Prevent onSpotlightLeft to bubble
+				oEvent.allowDomDefault();
+				// Allow keydown to bubble
+				return true;
+				// Prevent onSpotlightLeft to bubble
 			} else {
 				this.blur();
 				oInput.blur();
