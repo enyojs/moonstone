61c784336b5437f86842fc88428cbd97ca34fd53
David.Um <joonho.um.snu@gmail.com>
2013-11-21 22:15:55 +0900

    GF-52200: Re-implement Wizard.FullScreenSample
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

--- patterns-samples/Wizard/Wizard.FullScreenSample.js
+++ patterns-samples/Wizard/Wizard.FullScreenSample.js
@@ -1,53 +1,19 @@
 enyo.kind({
-	name: "Sample.Wizard.Panels",
+	name: "moon.Sample.Wizard.FullScreenSample",
 	kind: "moon.Panels",
 	arrangerKind: "CardArranger",
-	classes: "moon enyo-unselectable enyo-fit",
 	handlers: {
 		onNext: "next",
 		onPrevious: "previous",
-		onCancel: "goCancel"
+		onCancel: "doCancel"
 	},
-	goCancel: function() {
-		this.log("Cancel");
-		return true;
-	}
-});
-
-enyo.kind({
-	name: "Sample.Wizard.Panel",
-	kind: "moon.Panel",
-	published: {
-		selectedText: "",
-		processed: false
-	},
-	events: {
-		onNext: "",
-		onPrevious: "",
-		onCancel: ""
-	},
-	bindings: [
-		{from: ".controller.title", to: ".title"}
-	],
-	rendered: function() {
-		this.inherited(arguments);
-		this.initialSetting();
-	},
-	initialSetting: function() {
-		// Stub
-	}
-});
-
-enyo.kind({
-	name: "moon.sample.wizard.FullScreenSample",
-	kind: "Sample.Wizard.Panels",
 	components: [
-		{name: "introPage",   kind: "moon.sample.wizard.IntroPageSample"},
-		{name: "stepPage1",   kind: "moon.sample.wizard.StepPageSample"},
-		{name: "stepPage2",   kind: "moon.sample.wizard.StepPageSample"},
-		{name: "stepPage3",   kind: "moon.sample.wizard.StepPageSample"},
-		{name: "stepPage4",   kind: "moon.sample.wizard.StepPageSample"},
-		{name: "confirmPage", kind: "moon.sample.wizard.ConfirmPageSample"}
+		{name: "introPage", kind: "moon.Sample.Wizard.Panel"},
+		{name: "stepPage1", kind: "moon.Sample.Wizard.Panel"},
+		{name: "stepPage2", kind: "moon.Sample.Wizard.Panel"},
+		{name: "stepPage3", kind: "moon.Sample.Wizard.Panel"},
+		{name: "stepPage4", kind: "moon.Sample.Wizard.Panel"},
+		{name: "finalPage", kind: "moon.Sample.Wizard.Panel"}
 	],
 	bindings: [
 		{from: ".controller", to: ".$.introPage.controller"},
@@ -55,82 +21,80 @@ enyo.kind({
 		{from: ".controller", to: ".$.stepPage2.controller"},
 		{from: ".controller", to: ".$.stepPage3.controller"},
 		{from: ".controller", to: ".$.stepPage4.controller"},
-		{from: ".controller", to: ".$.confirmPage.controller"}
+		{from: ".controller", to: ".$.finalPage.controller"}
 	],
-	onTap: function(oSender, oEvent) {
-		//* override from panels
-		// no action for Carosel Arranger using button
+	next: function() {
+		this.inherited(arguments);
+		if (this.index === this.getPanels().length - 1) {
+			this.getPanels()[this.index].finalSetting();
+		}
+	},
+	doCancel: function() {
+		this.log("Cancel");
 		return true;
 	}
 });
 
 enyo.ready(function(){
 	var wizardModel = new enyo.Model({
-			title: "Wizard Title",
-			wizContainer: new enyo.Collection([
-				{
-					id: "01",
-					subtitle: "Step Title 1",
-					imgsrc: "../assets/album.PNG",
-					instruction: "INSTRUCTIONAL TEXT GOES HERE",
-					detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet ipsum tortor,a " +
-							"hendrerit urna. Integer eget faucibus purus. Nunc non arcu turpis, venenatis aliquam neque. In " +
-							"pulvinar lectus at orci fringilla eget consectetur arcu vulputate. Nullam sodales dui eu tellus " +
-							"interdum vel volutpat velit semper."
-				},
-				{
-					id: "02",
-					subtitle: "Step Title 2",
-					imgsrc: "",
-					instruction: "INSTRUCTIONAL TEXT GOES HERE 2",
-					detail: "INPUT FIELD DESCRIPTION GOES HERE 2"
-				},
-				{
-					id: "03",
-					subtitle: "Step Title 3",
-					imgsrc: "",
-					instruction: "INSTRUCTIONAL TEXT GOES HERE 3",
-					detail: "INPUT FIELD DESCRIPTION GOES HERE 3"
-				},
-				{
-					id: "04",
-					subtitle: "Step Title 4",
-					imgsrc: "",
-					instruction: "INSTRUCTIONAL TEXT GOES HERE 4",
-					detail: "INPUT FIELD DESCRIPTION GOES HERE 4"
-				},
-				{
-					id: "05",
-					subtitle: "Step Title 5",
-					imgsrc: "",
-					instruction: "INSTRUCTIONAL TEXT GOES HERE 5",
-					detail: "INPUT FIELD DESCRIPTION GOES HERE 5"
-				},
-				{
-					id: "06",
-					subtitle: "Step Title 6",
-					imgsrc: "",
-					instruction: "Confirm your selections before complete",
-					detail: ""
-				}
-			]),
-			wizResults: new enyo.Collection([
-				{step: "STEP TITLE 01 : ", result: " -- ", processed: "[FALSE]"},
-				{step: "STEP TITLE 02 : ", result: " -- ", processed: "[FALSE]"},
-				{step: "STEP TITLE 03 : ", result: " -- ", processed: "[FALSE]"},
-				{step: "STEP TITLE 04 : ", result: " -- ", processed: "[FALSE]"},
-				{step: "STEP TITLE 05 : ", result: " -- ", processed: "[FALSE]"},
-				{step: "STEP TITLE 06 : ", result: " -- ", processed: "[FALSE]"}
-			])
-		}
-	);
+		title: "Wizard Title",
+		wizContainer: new enyo.Collection([
+			{
+				subtitle: "Step Title 1",
+				imgsrc: "../assets/album.PNG",
+				instruction: "INSTRUCTIONAL TEXT GOES HERE",
+				detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet ipsum tortor,a " +
+						"hendrerit urna. Integer eget faucibus purus. Nunc non arcu turpis, venenatis aliquam neque. In " +
+						"pulvinar lectus at orci fringilla eget consectetur arcu vulputate. Nullam sodales dui eu tellus " +
+						"interdum vel volutpat velit semper."
+			},
+			{
+				subtitle: "Step Title 2",
+				imgsrc: "",
+				instruction: "INSTRUCTIONAL TEXT GOES HERE 2",
+				detail: "INPUT FIELD DESCRIPTION GOES HERE 2"
+			},
+			{
+				subtitle: "Step Title 3",
+				imgsrc: "",
+				instruction: "INSTRUCTIONAL TEXT GOES HERE 3",
+				detail: "INPUT FIELD DESCRIPTION GOES HERE 3"
+			},
+			{
+				subtitle: "Step Title 4",
+				imgsrc: "",
+				instruction: "INSTRUCTIONAL TEXT GOES HERE 4",
+				detail: "INPUT FIELD DESCRIPTION GOES HERE 4"
+			},
+			{
+				subtitle: "Step Title 5",
+				imgsrc: "",
+				instruction: "INSTRUCTIONAL TEXT GOES HERE 5",
+				detail: "INPUT FIELD DESCRIPTION GOES HERE 5"
+			},
+			{
+				subtitle: "Step Title 6",
+				imgsrc: "",
+				instruction: "Confirm your selections before complete",
+				detail: ""
+			}
+		]),
+		wizResults: new enyo.Collection([
+			{step: "STEP TITLE 01 : ", result: " -- ", processed: "[FALSE]"},
+			{step: "STEP TITLE 02 : ", result: " -- ", processed: "[FALSE]"},
+			{step: "STEP TITLE 03 : ", result: " -- ", processed: "[FALSE]"},
+			{step: "STEP TITLE 04 : ", result: " -- ", processed: "[FALSE]"},
+			{step: "STEP TITLE 05 : ", result: " -- ", processed: "[FALSE]"},
+			{step: "STEP TITLE 06 : ", result: " -- ", processed: "[FALSE]"}
+		])
+	});
 
 	new enyo.Application({
 		view: {
 			classes: "enyo-unselectable moon",
 			components: [
 				{
-					kind: "moon.sample.wizard.FullScreenSample",
+					kind: "moon.Sample.Wizard.FullScreenSample",
 					controller: ".app.controllers.WizardController",
 					classes: "enyo-fit"
 				}
--- patterns-samples/Wizard/Wizard.Panel.js
+++ patterns-samples/Wizard/Wizard.Panel.js
@@ -1,10 +1,53 @@
 enyo.kind({
 	name: "moon.Sample.Wizard.Panel",
 	kind: "moon.Panel",
+	autoNumber: false,
+	events: {
+		onPrevious: "",
+		onNext: "",
+		onCancel: ""
+	},
+	components: [
+		{fit:true, kind: "moon.Scroller", horizontal: "hidden", components: [
+			{layoutKind: "FittableColumnsLayout", components: [
+				{name: "imgmenu", kind: "enyo.Image", classes: "moon-6h moon-8v"},
+				{fit: true, components: [
+					{name: "headline"},
+					{name: "detail"}
+				]}
+			]}
+		]},
+		{kind: "moon.Button", ontap: "doCancel", content: "Cancel"}
+	],
 	headerComponents: [
-		{classes: "wizard-nav-button-container moon-hspacing", components: [
-			{name: "prev", kind: "moon.Button", content: "Previous"},
-			{name: "post", kind: "moon.Button", content: "Next"}
-		]}
-	]
+		{name: "prev", kind: "moon.Button", ontap: "doPrevious", content: "Previous"},
+		{name: "post", kind: "moon.Button", ontap: "doNext", content: "Next"}
+	],
+	bindings: [
+		{from: ".controller.title", to: ".title"}
+	],
+	rendered: function() {
+		this.inherited(arguments);
+		this.initialSetting();
+	},
+	initialSetting: function() {
+		var idx = this.indexInContainer();
+		var subTitleIdx;
+		if (!idx) {
+			this.$.prev.hide();
+		}
+		if (idx < 9) {
+			subTitleIdx = "0" + (idx + 1);
+		}
+		var collection = this.controller.get('wizContainer');
+		var record = collection.at(idx);
+
+		this.$.header.setTitleBelow(subTitleIdx + ". " + record.get("subtitle"));
+		this.$.imgmenu.set("src", record.get("imgsrc"));
+		this.$.headline.set("content", record.get("instruction"));
+		this.$.detail.set("content", record.get("detail"));
+	},
+	finalSetting: function() {
+		this.$.post.setContent("DONE");
+	}
 });
