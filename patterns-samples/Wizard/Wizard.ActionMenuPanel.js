7746b370392b6460b08777c6b8b4a14912ab1700
David.Um <joonho.um.snu@gmail.com>
2013-11-22 00:35:00 +0900

    GF-52200: Fix img path
    Enyo-DCO-1.1-Signed-off-by: David Um <david.um@lge.com>

--- patterns-samples/Wizard/Wizard.ActionMenuPanel.js
+++ patterns-samples/Wizard/Wizard.ActionMenuPanel.js
@@ -2,7 +2,7 @@ enyo.kind({
 	name: "moon.Sample.Wizard.ActionMenuPanel",
 	kind: "moon.Sample.Wizard.FullPanel",
 	headerComponents: [
-		{name: "listAction", kind: "moon.ListActions", autoCollapse: true, iconSrc: "./assets/icon-list.png", listActions: [
+		{name: "listAction", kind: "moon.ListActions", autoCollapse: true, iconSrc: "../assets/icon-list.png", listActions: [
 			{action: "Cost", components: [
 				{kind: "moon.Divider", content:"Cost"},
 				{kind: "moon.Scroller", horizontal: "hidden", defaultKind: "moon.ToggleItem", fit: true, components: [
