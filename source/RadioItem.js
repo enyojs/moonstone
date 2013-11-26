684b30d540ace4a3cd05648f23868740ce75875e
Anish_Ramesan <anish.ramesan@lge.com>
2013-11-26 12:45:34 +0530

    Revert "GF-52731 updated RadioItem comments"
    This reverts commit 72631c6d45575c4a8671c49387dbcb9a4abafa29.

--- source/RadioItem.js
+++ source/RadioItem.js
@@ -2,8 +2,8 @@
 	_moon.RadioItem_ is a modified [moon.Item](#moon.Item) control designed for
 	use inside a [moon.RadioItemGroup](#moon.RadioItemGroup).
 
-	It contains the properties of [moon.SelectableItem](#moon.SelectableItem) with
-	an additional indicator component [moon.RadioItemIndicator](#moon.RadioItemIndicator).
+	For more information, see the documentation on [Radio
+	Items](building-apps/controls/radio-items.html) in the Enyo Developer Guide.
 */
 enyo.kind({
 	name: "moon.RadioItem",
