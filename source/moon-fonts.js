7581028fe95c52edd6838eddd1dde50330eec656
Blake Stephens <blake.stephens@lge.com>
2013-12-05 15:56:20 -0800

    Adding support for zh-HK Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- source/moon-fonts.js
+++ source/moon-fonts.js
@@ -2,6 +2,7 @@
     if (window.ilib) {
         var loc = new ilib.Locale();
         var language = loc.getLanguage();
+        var region = loc.getRegion();
         var style;
         if (language === 'ur') {
             style = document.createElement("style");
@@ -30,6 +31,24 @@
                 '        unicode-range: U+3000-U+303F;' +
                 '        unicode-range: U+3040-U+309F;' +
                 '        unicode-range: U+30A0-U+30FF;' +
+                '        unicode-range: U+3200-U+33FF;' +
+                '        unicode-range: U+3400-U+4DBF;' +
+                '        unicode-range: U+4E00-U+9FFF;' +
+                '        unicode-range: U+E000-U+FAFF;' +
+                '        unicode-range: U+FF00-U+FFEF;' +
+                '}';
+        }
+        else if (language === 'zh' && region === 'HK') {
+            style = document.createElement("style");
+            document.head.appendChild(style);
+            style.innerText =
+                '@font-face { ' +
+                '        font-family: "LG Display";' +
+                '        src: local("LG Display_HK");' +
+                '        font-weight: normal;' +
+                '        unicode-range: U+0000-U+00FF;' +
+                '        unicode-range: U+2E80-U+2EFF;' +
+                '        unicode-range: U+3000-U+303F;' +
                 '        unicode-range: U+3200-U+33FF;' +
                 '        unicode-range: U+3400-U+4DBF;' +
                 '        unicode-range: U+4E00-U+9FFF;' +
