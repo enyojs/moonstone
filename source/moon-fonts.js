a04f396589adb869fe07bca4614b4b47fc9312f4
Blake Stephens <blake.stephens@lge.com>
2013-12-09 14:33:15 -0800

    Reorganized moon-fonts.js to be simpler and not override itself. Added fallback fonts in case font names change. Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- source/moon-fonts.js
+++ source/moon-fonts.js
@@ -1,68 +1,72 @@
 (function() {
 	if (window.ilib) {
-		var loc = new ilib.Locale();
-		var language = loc.getLanguage();
-		var region = loc.getRegion();
-		var style;
+		var loc = new ilib.Locale(),
+			language = loc.getLanguage(),
+			region = loc.getRegion(),
+			styleContent = '';
+
 		if (language === 'ur') {
-			style = document.createElement("style");
-			document.head.appendChild(style);
-			style.innerText =
+			styleContent =
 				'@font-face { ' +
-				'	font-family: "LG Display";' +
-				'	src: local("LG Display_Urdu");' +
-				'	font-weight: normal;' +
-				'	unicode-range: U+0600-U+06FF;' +
-				'	unicode-range: U+FE70-U+FEFE;' +
-				'	unicode-range: U+FB50-U+FDFF;' +
+				'  font-family: "LG Display";' +
+				'  src: local("LG Display_Urdu");' +
+				'  font-weight: normal;' +
+				'  unicode-range: ' +
+				'U+0600-U+06FF, ' +
+				'U+FE70-U+FEFE, ' +
+				'U+FB50-U+FDFF;' +
 				'}';
 		}
 		else if (language === 'ja') {
-			style = document.createElement("style");
-			document.head.appendChild(style);
-			style.innerText =
+			styleContent =
 				'@font-face { ' +
-				'	font-family: "LG Display";' +
-				'	src: local("LG Display_JP");' +
-				'	font-weight: normal;' +
-				'	unicode-range: U+0000-U+00FF;' +
-				'	unicode-range: U+2E80-U+2EFF;' +
-				'	unicode-range: U+2F00-U+2FDF;' +
-				'	unicode-range: U+3000-U+303F;' +
-				'	unicode-range: U+3040-U+309F;' +
-				'	unicode-range: U+30A0-U+30FF;' +
-				'	unicode-range: U+3200-U+33FF;' +
-				'	unicode-range: U+3400-U+4DBF;' +
-				'	unicode-range: U+4E00-U+9FFF;' +
-				'	unicode-range: U+E000-U+FAFF;' +
-				'	unicode-range: U+FF00-U+FFEF;' +
+				'  font-family: "LG Display";' +
+				'  src: local("LG Display_JP");' +
+				'  font-weight: normal;' +
+				'  unicode-range: ' +
+				'U+0000-U+00FF, ' +
+				'U+2E80-U+2EFF,' +
+				'U+2F00-U+2FDF,' +
+				'U+3000-U+303F,' +
+				'U+3040-U+309F,' +
+				'U+30A0-U+30FF,' +
+				'U+3200-U+33FF,' +
+				'U+3400-U+4DBF,' +
+				'U+4E00-U+9FFF,' +
+				'U+E000-U+FAFF,' +
+				'U+FF00-U+FFEF;' +
 				'}';
 		}
 		else if (language === 'zh' && region === 'HK') {
-			style = document.createElement("style");
-			document.head.appendChild(style);
 			var unicodeRanges = 
-				'	unicode-range: U+0000-U+00FF;' +
-				'	unicode-range: U+2E80-U+2EFF;' +
-				'	unicode-range: U+3000-U+303F;' +
-				'	unicode-range: U+3200-U+33FF;' +
-				'	unicode-range: U+3400-U+4DBF;' +
-				'	unicode-range: U+4E00-U+9FFF;' +
-				'	unicode-range: U+E000-U+FAFF;' +
-				'	unicode-range: U+FF00-U+FFEF;';
-			style.innerText =
+				'U+0000-U+00FF,' +
+				'U+2E80-U+2EFF,' +
+				'U+3000-U+303F,' +
+				'U+3200-U+33FF,' +
+				'U+3400-U+4DBF,' +
+				'U+4E00-U+9FFF,' +
+				'U+E000-U+FAFF,' +
+				'U+FF00-U+FFEF';
+			styleContent =
 				'@font-face { ' +
-				'	font-family: "LG Display";' +
-				'	src: local("LG Display_HK");' +
-				'	font-weight: normal;' +
-				unicodeRanges +
+				'  font-family: "LG Display";' +
+				'  src: local("LG Display GP4_HK"), local("LG Display_HK-Regular");' +
+				'  font-weight: normal;' +
+				'  unicode-range: ' + unicodeRanges + ';' +
 				'} ' +
 				'@font-face { ' +
-				'	font-family: "LG Display Light";' +
-				'	src: local("LG Display_HK-Light");' +
-				'	font-weight: normal;' +
-				unicodeRanges +
+				'  font-family: "LG Display Light";' +
+				'  src: local("LG Display GP4_HK Light"), local("LG Display_HK-Light");' +
+				'  font-weight: normal;' +
+				'  unicode-range: ' + unicodeRanges + ';' +
 				'}';
+		}
+
+		if (styleContent) {
+			var style = document.createElement("style");
+			style.setAttribute("class", "enyo-localization-font-override");
+			document.head.appendChild(style);
+			style.innerText = styleContent;	
 		}
 	}
 })();
