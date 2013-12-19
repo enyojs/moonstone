c6912356593eb34eee48ad0999f76960a411ee6b
Gray Norton <gray.norton@lge.com>
2013-12-19 12:28:29 -0800

    Update docs for moon.selectionOverlaySupport.
    Enyo-DCO-1.1-Signed-Off-By: Gray Norton (gray.norton@lge.com)

--- source/SelectionOverlaySupport.js
+++ source/SelectionOverlaySupport.js
@@ -8,7 +8,7 @@
 
 	The item may define a _selectionScrimIcon_ URL to override the default icon.
 
-		{name: "list", selection: false, kind: "moon.DataList", components: [
+		{name: "list", selection: true, kind: "moon.DataList", components: [
 			{mixins: ["moon.SelectionOverlaySupport"], selectionScrimIcon: "assets/my-icon.png",
 				kind: "moon.ImageItem", bindings: [
 					{from: ".model.title", to: ".label"},
@@ -18,6 +18,21 @@
 			}
 		]}
 
+	By default, the overlay icon is centered horizontally and vertically over
+	the item, but you can override the default by specifying percentage values
+	for _selectionOverlayHorizontalOffset_ and _selectionOverlayVerticalOffset_.
+	Horizontal offset is measured from the left in left-to-right locales, and from
+	the right in right-to-left locales.
+
+		{name: "gridList", selection: true, kind: "moon.DataGridList", components: [
+			{mixins: ["moon.SelectionOverlaySupport"], kind: "moon.GridListImageItem",
+				selectionOverlayVerticalOffset: 35, bindings: [
+					{from: ".model.text", to: ".caption"},
+					{from: ".model.subText", to: ".subCaption"},
+					{from: ".model.url", to: ".source"}
+				]
+			}
+		]}
 */
 moon.SelectionOverlaySupport = {
 	name: "moon.SelectionOverlaySupport",
