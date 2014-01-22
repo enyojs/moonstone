791eb53c64c3cfda92cb0eee31233df31cde0007
brooke.peterson <brooke.peterson@lge.com>
2014-01-22 14:35:06 -0800

    ANG-276: placeholder: toggle item Enyo-DCO-1.1-Signed-off-by: Brooke Peterson <brooke.peterson@lge.com>

--- source/ToggleText.js
+++ source/ToggleText.js
@@ -1,6 +1,6 @@
 /**
 	_moon.ToggleText_, which extends [moon.Checkbox](#moon.Checkbox), is a control
-	that looks like a switch with labels for an "on" state and an "off" state.
+	that looks like a switch with icons for an "on" state and an "off" state.
 	When the ToggleText is tapped, it switches its state and fires an _onChange_
 	event.
 */
@@ -8,31 +8,15 @@
 enyo.kind({
 	name: "moon.ToggleText",
 	kind: "moon.Checkbox",
-	//* @public
-	published: {
-		//* Text label for the "on" state
-		onContent: moon.$L("on"),   // i18n "ON" label in moon.ToggleText / moon.ToggleItem widget
-		//* Text label for the "off" state
-		offContent: moon.$L("off")  // i18n "OFF" label in moon.ToggleText / moon.ToggleItem widget
-	},
 	//* @protected
-	classes: "moon-toggle-text",
-	components: [
-		{name: "label", classes: "moon-toggle-text-text"}
-	],
+	classes: "moon-toggle-indicator",
 	create: function() {
 		this.inherited(arguments);
 		this.checkedChanged();
 	},
 	checkedChanged: function() {
 		this.inherited(arguments);
-		this.$.label.setContent(this.getChecked() ? this.onContent : this.offContent);
-	},
-	shouldDoTransition: function(inChecked) {
-		return true;
-	},
-	createOverlayComponent: function() {
-		var content = this.getChecked() ? this.getOnContent() : this.getOffContent();
-		return this.createComponent({name: "overlay", classes: "moon-overlay", content: content});
+		this.addRemoveClass("moon-toggle-item-on", this.checked);
+		this.addRemoveClass("moon-toggle-item-off", !this.checked);		
 	}
 });
