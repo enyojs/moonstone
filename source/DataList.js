3a7a7a6431a84ba4ffcfd839b467499b19deca47
Blake Stephens <blake.stephens@lge.com>
2014-01-27 17:31:25 -0800

    Appeasing the Travis… Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- source/DataList.js
+++ source/DataList.js
@@ -23,7 +23,7 @@ moon.DataListSpotlightSupport = {
 			if (enyo.Spotlight.getPointerMode() &&
 				((spot = enyo.Spotlight.getCurrent()) && (spot === this || spot.isDescendantOf(this)))) {
 				// If we haven't enabled spotlightable controls,
-				if (this.$.scroller.spotlightPagingControls != true) {
+				if (!this.$.scroller.spotlightPagingControls) {
 					// go ahead and unspot whatever's currently spotted.
 					enyo.Spotlight.unspot();
 				}
