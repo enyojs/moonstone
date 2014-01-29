a429c9b5a2620ac7e7e5a59a6edc60acb20959c3
Blake Stephens <blake.stephens@lge.com>
2014-01-28 17:07:30 -0800

    Improve the reliability of locale-specific font definitions. Locale specific fonts are now re-evaluated every time enyo.updateLocale() is executed, allowing the fonts to stay in-sync with the currently set locale. * For the above feature to be supported, this change is dependent on https://github.com/enyojs/enyo-ilib/pull/39 * This commit includes a structural-indention change caused by wrapping code in a named function. Please diff with ignoring whitespace changes for best results. Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- source/moon-fonts.js
+++ source/moon-fonts.js
@@ -1,77 +1,98 @@
 (function() {
 	if (window.ilib) {
-		var loc = new ilib.Locale(),
-			language = loc.getLanguage(),
-			region = loc.getRegion(),
-			fontFamilyRegular = 'LG Display-Light',
-			fontFamilyBold    = 'LG Display-Regular',
-			fontFamilyLight   = 'LG Display-Light',
-			unicodeRanges = '';
 
-		if (language === 'ur') {
-			fontFamilyRegular = 'LG Display_Urdu';
-			fontFamilyBold    = 'LG Display_Urdu';
-			fontFamilyLight   = 'LG Display_Urdu';
-			unicodeRanges = 
-				'U+0600-U+06FF, ' +
-				'U+FE70-U+FEFE, ' +
-				'U+FB50-U+FDFF';
-		}
-		else if (language === 'ja') {
-			fontFamilyRegular = 'LG Display_JP';
-			fontFamilyBold    = 'LG Display_JP';
-			fontFamilyLight   = 'LG Display_JP';
-			unicodeRanges = 
-				'U+0000-U+00FF, ' +
-				'U+2E80-U+2EFF,' +
-				'U+2F00-U+2FDF,' +
-				'U+3000-U+303F,' +
-				'U+3040-U+309F,' +
-				'U+30A0-U+30FF,' +
-				'U+3200-U+33FF,' +
-				'U+3400-U+4DBF,' +
-				'U+4E00-U+9FFF,' +
-				'U+E000-U+FAFF,' +
-				'U+FF00-U+FFEF';
-		}
-		else if (language === 'zh' && region === 'HK') {
-			fontFamilyRegular = 'LG Display GP4_HK-Light';
-			fontFamilyBold    = 'LG Display GP4_HK-Regular';
-			fontFamilyLight   = 'LG Display GP4_HK-Light';
-			unicodeRanges = 
-				'U+0000-U+00FF,' +
-				'U+2E80-U+2EFF,' +
-				'U+3000-U+303F,' +
-				'U+3200-U+33FF,' +
-				'U+3400-U+4DBF,' +
-				'U+4E00-U+9FFF,' +
-				'U+E000-U+FAFF,' +
-				'U+FF00-U+FFEF';
+		var funEnyoLocaleChanged = enyo.updateLocale,
+			funLocaleSpecificFonts = null;
+
+		funLocaleSpecificFonts = function() {
+			var loc = new ilib.Locale(),
+				language = loc.getLanguage(),
+				region = loc.getRegion(),
+				fontFamilyRegular = 'LG Display-Light',
+				fontFamilyBold    = 'LG Display-Regular',
+				fontFamilyLight   = 'LG Display-Light',
+				unicodeRanges = '',
+				styleId = "enyo-localization-font-override",
+				styleElem = document.getElementById(styleId);
+
+			if (!styleElem) {
+				styleElem = document.createElement("style");
+				styleElem.setAttribute("id", styleId);
+				document.head.appendChild(styleElem);
+			}
+
+			if (language === 'ur') {
+				fontFamilyRegular = 'LG Display_Urdu';
+				fontFamilyBold    = 'LG Display_Urdu';
+				fontFamilyLight   = 'LG Display_Urdu';
+				unicodeRanges = 
+					'U+0600-U+06FF, ' +
+					'U+FE70-U+FEFE, ' +
+					'U+FB50-U+FDFF';
+			}
+			else if (language === 'ja') {
+				fontFamilyRegular = 'LG Display_JP';
+				fontFamilyBold    = 'LG Display_JP';
+				fontFamilyLight   = 'LG Display_JP';
+				unicodeRanges = 
+					'U+0000-U+00FF, ' +
+					'U+2E80-U+2EFF,' +
+					'U+2F00-U+2FDF,' +
+					'U+3000-U+303F,' +
+					'U+3040-U+309F,' +
+					'U+30A0-U+30FF,' +
+					'U+3200-U+33FF,' +
+					'U+3400-U+4DBF,' +
+					'U+4E00-U+9FFF,' +
+					'U+E000-U+FAFF,' +
+					'U+FF00-U+FFEF';
+			}
+			else if (language === 'zh' && region === 'HK') {
+				fontFamilyRegular = 'LG Display GP4_HK-Light';
+				fontFamilyBold    = 'LG Display GP4_HK-Regular';
+				fontFamilyLight   = 'LG Display GP4_HK-Light';
+				unicodeRanges = 
+					'U+0000-U+00FF,' +
+					'U+2E80-U+2EFF,' +
+					'U+3000-U+303F,' +
+					'U+3200-U+33FF,' +
+					'U+3400-U+4DBF,' +
+					'U+4E00-U+9FFF,' +
+					'U+E000-U+FAFF,' +
+					'U+FF00-U+FFEF';
+			}
+
+			if (unicodeRanges) {
+				styleElem.innerHTML = 
+					'@font-face { ' +
+					'  font-family: "Moonstone LG Display";' +
+					'  src: local("' + fontFamilyRegular + '");' +
+					'  font-weight: normal;' +
+					'  unicode-range: ' + unicodeRanges + ';' +
+					'} ' +
+					'@font-face { ' +
+					'  font-family: "Moonstone LG Display Bold";' +
+					'  src: local("' + fontFamilyBold + '");' +
+					'  font-weight: normal;' +
+					'  unicode-range: ' + unicodeRanges + ';' +
+					'} ' +
+					'@font-face { ' +
+					'  font-family: "Moonstone LG Display Light";' +
+					'  src: local("' + fontFamilyLight + '");' +
+					'  font-weight: normal;' +
+					'  unicode-range: ' + unicodeRanges + ';' +
+					'}';
+			}
+			else {
+				styleElem.innerHTML = '';
+			}
 		}
 
-		if (unicodeRanges) {
-			var style = document.createElement("style");
-			style.setAttribute("class", "enyo-localization-font-override");
-			document.head.appendChild(style);
-			style.innerText = 
-				'@font-face { ' +
-				'  font-family: "Moonstone LG Display";' +
-				'  src: local("' + fontFamilyRegular + '");' +
-				'  font-weight: normal;' +
-				'  unicode-range: ' + unicodeRanges + ';' +
-				'} ' +
-				'@font-face { ' +
-				'  font-family: "Moonstone LG Display Bold";' +
-				'  src: local("' + fontFamilyBold + '");' +
-				'  font-weight: normal;' +
-				'  unicode-range: ' + unicodeRanges + ';' +
-				'} ' +
-				'@font-face { ' +
-				'  font-family: "Moonstone LG Display Light";' +
-				'  src: local("' + fontFamilyLight + '");' +
-				'  font-weight: normal;' +
-				'  unicode-range: ' + unicodeRanges + ';' +
-				'}';
-		}
+		enyo.updateLocale = function() {
+			funEnyoLocaleChanged.apply(this,arguments);
+			funLocaleSpecificFonts();
+		};
+
+		funLocaleSpecificFonts();
 	}
 })();
