179c499ebdd777f2f82ef8c670385971c9fb3636
Blake Stephens <blake.stephens@lge.com>
2014-01-31 14:00:12 -0800

    Added ability to generate all custom-locale-fonts' rules programatically. Complete rewrite of of moon-fonts.js to allow for dynamic generation of all font family CSS rules needed by any language that caries its own specific font. All languages font-face rules are generated with language specific names so they can be referred to from within any locale currently selected. (In the case of Settings app, where there is a single locale chosen, but you still need to see some language/country strings in their native fonts.)
    Also made it easier to add new fonts for new languages, or new fonts for existing languages.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- source/moon-fonts.js
+++ source/moon-fonts.js
@@ -8,12 +8,99 @@
 			var loc = new ilib.Locale(),
 				language = loc.getLanguage(),
 				region = loc.getRegion(),
-				fontFamilyRegular = 'LG Display-Light',
-				fontFamilyBold    = 'LG Display-Regular',
-				fontFamilyLight   = 'LG Display-Light',
-				unicodeRanges = '',
-				styleId = "enyo-localization-font-override",
-				styleElem = document.getElementById(styleId);
+				styleId = "enyo-locale-font-override",
+				styleElem = document.getElementById(styleId),
+				fontDefinitionCss = "",
+				fonts = {
+					"NonLatin": {
+						regular: "LG Display-Light",
+						bold:    "LG Display-Regular"
+					},
+					"ja": {
+						regular: "LG Display_JP",
+						unicodeRanges:
+							'U+0000-U+00FF, ' +
+							'U+2E80-U+2EFF,' +
+							'U+2F00-U+2FDF,' +
+							'U+3000-U+303F,' +
+							'U+3040-U+309F,' +
+							'U+30A0-U+30FF,' +
+							'U+3200-U+33FF,' +
+							'U+3400-U+4DBF,' +
+							'U+4E00-U+9FFF,' +
+							'U+E000-U+FAFF,' +
+							'U+FF00-U+FFEF'
+					},
+					"ur": {
+						regular: "LG Display_Urdu",
+						unicodeRanges:
+							'U+0600-U+06FF, ' +
+							'U+FE70-U+FEFE, ' +
+							'U+FB50-U+FDFF'
+					},
+					"zh-HK": {
+						regular: "LG Display GP4_HK-Light",
+						bold:    "LG Display GP4_HK-Regular",
+						unicodeRanges:
+							'U+0000-U+00FF,' +
+							'U+2E80-U+2EFF,' +
+							'U+3000-U+303F,' +
+							'U+3200-U+33FF,' +
+							'U+3400-U+4DBF,' +
+							'U+4E00-U+9FFF,' +
+							'U+E000-U+FAFF,' +
+							'U+FF00-U+FFEF'
+					}
+				};
+
+			// Generate a single font-face rule
+			this.buildFont = function(inOptions) {
+				if (!inOptions && !inOptions.name) {
+					return "";
+				}
+				var strOut = '@font-face { \n' +
+					'  font-family: "' + inOptions.name + '";\n' +
+					'  font-weight: ' + ( inOptions.weight || 'normal' ) + ';\n';
+
+				if (inOptions.localName) {
+					strOut+= '  src: local("' + inOptions.localName + '");\n';
+				}
+				if (inOptions.unicodeRanges) {
+					strOut+= '  unicode-range: ' + inOptions.unicodeRanges + ';\n';
+				}
+				strOut+= '} \n';
+				return strOut;
+			};
+
+			// Generate a collection of font-face rules, in multiple font-variants
+			this.buildFontSet = function(strLang, bitDefault) {
+				var strOut = "",
+					name = (bitDefault) ? "" : " " + strLang;
+
+				if (fonts[strLang].regular) {
+					// Build Regular
+					strOut+= this.buildFont({
+						name: "Moonstone LG Display" + name,
+						localName: fonts[strLang].regular,
+						unicodeRanges: fonts[strLang].unicodeRanges
+					});
+
+					// Build Bold
+					strOut+= this.buildFont({
+						name: "Moonstone LG Display" + name + " Bold",
+						localName: fonts[strLang].bold || fonts[strLang].regular,
+						unicodeRanges: fonts[strLang].unicodeRanges
+					});
+
+					// Build Light
+					strOut+= this.buildFont({
+						name: "Moonstone LG Display" + name + " Light",
+						localName: fonts[strLang].light || fonts[strLang].regular,
+						unicodeRanges: fonts[strLang].unicodeRanges
+					});
+				}
+				return strOut;
+			};
 
 			if (!styleElem) {
 				styleElem = document.createElement("style");
@@ -21,71 +108,23 @@
 				document.head.appendChild(styleElem);
 			}
 
-			if (language === 'ur') {
-				fontFamilyRegular = 'LG Display_Urdu';
-				fontFamilyBold    = 'LG Display_Urdu';
-				fontFamilyLight   = 'LG Display_Urdu';
-				unicodeRanges = 
-					'U+0600-U+06FF, ' +
-					'U+FE70-U+FEFE, ' +
-					'U+FB50-U+FDFF';
-			}
-			else if (language === 'ja') {
-				fontFamilyRegular = 'LG Display_JP';
-				fontFamilyBold    = 'LG Display_JP';
-				fontFamilyLight   = 'LG Display_JP';
-				unicodeRanges = 
-					'U+0000-U+00FF, ' +
-					'U+2E80-U+2EFF,' +
-					'U+2F00-U+2FDF,' +
-					'U+3000-U+303F,' +
-					'U+3040-U+309F,' +
-					'U+30A0-U+30FF,' +
-					'U+3200-U+33FF,' +
-					'U+3400-U+4DBF,' +
-					'U+4E00-U+9FFF,' +
-					'U+E000-U+FAFF,' +
-					'U+FF00-U+FFEF';
-			}
-			else if (language === 'zh' && region === 'HK') {
-				fontFamilyRegular = 'LG Display GP4_HK-Light';
-				fontFamilyBold    = 'LG Display GP4_HK-Regular';
-				fontFamilyLight   = 'LG Display GP4_HK-Light';
-				unicodeRanges = 
-					'U+0000-U+00FF,' +
-					'U+2E80-U+2EFF,' +
-					'U+3000-U+303F,' +
-					'U+3200-U+33FF,' +
-					'U+3400-U+4DBF,' +
-					'U+4E00-U+9FFF,' +
-					'U+E000-U+FAFF,' +
-					'U+FF00-U+FFEF';
+			// Build all the fonts so they could be explicitly called
+			for (var lang in fonts) {
+				fontDefinitionCss+= this.buildFontSet(lang);
 			}
 
-			if (unicodeRanges) {
-				styleElem.innerHTML = 
-					'@font-face { ' +
-					'  font-family: "Moonstone LG Display";' +
-					'  src: local("' + fontFamilyRegular + '");' +
-					'  font-weight: normal;' +
-					'  unicode-range: ' + unicodeRanges + ';' +
-					'} ' +
-					'@font-face { ' +
-					'  font-family: "Moonstone LG Display Bold";' +
-					'  src: local("' + fontFamilyBold + '");' +
-					'  font-weight: normal;' +
-					'  unicode-range: ' + unicodeRanges + ';' +
-					'} ' +
-					'@font-face { ' +
-					'  font-family: "Moonstone LG Display Light";' +
-					'  src: local("' + fontFamilyLight + '");' +
-					'  font-weight: normal;' +
-					'  unicode-range: ' + unicodeRanges + ';' +
-					'}';
+			// Set up the override so "Moonstone LG Display" becomes the local-specific font.
+			if (language === "ja") {
+				fontDefinitionCss+= this.buildFontSet("ja", true);
 			}
-			else {
-				styleElem.innerHTML = '';
+			else if (language === "ur") {
+				fontDefinitionCss+= this.buildFontSet("ur", true);
 			}
+			else if (language === "zh" && region === "HK") {
+				fontDefinitionCss+= this.buildFontSet("zh-HK", true);
+			}
+
+			styleElem.innerHTML = fontDefinitionCss;
 		};
 
 		enyo.updateLocale = function() {
