6697214cb8a33e0c582d4ca5521476ad83c622fb
Kevin Schaaf <kevin.schaaf@lge.com>
2014-01-30 20:25:46 -0800

    Remove copyrighted assets from samples. Enyo-DCO-1.1-Signed-Off-By: Kevin Schaaf (kevin.schaaf@lge.com)

--- samples/ImageItemSample.js
+++ samples/ImageItemSample.js
@@ -10,19 +10,19 @@ enyo.kind({
 						components: [
 							{
 								kind: "moon.ImageItem",
-								source: "./assets/breaking_bad.png",
+								source: enyo.Image.placeholder,
 								label: "Breaking Bad",
 								text: "A struggling high school chemistry teacher who is diagnosed with inoperable lung cancer turns to a life of crime, producing and selling methamphetamine with a former student"
 							},
 							{
 								kind: "moon.ImageItem",
-								source: "./assets/south_park.png",
+								source: enyo.Image.placeholder,
 								label: "South Park",
 								text: "Follows the misadventures of four irreverent grade schoolers in the quiet, dysfunctional town of South Park, Colorado."
 							},
 							{
 								kind: "moon.ImageItem",
-								source: "./assets/paulie.png",
+								source: enyo.Image.placeholder,
 								label: "Paulie",
 								text: "Life from a parrot's point of view."
 							}
@@ -38,21 +38,21 @@ enyo.kind({
 						components: [
 							{
 								kind: "moon.ImageItem",
-								source: "./assets/breaking_bad.png",
+								source: enyo.Image.placeholder,
 								label: "Breaking Bad",
 								imageAlignRight: true,
 								text: "A struggling high school chemistry teacher who is diagnosed with inoperable lung cancer turns to a life of crime, producing and selling methamphetamine with a former student"
 							},
 							{
 								kind: "moon.ImageItem",
-								source: "./assets/south_park.png",
+								source: enyo.Image.placeholder,
 								label: "South Park",
 								imageAlignRight: true,
 								text: "Follows the misadventures of four irreverent grade schoolers in the quiet, dysfunctional town of South Park, Colorado."
 							},
 							{
 								kind: "moon.ImageItem",
-								source: "./assets/paulie.png",
+								source: enyo.Image.placeholder,
 								label: "Paulie",
 								imageAlignRight: true,
 								text: "Life from a parrot's point of view."
