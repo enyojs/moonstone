5bf2c2753851bd7f30533a069c8e4e65cddf8b64
Gray Norton <gray.norton@lge.com>
2014-02-13 19:12:56 -0800

    Restoring ToggleText. We'll create a new ToggleSwitch kind for the visual toggle.
    We may get rid of ToggleText since it's no longer used in ToggleItem, but leaving it for now.
    
    Enyo-DCO-1.1-Signed-Off-By: Gray Norton (gray.norton@lge.com)

--- source/ToggleText.js
+++ source/ToggleText.js
@@ -1,6 +1,6 @@
 /**
 	_moon.ToggleText_, which extends [moon.Checkbox](#moon.Checkbox), is a control
-	that looks like a switch with icons for an "on" state and an "off" state.
+	that looks like a switch with labels for an "on" state and an "off" state.
 	When the ToggleText is tapped, it switches its state and fires an _onChange_
 	event.
 */
@@ -8,15 +8,24 @@
 enyo.kind({
 	name: "moon.ToggleText",
 	kind: "moon.Checkbox",
+	//* @public
+	published: {
+		//* Text label for the "on" state
+		onContent: moon.$L("on"),   // i18n "ON" label in moon.ToggleText widget
+		//* Text label for the "off" state
+		offContent: moon.$L("off")  // i18n "OFF" label in moon.ToggleText widget
+	},
 	//* @protected
-	classes: "moon-toggle-indicator",
+	classes: "moon-toggle-text",
+	components: [
+		{name: "label", classes: "moon-toggle-text-text"}
+	],
 	create: function() {
 		this.inherited(arguments);
 		this.checkedChanged();
 	},
 	checkedChanged: function() {
 		this.inherited(arguments);
-		this.addRemoveClass("moon-toggle-item-on", this.checked);
-		this.addRemoveClass("moon-toggle-item-off", !this.checked);		
+		this.$.label.setContent(this.getChecked() ? this.onContent : this.offContent);
 	}
 });
