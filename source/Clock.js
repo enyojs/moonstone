94dcd4efca846b01e02692cc608bb068c0d9f881
Blake Stephens <blake.stephens@lge.com>
2014-02-24 17:25:57 -0800

    Switching Clock and TimePicker to use the factory method ilib.Date.newInstance() instead of a locale specific calendar/date object.

--- source/Clock.js
+++ source/Clock.js
@@ -133,20 +133,13 @@ enyo.kind({
 		}
 		this.initDefaults();
 	},
-	getILibDate: function(params){
-		if (this.ilibLocaleInfo.locale.spec === "th-TH") {
-			return new ilib.Date.ThaiSolarDate(params)
-		} else {
-			return new ilib.Date.GregDate(params)
-		}
-	},
 	updateHour: function(inDate, inHour) {
 		inHour = (inHour > 12 ? inHour-12: inHour) || 12;
-		var hour = this._hf ? this._hf.format(this.getILibDate({unixtime: inDate.getTime(), timezone:"UTC"})) : inHour;
+		var hour = this._hf ? this._hf.format(ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"UTC"})) : inHour;
 		this.$.hour.setContent(hour);
 	},
 	updateMinute: function(inDate, inHour) {
-		var time = this._mf ? this._mf.format(this.getILibDate({unixtime: inDate.getTime(), timezone:"UTC"})) : this._formatNumber(inDate.getMinutes());
+		var time = this._mf ? this._mf.format(ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"UTC"})) : this._formatNumber(inDate.getMinutes());
 		var meridiem = "";
 		if (!this.ilibLocaleInfo || this.ilibLocaleInfo.locale.spec === "en-US") {
 			meridiem = inHour > 11 ? "pm" : "am";
@@ -155,7 +148,7 @@ enyo.kind({
 		this.$.meridiem.setContent(meridiem);
 	},
 	updateMonthDay: function(inDate) {
-		var md = this._mdf ? this._mdf.format(this.getILibDate({unixtime: inDate.getTime(), timezone:"UTC"})) : this.months[inDate.getMonth()] + " " + this._formatNumber(inDate.getUTCDate());
+		var md = this._mdf ? this._mdf.format(ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"UTC"})) : this.months[inDate.getMonth()] + " " + this._formatNumber(inDate.getUTCDate());
 		this.$.bottom.setContent(md);
 	},
 	handleLocaleChangeEvent: function() {
