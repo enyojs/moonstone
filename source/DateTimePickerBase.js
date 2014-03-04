acc190d28930e36a322a5cc4897fcc18139fd2aa
Aaron Tam <aaron.tam@lge.com>
2014-03-04 00:38:00 -0800

    BHV-1368: Add wrapper div around ExpandableListItem header.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- source/DateTimePickerBase.js
+++ source/DateTimePickerBase.js
@@ -67,7 +67,9 @@ enyo.kind({
 	defaultOrdering: null, // set in subkind
 	components: [
 		{name: "headerWrapper", kind: "moon.Item", classes: "moon-date-picker-header-wrapper", onSpotlightFocus: "headerFocus", ontap: "expandContract", components: [
-			{name: "header", kind: "moon.MarqueeText", classes: "moon-expandable-list-item-header moon-expandable-picker-header"},
+			{name: "headerContainer", classes: "moon-expandable-list-item-header moon-expandable-picker-header", components: [
+				{name: "header", kind: "moon.MarqueeText"}
+			]},
 			{name: "currentValue", kind: "moon.MarqueeText", classes: "moon-expandable-picker-current-value"}
 		]},
 		{name: "drawer", kind: "enyo.Drawer", classes:"moon-expandable-list-item-client indented", components: [
