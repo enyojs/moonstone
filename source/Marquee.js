992e9f24860a83f790856507e0a8f22d1587312f
Kevin Schaaf <kevin.schaaf@lge.com>
2014-03-07 16:49:25 -0800

    BHV-1433: Apply the translateZ(0) to the div that's animating, not the container.  Fixes the layering issue without needing to use negative z-position. DCO-1.1-Signed-Off-By: Kevin Schaaf <kevin.schaaf@lge.com>

--- source/Marquee.js
+++ source/Marquee.js
@@ -369,7 +369,7 @@ moon.MarqueeItem = {
 		this.$.marqueeText.applyStyle("transition-duration", duration + "s");
 		this.$.marqueeText.applyStyle("-webkit-transition-duration", duration + "s");
 
-		enyo.dom.transform(this, {translateZ: -0.1});
+		enyo.dom.transform(this.$.marqueeText, {translateZ: 0});
 
 		// Need this timeout for FF!
 		setTimeout(this.bindSafely(function() {
@@ -388,7 +388,7 @@ moon.MarqueeItem = {
 		setTimeout(this.bindSafely(function() {
 			this.$.marqueeText.removeClass("animate-marquee");
 			enyo.dom.transform(this.$.marqueeText, {translateX: null});
-			enyo.dom.transform(this, {translateZ: null});
+			enyo.dom.transform(this.$.marqueeText, {translateZ: null});
 		}), enyo.platform.firefox ? 100 : 0);
 	},
 	//* Flips distance value for RTL support
