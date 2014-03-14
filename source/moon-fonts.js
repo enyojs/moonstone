c823877d9adb15134c2f10b7b897f16566618772
Blake Stephens <blake.stephens@lge.com>
2014-03-14 15:35:00 -0700

    Added documentation to make the function of this file more clear. Added zh-TW too. Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- source/moon-fonts.js
+++ source/moon-fonts.js
@@ -155,7 +155,7 @@
 			else if (language === "ur") {
 				fontDefinitionCss+= this.buildFontSet("ur", true);
 			}
-			else if (language === "zh" && region === "HK") {
+			else if (language === "zh" && (region === "HK" || region === 'TW')) {
 				fontDefinitionCss+= this.buildFontSet("zh-HK", true);
 			}
 			else if (language === "zh" && region === "TW") {
