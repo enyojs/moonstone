168faa1cc18d9d164a0f655e98c1bd4e0ff9038d
Aaron Tam <aaron.tam@lge.com>
2014-03-28 11:36:09 -0700

    ENYO-3600: Update documentation to not reference deprecated method.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- source/Scroller.js
+++ source/Scroller.js
@@ -2,12 +2,11 @@
 	_moon.Scroller_ extends [enyo.Scroller](#enyo.Scroller), adding support for
 	5-way focus (Spotlight) and pagination buttons.
 
-	_moon.Scroller_ responds to the _onSpotlightFocused_ event by scrolling the
-	event originator into view. This ensures that 5-way (Spotlight) focused
-	controls are always in view.
-
-	In addition, _moon.Scroller_ responds to explicit/programmatic requests from
+	_moon.Scroller_ responds to explicit/programmatic requests from
 	controls to be scrolled into view via the _onRequestScrollIntoView_ event.
+	These requests are generally made in response to the _onSpotlightFocused_
+	event handled by these controls and ensures that 5-way (Spotlight) focused
+	controls are in view.
 
 	For more information, see the documentation on
 	[Scrollers](building-apps/layout/scrollers.html) in the Enyo Developer Guide.
