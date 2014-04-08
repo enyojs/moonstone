186f630324837b90a2243a8b712fd5b7bd55bc29
Aaron Tam <aaron.tam@lge.com>
2014-04-08 10:21:22 -0700

    BHV-3181: Remove _marquee_checkRtl function that got included with the merge.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- source/Marquee.js
+++ source/Marquee.js
@@ -407,25 +407,6 @@ moon.MarqueeItem = {
 		if (this._marquee_puppetMaster) {
 			this._marquee_puppetMaster.resetMarquee();
 		}
-	},
-	_marquee_checkRtl: function() {
-		var content = this.content;
-		if (content && typeof content === "object") {
-			content = content.toString();
-		}
-		// Set RTL mode based on first character of content
-		if (content && content.length) {
-			var firstCharCode = content.charCodeAt(0);
-			// Check if within Hebrew or Arabic ranges (in addition to Syriac to reduce number of comparisons)
-			// Hebrew: 1424-1535
-			// Arabic: 1536-1791, 1872-1919, 64336-65023, 65136-65278
-			// Syriac: 1792-1871
-			var isRtl = ((firstCharCode >= 1424 && firstCharCode <= 1919) ||
-				(firstCharCode >= 64336 && firstCharCode <= 65023) ||
-				(firstCharCode >= 65136 && firstCharCode <= 65278));
-			this.rtl = isRtl;
-			this.applyStyle("direction", isRtl ? "rtl" : "ltr");
-		}
 	}
 };
 
