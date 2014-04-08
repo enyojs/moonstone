2c17b12c634400b420bfe12609701bfae8385dd7
Blake Stephens <blake.stephens@lge.com>
2014-04-07 19:02:12 -0700

    Reworked spinner to be GPU accelerated and customizable. Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/SpinnerSample.js
+++ samples/SpinnerSample.js
@@ -5,6 +5,11 @@ enyo.kind({
 		{kind: "moon.Divider", content: "Spinner"},
 		{kind: "moon.Spinner"},
 		{kind: "moon.Divider", content: "Spinner with Content"},
-		{kind: "moon.Spinner", content: "Loading..."}
+		{kind: "moon.Spinner", content: "Loading..."},
+
+		{kind: "moon.Divider", content: "GIF Spinner"},
+		{kind: "moon.SpinnerGif"},
+		{kind: "moon.Divider", content: "GIF Spinner with Content"},
+		{kind: "moon.SpinnerGif", content: "Loading..."}
 	]
 });
--- source/Spinner.js
+++ source/Spinner.js
@@ -16,6 +16,65 @@ enyo.kind({
 	//* @protected
 	classes: "moon-spinner",
 	//* @public
+	published: {
+		//* Set the background of the spinner to transparent (defaults to false)
+		transparent: false
+	},
+	components: [
+		{name: "decorator", classes: "moon-spinner-ball-decorator spin-ball-animation", components: [
+			{classes: "moon-spinner-ball moon-spinner-ball1"},
+			{classes: "moon-spinner-ball moon-spinner-ball2"},
+			{classes: "moon-spinner-ball moon-spinner-ball3"}
+		]},
+		{name: "client", classes: "moon-spinner-text"}
+	],
+	create: function() {
+		this.inherited(arguments);
+		this.contentChanged();
+		this.transparentChanged();
+		// We have to reinitialize the animation, so that all the start-delays sync-up
+		var self = this;
+		enyo.job("spinnerAnimationDelayFor"+this.id, function() {
+			// Remove an animation, stick on adifferent one, remove that, and put back the first
+			self.$.decorator.addRemoveClass("spin-ball-animation", 0);
+			self.$.decorator.addRemoveClass("propeller-ball-animation", 1);
+			self.$.decorator.addRemoveClass("spin-ball-animation", 1);
+			self.$.decorator.addRemoveClass("propeller-ball-animation", 0);
+		}, 1830);
+	},
+	//* @public
+	//* Hides the animating spinner.
+	stop: function() {
+		this.set("showing", false);
+	},
+	//* Shows the spinner with animation.
+	start: function() {
+		this.set("showing", true);
+	},
+	/** Toggle existing state of spinner.
+		If spinner is visible it will be removed and viceversa.
+	*/
+	toggle: function() {
+		this.set("showing", !this.get("showing"));
+	},
+	//* @protected
+	contentChanged: function() {
+		this.inherited(arguments);
+		this.$.client.setContent(this.content);
+		this.$.client.set("showing", !!this.content);
+		this.addRemoveClass("content", !!this.content);
+	},
+	transparentChanged: function() {
+		this.addRemoveClass("moon-spinner-transparent-background", !!this.get("transparent"));
+	}
+});
+
+
+enyo.kind({
+	name: "moon.SpinnerGif",
+	//* @protected
+	classes: "moon-spinner-gif",
+	//* @public
 	//* Hides the animating spinner.
 	stop: function() {
 		this.setShowing(false);
