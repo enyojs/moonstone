99729b098eb29d73852adf193604542bc48ad862
Aaron Tam <aaron.tam@lge.com>
2014-04-10 13:43:16 -0700

    BHV-4643: Add additional moon.Item samples.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- samples/ItemSample.js
+++ samples/ItemSample.js
@@ -10,7 +10,16 @@ enyo.kind({
 					{kind: "moon.Item", content: "Item 2 (Disabled)", disabled:true},
 					{kind: "moon.Item", content: "Item 3"},
 					{kind: "moon.Item", content: "Item with very long text that should truncate"},
-					{kind: "moon.Item", content: "Item   with   extra   spaces   that   should   truncate"}
+					{kind: "moon.Item", content: "Item   with   extra   spaces   that   should   truncate"},
+					{kind: "moon.Item", components: [
+						{kind: "moon.Icon", icon: "drawer"},
+						{tag: "span", content: "Item with components"}
+					]},
+					{kind: "moon.Item", components: [
+						{kind: "moon.MarqueeText", content: "Item with more complex components"},
+						{kind: "moon.Image", src: "http://placehold.it/440x80&text=Image+One", alt: "Image One"},
+						{kind: "moon.Image", src: "http://placehold.it/440x80&text=Image+Two", alt: "Image Two"}
+					]}
 				]
 			}
 		]}
