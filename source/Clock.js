eb64ffefbb4ec44b6e9b97d449d5b8fca9696c50
David.Um <david.um@lge.com>
2014-04-18 16:34:51 +0900

    Adjust existing API to "direct" mode.
    Enyo-DCO-1.1-Signed-off-by: David Um david.um@lge.com

--- source/Clock.js
+++ source/Clock.js
@@ -125,8 +125,14 @@ enyo.kind({
 		// reinitialize data format to use timezone: Etc/UTC
 	},
 	refreshJob: function() {
-		var d = new Date(Date.now() + this._timeDiff);
-		var h = d.getHours();
+		var d, h;
+		if (this.mode == "normal") {
+			d = new Date(Date.now() + this._timeDiff);
+			h = d.getHours();
+		} else {
+			d = this.date;
+			h = (this.date.hour) ? this.date.hour : 0;
+		}		
 		this.updateHour(d, h);
 		this.updateMinute(d, h);
 		this.updateMonthDay(d);
@@ -151,13 +157,34 @@ enyo.kind({
 		}
 		this.initDefaults();
 	},
+	/**	
+		If user sets time without using JS Date object, 
+		it should be parsed into array for ilib.Date object.
+	*/
+	parseDirectDate: function(inDate) {
+		return {
+			year: inDate.year ? inDate.year : 0,
+			month: inDate.month ? inDate.month : 0,
+			day: inDate.day ? inDate.day : 0,
+			hour: inDate.hour ? inDate.hour : 0,
+			minute: inDate.min ? inDate.min : 0,
+			second: inDate.sec ? inDate.sec : 0,
+			timezone: "Etc/UTC"
+		};
+	},
 	updateHour: function(inDate, inHour) {
 		inHour = (inHour > 12 ? inHour-12: inHour) || 12;
-		var hour = this._hf ? this._hf.format(ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"Etc/UTC"})) : inHour;
+		
+		var hour = this._hf ? this._hf.format((this.mode == "normal")	? ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"Etc/UTC"})
+																		: ilib.Date.newInstance(this.parseDirectDate(inDate)))
+							: inHour;
 		this.$.hour.setContent(hour);
 	},
 	updateMinute: function(inDate, inHour) {
-		var time = this._mf ? this._mf.format(ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"Etc/UTC"})) : this._formatNumber(inDate.getMinutes());
+		var time = this._mf ? this._mf.format((this.mode == "normal")	? ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"Etc/UTC"})
+																		: ilib.Date.newInstance(this.parseDirectDate(inDate))) 
+							: (this.mode == "normal")	? this._formatNumber(inDate.getMinutes())
+														: this._formatNumber(inDate.min);
 		var meridiem = "";
 		if (!this.ilibLocaleInfo || this.ilibLocaleInfo.locale.spec === "en-US") {
 			meridiem = inHour > 11 ? "pm" : "am";
@@ -166,7 +193,10 @@ enyo.kind({
 		this.$.meridiem.setContent(meridiem);
 	},
 	updateMonthDay: function(inDate) {
-		var md = this._mdf ? this._mdf.format(ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"Etc/UTC"})) : this.months[inDate.getMonth()] + " " + this._formatNumber(inDate.getUTCDate());
+		var md = this._mdf	? this._mdf.format((this.mode == "normal") ? ilib.Date.newInstance({unixtime: inDate.getTime(), timezone:"Etc/UTC"})
+																		: ilib.Date.newInstance(this.parseDirectDate(inDate))) 
+							: (this.mode == "normal")	? this.months[inDate.getMonth()] + " " + this._formatNumber(inDate.getUTCDate())
+														: inDate.month ? this.months[inDate.month] : 0 + " " + this._formatNumber(inDate.day);
 		this.$.bottom.setContent(md);
 	},
 	handleLocaleChangeEvent: function() {
