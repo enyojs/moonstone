5fae1e4dd524c5ed4b33c6b9eb6133b655c7c595
Aaron Tam <aaron.tam@lge.com>
2014-05-05 10:38:03 -0700

    Revert "Revert "Merge pull request #1266 from enyojs/LEX-BHV-3479""
    This reverts commit 6e8bfbc6b3c1f3485ca605086a32ba2146287fbc.
    
    There was a workaround for the regression that did not involve any additional changes to Moonstone (only in Spotlight), so we are reverting the reversion.

--- source/Scroller.js
+++ source/Scroller.js
@@ -91,8 +91,13 @@ enyo.kind({
 		until the edge of _inControl_ is aligned with the edge of the visible scroll
 		area. Optional third parameter to indicate whether or not it should animate
 		the scroll. Defaults to animation unless it is set to false.
+		If _setLastFocusedChild_ is true, scroller will set up _inControl_ to be the spotted child
+		when scroller is spotted.
 	*/
-	scrollToControl: function(inControl, inScrollFullPage, animate) {
+	scrollToControl: function(inControl, inScrollFullPage, animate, setLastFocusedChild) {
+		if (setLastFocusedChild) {
+			this.$.strategy.setLastFocusedChild(inControl);
+		}
 		this.$.strategy.animateToControl(inControl, inScrollFullPage, animate);
 	},
 
@@ -103,7 +108,7 @@ enyo.kind({
 	scrollTo: function (x, y, animate) {
 		this.$.strategy.scrollTo(x, y, animate);	
 	},
-		
+	
 	//* @protected
 	bindings: [
 		{from: ".scrollInterval",				to:".$.strategy.interval"},
