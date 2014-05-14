146f614462955f3f1a43a1a1b15619b61f3468fd
Aaron Tam <aaron.tam@lge.com>
2014-05-14 15:46:24 -0700

    BHV-693: Fixed spacing, updated samples, updated documentation.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- samples/ToggleButtonSample.js
+++ samples/ToggleButtonSample.js
@@ -6,15 +6,15 @@ enyo.kind({
 		{kind: 'moon.Scroller', fit: true, components: [
 			{classes: "moon-toggle-button-sample-wrapper", components: [
 				{kind: "moon.Divider", content: "Toggle Buttons:"},
-				{kind: "moon.ToggleButton", toggleOnLabel: "wifi is On", toggleOffLabel: "wifi is off", ontap: "buttonTapped"},
+				{kind: "moon.ToggleButton", toggleOnLabel: "wifi is on", toggleOffLabel: "wifi is off", ontap: "buttonTapped"},
 				{kind: "moon.ToggleButton", contentUpperCase : false, toggleOnLabel: "Internet connected", toggleOffLabel: "Internet disconnected", ontap: "buttonTapped"},
-				{kind: "moon.ToggleButton", disabled: true, toggleOnLabel: "Disabled Button Activated Value", toggleOffLabel: "Disabled Button Deactivated Value", ontap: "buttonTapped"},
+				{kind: "moon.ToggleButton", disabled: true, toggleOnLabel: "Disabled Active Button", toggleOffLabel: "Disabled Inactive Button", ontap: "buttonTapped"},
 				{kind: "moon.ToggleButton", disabled: true, value: true, content: "Disabled Active Button", ontap: "buttonTapped"},
-				{kind: "moon.ToggleButton", content: "Your set-top box is", ontap: "buttonTapped"},
+				{kind: "moon.ToggleButton", content: "Set-top box", ontap: "buttonTapped"},
 				{classes: "moon-1v"},
 				{kind: "moon.Divider", content: "Toggle Buttons are set on programmically by default:"},
 				{kind: "moon.ToggleButton", value: true, toggleOnLabel: "English", toggleOffLabel: "Spanish", ontap: "buttonTapped"},
-				{kind: "moon.ToggleButton", value: true, content: "Notifications:", ontap: "buttonTapped"},
+				{kind: "moon.ToggleButton", value: true, content: "Notifications", ontap: "buttonTapped"},
 				{classes: "moon-1v"},
 				{kind: "moon.Divider", content: "Captioned Buttons:"},
 				{kind: "moon.CaptionDecorator", side: "top", content: "Pow", components: [
@@ -32,9 +32,9 @@ enyo.kind({
 				{classes: "moon-1v"},
 				{kind: "moon.Divider", content: "Grouped Buttons:"},
 				{kind: "enyo.Group", classes: "moon-toggle-button-sample-group", components: [
-					{kind: "moon.ToggleButton", content: "iPod is", ontap: "buttonTapped"},
-					{kind: "moon.ToggleButton", toggleOnLabel: "Banana Ripen", toggleOffLabel: "Banana Green", ontap: "buttonTapped"},
-					{kind: "moon.ToggleButton", toggleOnLabel: "Saskatoonberry Sweet", toggleOffLabel: "Saskatoonberry Bitter", ontap: "buttonTapped"}
+					{kind: "moon.ToggleButton", content: "Apple", ontap: "buttonTapped"},
+					{kind: "moon.ToggleButton", toggleOnLabel: "Ripened Banana", toggleOffLabel: "Raw Banana", value: true, ontap: "buttonTapped"},
+					{kind: "moon.ToggleButton", toggleOnLabel: "Sweet Saskatoonberry", toggleOffLabel: "Bitter Saskatoonberry", ontap: "buttonTapped"}
 				]}
 			]}
 		]},
