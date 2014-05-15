4c73a8c545efb88e9d20d8570433afffc57e5e5f
Brooke Peterson <brooke.peterson@lge.com>
2014-05-14 20:02:19 -0700

    added the revised ToggleButtonSampel.js

--- samples/ToggleButtonSample.js
+++ samples/ToggleButtonSample.js
@@ -11,10 +11,13 @@ enyo.kind({
 				{kind: "moon.ToggleButton", disabled: true, toggleOnLabel: "Disabled Active Button", toggleOffLabel: "Disabled Inactive Button", ontap: "buttonTapped"},
 				{kind: "moon.ToggleButton", disabled: true, value: true, content: "Disabled Active Button", ontap: "buttonTapped"},
 				{kind: "moon.ToggleButton", content: "Set-top box", ontap: "buttonTapped"},
+				{kind: "moon.ToggleButton", small: true, content: "Small Toggle", ontap: "buttonTapped"},
+				{kind: "moon.ToggleButton", small: true, disabled: true, toggleOnLabel: "Small Disabled Active Button", toggleOffLabel: "Small Disabled Inactive Button", ontap: "buttonTapped"},
 				{classes: "moon-1v"},
 				{kind: "moon.Divider", content: "Toggle Buttons are set on programmically by default:"},
 				{kind: "moon.ToggleButton", value: true, toggleOnLabel: "English", toggleOffLabel: "Spanish", ontap: "buttonTapped"},
 				{kind: "moon.ToggleButton", value: true, content: "Notifications", ontap: "buttonTapped"},
+				{kind: "moon.ToggleButton", small: true, value: true, content: "Small Button", ontap: "buttonTapped"},
 				{classes: "moon-1v"},
 				{kind: "moon.Divider", content: "Captioned Buttons:"},
 				{kind: "moon.CaptionDecorator", side: "top", content: "Pow", components: [
@@ -34,7 +37,8 @@ enyo.kind({
 				{kind: "enyo.Group", classes: "moon-toggle-button-sample-group", components: [
 					{kind: "moon.ToggleButton", content: "Apple", ontap: "buttonTapped"},
 					{kind: "moon.ToggleButton", toggleOnLabel: "Ripened Banana", toggleOffLabel: "Raw Banana", value: true, ontap: "buttonTapped"},
-					{kind: "moon.ToggleButton", toggleOnLabel: "Sweet Saskatoonberry", toggleOffLabel: "Bitter Saskatoonberry", ontap: "buttonTapped"}
+					{kind: "moon.ToggleButton", toggleOnLabel: "Sweet Saskatoonberry", toggleOffLabel: "Bitter Saskatoonberry", ontap: "buttonTapped"},
+					{kind: "moon.ToggleButton", small: true, toggleOnLabel: "Blue Blueberry", toggleOffLabel: "Red Raspberry", ontap: "buttonTapped"}
 				]}
 			]}
 		]},
