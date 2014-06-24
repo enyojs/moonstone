2744cdb892be23cd2b12daa54750c2f96e5388d5
David.Um <david.um@lge.com>
2014-06-24 21:29:41 +0900

    ANG-10315 Reduce redundant function call only after models added, additional didScroll call could be required.
    Enyo-DCO-1.1-Signed-off-by: David Um david.um@lge.com

--- source/DataList.js
+++ source/DataList.js
@@ -101,12 +101,14 @@ moon.DataListSpotlightSupport = {
 				enyo.Spotlight.spot(spottableControl);
 				return true;
 			}
-		} else {
+		} else if (this.needToAdjustPages) {
 			var pagesForIndex = this.delegate.pageForIndex(this, inEvent.index),
 				pageCount = this.delegate.pageCount(this),
 				lastPageIndex = pages.lastPage.index;
+
 			if (pagesForIndex === lastPageIndex && pageCount -1 !== lastPageIndex) {
 				this.didScroll(this, {scrollBounds: {left: null, top: null, xDir: 1, yDir: 1}});
+				this.needToAdjustPages = false;
 			}
 		}
 	},
@@ -292,6 +294,8 @@ moon.DataListSpotlightSupport = {
 			this.unspotAndRememberFocus();
 			sup.apply(this, arguments);
 			this.restoreFocus();
+			// For specific case, page adjusting is required after models added
+			this.needToAdjustPages = true;
 		};
 	}),
 	modelsRemoved: enyo.inherit(function (sup) {
