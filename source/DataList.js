003938f8eea83b0d145b752b5e163dcdad92642f
Aaron Tam <aaron.tam@lge.com>
2014-07-09 20:58:27 -0700

    BHV-11483: Consolidate variable declarations.

--- source/DataList.js
+++ source/DataList.js
@@ -166,14 +166,20 @@ moon.DataListSpotlightSupport = {
 		var pages = (this.$.page1.index < this.$.page2.index) ?
 			[this.$.page1, this.$.page2] :
 			[this.$.page2, this.$.page1],
-			pageIdx;
+			pageIdx,
+			page,
+			pb,
+			i,
+			c,
+			cb;
+
 		for (pageIdx = 0; pageIdx < pages.length; pageIdx++) {
-			var page = pages[pageIdx];
-			var pb = page.getBounds();
+			page = pages[pageIdx];
+			pb = page.getBounds();
 			// Loop through children in each page top-down
-			for (var i=0; i<page.children.length; i++) {
-				var c = page.children[i];
-				var cb = c.getBounds();
+			for (i=0; i<page.children.length; i++) {
+				c = page.children[i];
+				cb = c.getBounds();
 				// Need to add page offset to target bounds
 				cb.top += pb.top;
 				cb.left += pb.left;
@@ -189,6 +195,7 @@ moon.DataListSpotlightSupport = {
 				}
 			}
 		}
+
 		return null;
 	},
 	// Retrieve the next/previous spottable child from the generated controls starting from the given index
