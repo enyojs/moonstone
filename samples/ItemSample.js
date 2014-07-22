5938079b50ee5953c9f609c0d45846ea57303183
Kunmyon Choi <kunmyon.choi@lge.com>
2014-07-22 09:37:18 -0700

    BHV-11870: Support moon.Item Icon Overlay
    Adding moon.ItemOverlay kind to support common item pattern.
     - Item is composed of label and icons
     - Label should have marquee
     - Icons can be on the left/right side of label
     - Icons can be shown on focus and auto hide
    
    DCO-1.1-Signed-Off-By: Kunmyon Choi kunmyon.choi@lge.com

--- samples/ItemSample.js
+++ samples/ItemSample.js
@@ -21,6 +21,48 @@ enyo.kind({
 						{kind: "moon.Image", src: "http://placehold.it/440x80&text=Image+Two", alt: "Image Two"}
 					]}
 				]
+			},
+			{classes:"moon-1v"},
+			{kind: "moon.Divider", content: "ItemOverlay Sample"},
+			{
+				components: [
+					{kind: "moon.Item", components: [
+						{kind: "moon.ItemOverlay", autoHide: true, right: true, components:[
+							{kind: "moon.Icon", icon: "arrowlargeup", small: true},
+							{kind: "moon.Icon", icon: "arrowlargedown", small: true},
+							{kind: "moon.Icon", icon: "arrowlargeleft", small: true},
+							{kind: "moon.Icon", icon: "arrowlargeright", small: true},
+						]},
+						{kind: "moon.MarqueeText", content: "Item   with   icons   auto   hides"}
+					]},
+					{kind: "moon.Item", components: [
+						{kind: "moon.ItemOverlay", autoHide: false, right: true, components:[
+							{kind: "moon.Icon", icon: "arrowlargeup", small: true},
+							{kind: "moon.Icon", icon: "arrowlargedown", small: true},
+							{kind: "moon.Icon", icon: "arrowlargeleft", small: true},
+							{kind: "moon.Icon", icon: "arrowlargeright", small: true},
+						]},
+						{kind: "moon.MarqueeText", content: "Item   with   icons   on   right   side"}
+					]},
+					{kind: "moon.Item", components: [
+						{kind: "moon.ItemOverlay", autoHide: false, right: false, components:[
+							{kind: "moon.Icon", icon: "arrowlargeup", small: true},
+							{kind: "moon.Icon", icon: "arrowlargedown", small: true},
+							{kind: "moon.Icon", icon: "arrowlargeleft", small: true},
+							{kind: "moon.Icon", icon: "arrowlargeright", small: true},
+						]},
+						{kind: "moon.MarqueeText", content: "Item   with   icons   on   left   side"}
+					]},
+					{kind: "moon.Item", components: [
+						{kind: "moon.ItemOverlay", autoHide: false, right: false, components:[
+							{kind: "moon.Icon", icon: "arrowlargeup", small: true}
+						]},
+						{kind: "moon.ItemOverlay", autoHide: false, right: true, components:[
+							{kind: "moon.Icon", icon: "arrowlargedown", small: true}
+						]},
+						{kind: "moon.MarqueeText", content: "Item   with   icons   on   both   sides"}
+					]}
+				]
 			}
 		]}
 	]
--- source/Item.js
+++ source/Item.js
@@ -85,4 +85,86 @@
 		}
 	});
 
+	/**
+	* _moon.ItemOverlay_ is a supplementary control that helps control layout inside of item.
+	* 
+	* ```
+	* {kind: "moon.Item", components: [
+	* 	{kind: "moon.ItemOverlay", autoHide: false, right: true, components:[
+	* 		{kind: "moon.Icon", icon: "arrowlargeup", small: true},
+	* 		{kind: "moon.Icon", icon: "arrowlargedown", small: true}
+	* 	]},
+	* 	{kind: "moon.MarqueeText", content: "Item   with   icons   auto   hides"}
+	* ]}
+	* ```
+	* 
+	* @ui
+	* @class moon.ItemOverlay
+	* @extends enyo.Control
+	* @public
+	*/
+	enyo.kind(
+		/** @lends moon.ItemOverlay.prototype */ {
+
+		 /**
+		 * @private
+		 */
+		name: 'moon.ItemOverlay',
+
+		/**
+	 	* @private
+	 	*/
+		classes: 'moon-item-overlay',
+
+		/**
+		* @private
+		*/
+		published: /** @lends moon.ItemOverlay.prototype */ {
+
+			/**
+			* When true, the controls are hidden but only shown on focus
+			*
+			* @type {Boolean}
+			* @default false
+			* @public
+			*/
+			autoHide: false,
+
+			/**
+			* When true, the controls are right aligned
+			*
+			* @type {Boolean}
+			* @default false
+			* @public
+			*/
+			right: false
+
+		},
+
+		/**
+		* @private
+		*/
+		create: function () {
+			this.inherited(arguments);
+			this.autoHideChanged();
+			this.rightChanged();
+		},
+
+		/**
+		* @private
+		*/
+		autoHideChanged: function() {
+			this.addRemoveClass('auto-hide', this.get('autoHide'));
+		},
+
+
+		/**
+		* @private
+		*/
+		rightChanged: function() {
+			this.addRemoveClass('right', this.get('right'));
+		}
+
+	});
+
 })(enyo, this);
