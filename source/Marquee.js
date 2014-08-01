693d81edfb5ae49b2d98169159fe5db783fafca1
Kunmyon Choi <kunmyon.choi@lge.com>
2014-08-01 16:09:31 -0700

    BHV-12260: Item border show and hide when marquee flow
    Issue:
    TranslateZ is creating H/W accelerated layer and make small blurred gap between normal and accelerated layer.
    
    Fix:
    Change translateZ value from -0.01 to "-0.1px".
    This fix can reduce the gap between H/W accelerated layer and normal layer.
    So that remove random blurred line show and hide.
    
    Previous PR is reverted because it was not reset translateZ to null. So, fixed it.
    
    DCO-1.1-Signed-Off-By: Kunmyon Choi kunmyon.choi@lge.com

--- source/Marquee.js
+++ source/Marquee.js
@@ -778,7 +778,7 @@
 			this.$.marqueeText.applyStyle('transition-duration', duration + 's');
 			this.$.marqueeText.applyStyle('-webkit-transition-duration', duration + 's');
 
-			enyo.dom.transform(this, {translateZ: -0.01});
+			enyo.dom.transform(this, {translateZ: '-0.1px'});
 
 			// Need this timeout for FF!
 			setTimeout(this.bindSafely(function () {
@@ -804,7 +804,7 @@
 			setTimeout(this.bindSafely(function () {
 				this.$.marqueeText.removeClass('animate-marquee');
 				enyo.dom.transform(this.$.marqueeText, {translateX: null});
-				enyo.dom.transform(this.$.marqueeText, {translateZ: null});
+				enyo.dom.transform(this, {translateZ: null});
 			}), enyo.platform.firefox ? 100 : 0);
 		},
 
