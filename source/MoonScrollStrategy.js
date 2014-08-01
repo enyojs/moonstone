31521465dc3039e788484a670a95b438d2222c0a
Cole Davis <cole.davis@lge.com>
2014-08-01 15:54:56 -0700

    Revert "BHV-11798: PopupSample: Error: Out of memory!"
    This reverts commit e2c78180b1c5d407f2093405a7a871a9fd4baac0.

--- source/MoonScrollStrategy.js
+++ source/MoonScrollStrategy.js
@@ -610,14 +610,12 @@
 		*/
 		requestSetupBounds: function(sender, event) {
 			this.scrollBounds = this._getScrollBounds();
-			if (this.scrollBounds.clientHeight && this.scrollBounds.clientWidth) {
-				this.setupBounds();
-				this.scrollBounds = null;
-				if ((this.showVertical() && this.$.scrollMath.bottomBoundary) || (this.showHorizontal() && this.$.scrollMath.rightBoundary)) {
-					this.alertThumbs();
-				}
-				return true;
+			this.setupBounds();
+			this.scrollBounds = null;
+			if ((this.showVertical() && this.$.scrollMath.bottomBoundary) || (this.showHorizontal() && this.$.scrollMath.rightBoundary)) {
+				this.alertThumbs();
 			}
+			return true;
 		},
 
 		/**
