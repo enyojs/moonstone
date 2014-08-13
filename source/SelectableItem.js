9ad4bce5981ec44b6999c6af5fd5d61f614ffd45
Jim Tang <jim.tang@lge.com>
2014-08-13 16:40:00 -0700

    JSDoc cleanup, continued. Enyo-DCO-1.1-Signed-Off-By: Jim Tang (jim.tang@lge.com)

--- source/SelectableItem.js
+++ source/SelectableItem.js
@@ -1,7 +1,6 @@
 (function (enyo, scope) {
 	/**
-	* Fires when the [`SelectableItem`]{@link moon.SelectableItem} is tapped. No event-specific data
-	* is sent with this event.
+	* Fires when the item is tapped. No event-specific data is sent with this event.
 	*
 	* @event moon.SelectableItem#onActivate
 	* @type {Object}
@@ -9,13 +8,14 @@
 	*/
 
 	/**
-	* `moon.SelectableItem` is a {@link moon.Item} with a flag to track selection state.  It is
-	* especially useful within the context of the [Enyo Group API]{@link enyo.Group}.
+	* {@link moon.SelectableItem} is a {@link moon.Item} with a flag to track
+	* selection state.  It is particularly useful within the context of the
+	* [Enyo Group API]{@link enyo.Group}.
 	*
 	* When selected, the item appears as underlined.
 	*
-	* If multiple `SelectableItem`s are used in a group, only one of them may be in
-	* the selected state at a given time.
+	* If multiple SelectableItem objects are used in a group, only one of them
+	* may be in the selected state at a given time.
 	*
 	* @class moon.SelectableItem
 	* @extends moon.Item
@@ -62,7 +62,7 @@
 		published: {
 
 			/**
-			* Is `true` if this item is currently selected; `false` if not.
+			* Will be `true` if this item is currently selected; otherwise, `false`.
 			*
 			* @type {Boolean}
 			* @default false
@@ -71,10 +71,11 @@
 			selected: false,
 
 			/**
-			* For use with the Enyo Group API; `true` if this item is the selected item in the
-			* group. Within this kind, [`selected`]{@link moon.SelectableItem#selected} and `active`
-			* appear to behave similarly; however, `active` is meant to be used by
-			* {@link enyo.Group}, while `selected` is to be used for changing selection state.
+			* For use with the [Enyo Group API]{@link enyo.Group}. Will be `true` if this item
+			* is the selected item in the group. Within this kind, the
+			* [selected]{@link moon.SelectableItem#selected} and `active` properties appear to
+			* behave similarly; however, `active` is meant to be used by {@link enyo.Group},
+			* while `selected` is to be used for changing selection state.
 			*
 			* @type {Boolean}
 			* @default false
@@ -163,9 +164,10 @@
 		},
 
 		/**
-		* For use with the Enyo Group API, which is supported by this object. Called when the active
-		* item within the group changes. The [`active`]{@link enyo.SelectableItem#active} property
-		* and [`onActivate`]{@link enyo.SelectableItem#event:onActivate} event are both part of the
+		* For use with the [Enyo Group API]{@link enyo.Group}, which is supported by this object.
+		* Called when the active item within the group changes. The
+		* [active]{@link enyo.SelectableItem#active} property and
+		* [onActivate]{@link enyo.SelectableItem#event:onActivate} event are both part of the
 		* Group API.
 		*
 		* @private
