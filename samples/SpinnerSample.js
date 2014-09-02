4ff3a4464da1cd49a1dd07080bacb9d0467eb88c
Blake Stephens <blake.stephens@lge.com>
2014-09-02 11:33:26 -0700

    Added example usage to the Spinner Sample.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/SpinnerSample.js
+++ samples/SpinnerSample.js
@@ -1,6 +1,7 @@
 enyo.kind({
 	name: "moon.sample.SpinnerSample",
 	classes: "moon enyo-unselectable enyo-fit",
+	kind: "moon.Scroller",
 	components: [
 		{kind: "moon.Divider", content: "Spinner"},
 		{kind: "moon.Spinner"},
@@ -10,6 +11,16 @@ enyo.kind({
 		{style: "text-align: center", components: [
 			{kind: "moon.Spinner", content: "Loading..."}
 		]},
+		{kind: "moon.Divider", content: "Spinner Centered Horizontally and Vertically in its Container"},
+		{classes: "absolute-container", components: [
+			{kind: "moon.Spinner", content: "Loading...", center: true}
+		]},
+		{classes: "moon-1v"},
+		{kind: "moon.Divider", content: "Spinner Only Centered Horizontally in its Container"},
+		{classes: "absolute-container", components: [
+			{kind: "moon.Spinner", content: "Loading...", center: true, middle: false}
+		]},
+		{classes: "moon-1v"},
 		{kind: "moon.Divider", content: "Spinner with Looooong Content"},
 		{kind: "moon.Spinner", content: "Loading so much content... This might take some arbitrary amount of time. Could be long, could be short. Who knows?"},
 		{kind: "moon.Divider", content: "Spinner with Components Inside"},
