1992aa5ba16fead9f7ec0a855ed0e41e6eea0ef8
art.dahm <art.dahm@lge.com>
2014-09-08 15:04:36 -0700

    BHV-14022 AlwaysViewingPanels: SubTitle's Marquee's Text Overlaps Tooltip

--- source/Marquee.js
+++ source/Marquee.js
@@ -794,7 +794,7 @@
 			this.$.marqueeText.applyStyle('transition-duration', duration + 's');
 			this.$.marqueeText.applyStyle('-webkit-transition-duration', duration + 's');
 
-			enyo.dom.transform(this, {translateZ: '-0.1px'});
+			enyo.dom.transform(this.$.marqueeText, {translateZ: '-0.1px'});
 
 			// Need this timeout for FF!
 			setTimeout(this.bindSafely(function () {
