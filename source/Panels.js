c5f557afab3103ae8a9d879dc112f3dd6d2ff8bc
Derek Anderson <derek.anderson@lge.com>
2014-09-30 16:46:32 -0700

    moved panel grow out of shared method

--- source/Panels.js
+++ source/Panels.js
@@ -674,7 +674,25 @@
 		* @public
 		*/
         setIndexDirect: function(inIndex) {
+            //set the new index
             this.toIndex = inIndex;
+
+            //if we are doing a direct transition
+            //we want to see if we should grow panels
+            //without animating
+            var panels = this.getPanels();
+            if(this.toIndex < this.index) {
+                //we need to grow panels with indices
+                //under the current one, to prevent
+                //animating later
+                for (var i = 0; i < this.index; i++) {
+                    if(!panels[i].offscreen){
+                        panels[i].grow();
+                    }
+                }
+            }
+
+            //change index of panel without animation
             this.skipArrangerAnimation();
         },
 
@@ -766,20 +784,6 @@
 		* @private
 		*/
 		skipArrangerAnimation: function () {
-
-            //if we are doing a direct transition
-            //we want to see if we should grow panels
-            //without animating
-            var panels = this.getPanels();
-            if(this.toIndex < this.index) {
-                //we need to grow panels with indices
-                //under the current one, to prevent
-                //animating later
-                for (var i = 0; i < this.index; i++) {
-                    panels[i].grow();
-                }
-            }
-
 			this._setIndex(this.toIndex);
 			this.completed();
 		},
