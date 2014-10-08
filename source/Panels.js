19dd02eacb7ba344957a594c70a2d9ac57f69378
Blake Stephens <blake.stephens@lge.com>
2014-10-08 15:52:25 -0700

    BHV-15685: Panels branding was causing unnecessary full-screen-repaints during every panel transition. This caused lag and blinking, as well as other performance issues. * Branding is now set once, instead of every panel transition. * Branding now has an appropriate size imposed on it. (Instead of none whatsoever) * Breadcrumb height is now a variable.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- source/Panels.js
+++ source/Panels.js
@@ -156,7 +156,9 @@
 		handleTools: [
 			{name: 'backgroundScrim', kind: 'enyo.Control', classes: 'moon-panels-background-scrim'},
 			{name: 'clientWrapper', kind: 'enyo.Control', classes: 'enyo-fill enyo-arranger moon-panels-client', components: [
-				{name: 'scrim', classes: 'moon-panels-panel-scrim'},
+				{name: 'scrim', classes: 'moon-panels-panel-scrim', components: [
+					{name: 'branding', kind: 'enyo.Image', sizing: 'contain', classes: 'moon-panels-branding'}
+				]},
 				{name: 'client', tag: null}
 			]},
 			{name: 'showHideHandle', kind: 'enyo.Control', classes: 'moon-panels-handle hidden', canGenerate: false, ontap: 'handleTap', onSpotlightLeft: 'handleSpotLeft', onSpotlightRight: 'handleSpotRight', onSpotlightFocused: 'handleFocused', onSpotlightBlur: 'handleBlur'},
@@ -417,6 +419,7 @@
 				// we need to ensure our handler has the opportunity to modify the flow during
 				// initialization
 				this.showingChanged();
+				this.brandingSrcChanged();
 			};
 		}),
 
@@ -937,9 +940,11 @@
 
 			this.inherited(arguments);
 
-			// Update display of branding image
-			if (this.getPanelInfo(0, this.index).breadcrumb !== this.getPanelInfo(0, this.inPrevious).breadcrumb) {
-				this.brandingSrcChanged();
+			// Can't check isPanelJoined on non-generated components
+			if (this.pattern === 'activity' && this.generated && !this.layout.isPanelJoined(this.index, 0)) {
+				this.$.branding.show();
+			} else {
+				this.$.branding.hide();
 			}
 		},
 
@@ -1222,9 +1227,7 @@
 		* @private
 		*/
 		brandingSrcChanged: function () {
-			if (this.pattern === 'activity') {
-				this.$.scrim.applyStyle('background-image', (this.brandingSrc && this.getPanelInfo(0, this.index).breadcrumb) ? 'url(' + enyo.path.rewrite(this.brandingSrc) + ')' : 'none');
-			}
+			this.$.branding.set('src', this.brandingSrc);
 		}
 	});
 
