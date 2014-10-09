5e192debb08d991608ad6378c074e33f8206b60e
Ryan Duffy <ryan.duffy@lge.com>
2014-10-09 14:12:31 -0500

    refactor logic to enable paging controls into new method
    Issue: BHV-15753
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- source/MoonScrollStrategy.js
+++ source/MoonScrollStrategy.js
@@ -512,6 +512,8 @@
 			;
 			s.bottomBoundary = -1 * b.maxTop;
 			s.rightBoundary = -1 * b.maxLeft;
+
+			this.updatePagingControlState();
 		},
 
 		/**
@@ -579,6 +581,22 @@
 			], function(c) {
 				c.addRemoveClass('hover', hover);
 			}, this);
+		},
+
+		/**
+		* @private
+		*/
+		updatePagingControlState: function () {
+			// Update disabled state of paging controls based on bounds
+			var m = this.$.scrollMath,
+				b = this._getScrollBounds(),
+				canVScroll = b.height > b.clientHeight,
+				canHScroll = b.width > b.clientWidth;
+
+			this.$.pageUpControl.setDisabled((b.top <= 0) || !canVScroll);
+			this.$.pageDownControl.setDisabled((b.top >= -1 * m.bottomBoundary) || !canVScroll);
+			this.$.pageLeftControl.setDisabled((b.left <= 0) || !canHScroll);
+			this.$.pageRightControl.setDisabled((b.left >= -1 * m.rightBoundary) || !canHScroll);
 		},
 
 		/**
@@ -772,16 +790,6 @@
 
 			b.maxLeft = Math.max(0, b.width - b.clientWidth);
 			b.maxTop = Math.max(0, b.height - b.clientHeight);
-
-			// Update disabled state of paging controls based on bounds
-			var m = this.$.scrollMath,
-				canVScroll = b.height > b.clientHeight,
-				canHScroll = b.width > b.clientWidth
-			;
-			this.$.pageUpControl.setDisabled((b.top <= 0) || !canVScroll);
-			this.$.pageDownControl.setDisabled((b.top >= -1 * m.bottomBoundary) || !canVScroll);
-			this.$.pageLeftControl.setDisabled((b.left <= 0) || !canHScroll);
-			this.$.pageRightControl.setDisabled((b.left >= -1 * m.rightBoundary) || !canHScroll);
 
 			enyo.mixin(b, this.getOverScrollBounds());
 
