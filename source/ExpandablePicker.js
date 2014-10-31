587db9b544bf3e4d5170d01cecc558b82cdd6654
DavidUm <david.um@lge.com>
2014-11-01 07:19:20 +0900

    ENYO-258 Add active property to checkboxItem
    and bound with active property of checkbox.
    
    Initially this.selected is null
    
    Enyo-DCO-1.1-Signed-off-by: David Um david.um@lge.com

--- source/ExpandablePicker.js
+++ source/ExpandablePicker.js
@@ -25,9 +25,9 @@
 	* ```
 	* {kind: 'moon.ExpandablePicker', noneText: 'None Selected', content: 'Choose City',
 	* components: [
-	* 	{content: 'San Francisco'},
-	* 	{content: 'Boston'},
-	* 	{content: 'Tokyo'}
+	*	{content: 'San Francisco'},
+	*	{content: 'Boston'},
+	*	{content: 'Tokyo'}
 	* ]}
 	* ```
 	*
@@ -199,27 +199,30 @@
 		* @private
 		*/
 		bindings: [
-			{from: '.allowHtml', to: '.$.header.allowHtml'},
-			{from: '.allowHtml', to: '.$.currentValue.allowHtml'},
-			{from: '.disabled', to: '.$.headerWrapper.disabled'}
+			{from: 'allowHtml', to: '$.header.allowHtml'},
+			{from: 'allowHtml', to: '$.currentValue.allowHtml'},
+			{from: 'disabled', to: '$.headerWrapper.disabled'}
 		],
 
 		/**
 		* @private
 		*/
-		create: function () {
-			this.inherited(arguments);
-			if (this.multipleSelection) {
-				this.selected = (this.selected) ? this.selected : [];
-				this.selectedIndex = (this.selectedIndex != -1) ? this.selectedIndex : [];
-				this.$.client.setHighlander(false);
-			}
-			this.initializeActiveItem();
-			this.selectedIndexChanged();
-			this.noneTextChanged();
-			this.helpTextChanged();
-			this.openChanged();
-		},
+		create: enyo.inherit(function (sup) {
+			return function() {
+				if (this.multipleSelection) {
+					this.selected = (this.selected) ? this.selected : [];
+					this.selectedIndex = (this.selectedIndex != -1) ? this.selectedIndex : [];
+				}
+				// super initialization
+				sup.apply(this, arguments);	
+				this.$.client.setHighlander(!this.multipleSelection);
+				this.initializeActiveItem();
+				this.selectedIndexChanged();
+				this.noneTextChanged();
+				this.helpTextChanged();
+				this.openChanged();
+			};
+		}),
 
 		/**
 		* @private
@@ -495,10 +498,12 @@
 
 			if (this.multipleSelection) {
 				if (index >= 0) {
+					// if toggledControl is checked but it is out of this.selected, them push it
 					if (inEvent.checked && (this.selected.indexOf(toggledControl) == -1)) {
 						this.selected.push(toggledControl);
 						this.selectedChanged();
 					}
+					// if toggledControl is not checked but it is in this.selected, them pull it out
 					if (!inEvent.checked && (this.selected.indexOf(toggledControl) >= 0)) {
 						this.selected.splice(this.selected.indexOf(toggledControl), 1);
 						this.selectedChanged();
