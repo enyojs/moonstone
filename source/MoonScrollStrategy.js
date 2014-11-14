60598d387b0b04a285fa65cac08b2f333b01f4b9
Aaron Tam <aaron.tam@lge.com>
2014-11-14 14:29:04 -0800

    debug: restore effectScroll

--- source/MoonScrollStrategy.js
+++ source/MoonScrollStrategy.js
@@ -524,12 +524,9 @@
 		* @private
 		*/
 		effectScroll: function(x, y) {
-			/*if (x !== this.scrollLeft || y !== this.scrollTop) {
-				this.scrollLeft = (x !== null && !isNaN(x))? x: (this.scrollLeft || 0);
-				this.scrollTop  = (y !== null && !isNaN(y))? y: (this.scrollTop  || 0);
-				enyo.dom.transformValue(this.$.client, this.translation, this.generateMatrix());
-				enyo.log('transform', this.scrollLeft, this.scrollTop);
-			}*/
+			this.scrollLeft = (x !== null && !isNaN(x))? x: (this.scrollLeft || 0);
+			this.scrollTop  = (y !== null && !isNaN(y))? y: (this.scrollTop  || 0);
+			enyo.dom.transformValue(this.$.client, this.translation, this.generateMatrix());
 		},
 
 		/**
