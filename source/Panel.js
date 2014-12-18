9de2cfbf6b3835f4b3bffac7223e90a743352b11
Ryan Duffy <ryan.duffy@lge.com>
2014-12-18 16:39:59 -0600

    add moon.config.accelerate support to Panels
    Issue: ENYO-594
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- source/Panel.js
+++ source/Panel.js
@@ -226,6 +226,16 @@
 		/**
 		* @private
 		*/
+		preventTransform: !moon.config.accelerate,
+
+		/**
+		* @private
+		*/
+		preventAccelerate: !moon.config.accelerate,
+
+		/**
+		* @private
+		*/
 		panelTools : [
 			{name: 'breadcrumb', ontap: 'handleBreadcrumbTap', classes: 'moon-panel-breadcrumb', components: [
 				{name: 'breadcrumbViewport', classes: 'moon-panel-breadcrumb-viewport', components: [
@@ -300,6 +310,14 @@
 		growing: false,
 
 		/**
+		* Set by {@link moon.BreadcrumbArranger} during {@link moon.BreadcrumbArranger#size}
+		* based on the value of {@link moon.Panels#animate}. 
+		*
+		* @private
+		*/
+		animate: !moon.config.accelerate,
+
+		/**
 		* @private
 		*/
 		create: function () {
@@ -313,6 +331,7 @@
 			// Note: This line will be deprecated soon. For backward compatiblity, I leave it for a while.
 			this.smallHeaderChanged();
 			this.headerTypeChanged();
+			this.animateChanged();
 		},
 
 		/**
@@ -417,6 +436,13 @@
 					this.expandHeader();
 				}
 			}
+		},
+
+		/**
+		* @private
+		*/
+		animateChanged: function () {
+			this.addRemoveClass('moon-composite', this.animate);
 		},
 
 		/**
