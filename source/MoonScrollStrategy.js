df29fa5f817f1f01485fcd7f8ecf44df4263b60d
Ryan Duffy <ryan.duffy@lge.com>
2014-12-30 14:48:13 -0600

    Revert "Merge pull request #1829 from enyojs/ENYO-592-krishnarangarajan-2.5"
    This reverts commit 5342dbef8a39fa4f68425349304b82ce78a2ee06, reversing
    changes made to 2772523ebf8cb918a7804ba2e1101f5ffcab7d06.

--- source/MoonScrollStrategy.js
+++ source/MoonScrollStrategy.js
@@ -491,7 +491,6 @@
 					val = this.scrollLeft - delta;
 					// When we hit the left, bounce and end scrolling
 					if (val <= -this.$.scrollMath.leftBoundary) {
-						this.$.pageRightControl.setDisabled(false);
 						this.setScrollLeft(-this.$.scrollMath.leftBoundary);
 						this.$.pageLeftControl.hitBoundary();
 					} else {
@@ -502,7 +501,6 @@
 					val = this.scrollTop - delta;
 					// When we hit the top, bounce and end scrolling
 					if (val <= -this.$.scrollMath.topBoundary) {
-						this.$.pageDownControl.setDisabled(false);
 						this.setScrollTop(-this.$.scrollMath.topBoundary);
 						this.$.pageUpControl.hitBoundary();
 					} else {
@@ -513,7 +511,6 @@
 					val = this.scrollLeft + delta;
 					// When we hit the right, bounce and end scrolling
 					if (val >= -this.$.scrollMath.rightBoundary) {
-						this.$.pageLeftControl.setDisabled(false);
 						this.setScrollLeft(-this.$.scrollMath.rightBoundary);
 						this.$.pageRightControl.hitBoundary();
 					} else {
@@ -525,7 +522,6 @@
 					val = this.scrollTop + delta;
 					// When we hit the bottom, bounce and end scrolling
 					if (val >= -this.$.scrollMath.bottomBoundary) {
-						this.$.pageUpControl.setDisabled(false);
 						this.setScrollTop(-this.$.scrollMath.bottomBoundary);
 						this.$.pageDownControl.hitBoundary();
 					} else {
