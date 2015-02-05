7c4a6a8710873b844c4a881f98acd8cfea9d4926
Aaron Tam <aaron.tam@lge.com>
2015-02-05 12:01:13 -0800

    ENYO-890: Prevent running animation when no arrangement is needed.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- source/Panels.js
+++ source/Panels.js
@@ -717,12 +717,13 @@
 			this.toIndex = index;
 
 			this.queuedIndex = null;
+			this._willMove = null;
 
 			// Ensure any VKB is closed when transitioning panels
 			this.blurActiveElementIfHiding(index);
 
 			// If panels will move for this index change, kickoff animation. Otherwise skip it.
-			if (this.shouldArrange() && this.animate) {
+			if (this.shouldAnimate()) {
 				enyo.Spotlight.mute(this);
 				// if back key feature is enabled and setIndex is not called from back key handler
 				if (this.allowBackKey && !moon.History.isHandlingBackAction()) {
@@ -761,8 +762,18 @@
 		},
 
 		/**
-		* Returns `true` if any panels will move in the transition from `fromIndex` to
+		* Returns `true` if the panels should animate in the transition from `fromIndex` to
 		* `toIndex`.
+		*
+		* @private
+		*/
+		shouldAnimate: function () {
+			this._willMove = this._willMove || (this._willMove == null && (this.shouldArrange() && this.animate));
+			return this._willMove;
+		},
+
+		/**
+		* Returns `true` if any panels will move in the transition from `fromIndex` to `toIndex`.
 		*
 		* @private
 		*/
