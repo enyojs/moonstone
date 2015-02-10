b4193af88623e72e19e16a511f0c43128e6ec971
Blake Stephens <blake.stephens@lge.com>
2015-02-10 15:33:22 -0800

    Switched from old SetContent style to newer set('content'... style, and whitespace and quoting improvements.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/DurationPickerSample.js
+++ samples/DurationPickerSample.js
@@ -1,11 +1,11 @@
 enyo.kind({
 	name: 'moon.sample.DurationPickerSample', components: [
-		{kind: 'FittableColumns', 
-		classes: 'moon enyo-unselectable enyo-fit',	components: [
-			{kind: 'FittableRows', 
+		{kind: 'FittableColumns',
+		classes: 'moon enyo-unselectable enyo-fit', components: [
+			{kind: 'FittableRows',
 			classes: 'moon enyo-unselectable', fit: true,
 			components: [
-				{kind: 'moon.Divider', content:'DurationPicker Full Version'},
+				{kind: 'moon.Divider', content: 'DurationPicker Full Version'},
 				{kind: 'moon.Scroller', fit: true, components: [
 					{classes: 'moon-7h moon-vspacing-s', components: [
 						{kind: 'moon.DurationPicker', name: 'pickerDuration', noneText: 'Pick Duration', content: 'Duration', countdown: true,
@@ -18,23 +18,23 @@ enyo.kind({
 					{kind: 'moon.Input', placeholder: 'h:m:s', name: 'newTemplate'}
 				]},
 				{kind: 'moon.Button', name: 'setTemplate', content: 'Set Template', ontap: 'setNewTemplate', small: true},
-				{kind: "moon.InputDecorator", components: [
-					{kind: "moon.Input", placeholder: '__:__:__', name: 'newValue'}
+				{kind: 'moon.InputDecorator', components: [
+					{kind: 'moon.Input', placeholder: '__:__:__', name: 'newValue'}
 				]},
 				{kind: 'moon.Button', name: 'setValue', content: 'Set Value', ontap: 'setNewValue', small: true},
-				{kind: 'moon.Divider', content:'Result'},
+				{kind: 'moon.Divider', content: 'Result'},
 				{kind: 'moon.BodyText', name: 'result', content: 'Duration Picker - Pick Duration'}
 			]},
 			{kind: 'FittableRows', classes: 'moon enyo-unselectable', components: [
-				{kind: 'moon.Divider', content:'DurationPicker Disabled'},
+				{kind: 'moon.Divider', content: 'DurationPicker Disabled'},
 				{kind: 'moon.Scroller', components: [
 					{classes: 'moon-7h moon-vspacing-s', components: [
 						{kind: 'moon.DurationPicker', name: 'pickerDurationDisabled', disabled: true, noneText: 'Pick Duration', content: 'Duration', countdown: true,
 							value: '20:20', template: 'm:s'}
 					]}
 				]},
-				{kind: 'moon.Scroller', fit:true, components: [
-					{kind: 'moon.Divider', content:'DurationPicker Non Editable'},
+				{kind: 'moon.Scroller', fit: true, components: [
+					{kind: 'moon.Divider', content: 'DurationPicker Non Editable'},
 					{classes: 'moon-7h moon-vspacing-s', components: [
 						{kind: 'moon.DurationPicker', name: 'pickerDurationNonEdit', disabledPicker: true, noneText: 'Pick Duration', content: 'Duration', countdown: true,
 							value: '50', template: 's'}
@@ -46,28 +46,28 @@ enyo.kind({
 	create: function(){
 		this.inherited(arguments);
 	},
-	durationChanged: function(inSender, inEvent) {
-		if (this.$.result && inEvent.value){
-			this.$.result.setContent(inEvent.name + ' changed to ' + inEvent.value);
+	durationChanged: function(sender, ev) {
+		if (this.$.result && ev.value) {
+			this.$.result.set('content', ev.name + ' changed to ' + ev.value);
 		}
 	},
-	countdownExpired: function(inSender, inEvent){
-		if (this.$.result && inEvent.name){
-			this.$.result.setContent(inEvent.name + ' expired');
+	countdownExpired: function(sender, ev) {
+		if (this.$.result && ev.name) {
+			this.$.result.set('content', ev.name + ' expired');
 		}
 	},
-	resetTapped: function(inSender, inEvent) {
+	resetTapped: function(sender, ev) {
 		this.$.pickerDuration.set('value', '');
-		this.$.result.setContent('DurationPicker -' + ' Pick Duration');
+		this.$.result.set('content', 'DurationPicker - Pick Duration');
 		return true;
 	},
-	countdownTapped: function(inSender, inEvent){
+	countdownTapped: function (sender, ev) {
 		this.$.pickerDuration.set('countdown', !this.$.pickerDuration.countdown);
 	},
-	setNewValue: function(){
+	setNewValue: function () {
 		this.$.pickerDuration.set('value', this.$.newValue.value);
 	},
-	setNewTemplate: function(){
+	setNewTemplate: function () {
 		this.$.pickerDuration.set('template', this.$.newTemplate.value);
 	}
 });
