8243520861e3043db3bf47ff0d5126cf59322956
Gray Norton <gray.norton@lge.com>
2015-04-15 14:58:00 -0700

    ENYO-1339: Avoid occasional stops when 5-way scrolling NewDataList
    Due to a race condition, NewDataList would sometimes stop scrolling
    in response to accelerated 5-way key events. This would happen when
    the items coming into view were not yet generated at the time the
    5-way events were handled.
    
    We guard against this happening by checking for this condition and
    returning true in the event handler, allowing events to be skipped
    until list generation catches up.
    
    Enyo-DCO-1.1-Signed-Off-By: Gray Norton (gray.norton@lge.com)

--- source/NewDataList.js
+++ source/NewDataList.js
@@ -6,7 +6,41 @@
 		kind: 'enyo.NewDataList',
 		scrollControls: [{kind: 'moon.ScrollControls'}],
 		touch: false,
-		mixins: [MoonScrollable]
+		mixins: [MoonScrollable],
+		handlers: {
+			onSpotlightUp: 'guard5way',
+			onSpotlightRight: 'guard5way',
+			onSpotlightDown: 'guard5way',
+			onSpotlightLeft: 'guard5way'
+		},
+		guard5way: function (sender, event) {
+			var e, v, idx, maxIdx, dec, inc;
+
+			if (!enyo.Spotlight.Accelerator.isAccelerating()) return false;
+
+			e = event.type;
+			v = this.direction === 'vertical';
+			idx = event.originator.index;
+			maxIdx = this.get('data').length - 1;
+
+			if (v) {
+				dec = 'onSpotlightUp';
+				inc = 'onSpotlightDown';
+			}
+			else {
+				dec = 'onSpotlightLeft';
+				inc = 'onSpotlightRight';
+			}
+
+			switch (e) {
+				case dec:
+					return (this.first > 0) && ((idx - this.first) <= this.dim2extent);
+				case inc:
+					return (this._last < maxIdx) && ((this._last - idx) <= this.dim2extent);
+				default:
+					return false;
+			}
+		}
 	});
 
 })(enyo, moon, this);
