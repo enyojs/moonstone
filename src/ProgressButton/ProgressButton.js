567aa943e96e91496889659b0b78030a57409187
Roy Sutton <roy.sutton@lge.com>
2015-08-28 14:48:15 -0700

    Cap displayed percent at 100
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton <roy.sutton@lge.com>

--- src/ProgressButton/ProgressButton.js
+++ src/ProgressButton/ProgressButton.js
@@ -230,7 +230,7 @@ module.exports = kind(
 				this.$.bar.applyStyle('-webkit-transform', 'translateX(-' + (100 + offset - progress) + '%)');
 			}
 		} else if (percent >= 100) {
-			this.updateProgressPercent(percent);
+			this.updateProgressPercent(100);
 			// Make it spottable again, now that it's finished.
 			this.$.bar.applyStyle('transform', null);
 			this.$.bar.applyStyle('-webkit-transform', null);
