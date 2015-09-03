1ac97f6d18218062cad394e46de29e215ff00344
Artem Krasnyi <krasnyi.artem@lge.com>
2015-09-03 14:27:13 +0300

    ENYO-426: Marquee shows ellipsis but Does Not start rolling in moon.InputHeader Enyo-DCO-1.1-Signed-off-by: Artem Krasnyi <krasnyi.artem@lge.com>

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -277,8 +277,7 @@ module.exports = kind(
 		oninput: 'handleInput',
 		onchange: 'handleChange',
 		onRequestCreateListActions: 'handleRequestCreateComponents',
-		onListActionOpenChanged: 'handleListActionOpenChanged',
-        onSpotlightKeyDown: 'handleSpotlightKeyDown'
+		onListActionOpenChanged: 'handleListActionOpenChanged'
 	},
 
 	/**
@@ -306,8 +305,8 @@ module.exports = kind(
 		{name: 'titleWrapper', kind: Control, classes: 'moon-header-title-wrapper', components: [
 			{name: 'title', kind: MarqueeText, classes: 'moon-header-text moon-header-title', canGenerate: false},
 			{name: 'inputDecorator', kind: InputDecorator, classes: 'moon-input-header-input-decorator',canGenerate: false, components: [
-                {name: 'titleInput', kind: Input, classes: 'moon-header-text moon-header-title',  onblur: 'marqueeToggle'},
-                {name: 'titleInputMarquee', kind: MarqueeText, classes: 'moon-header-text moon-header-title moon-input header-marquee', ontap: 'marqueeToggle', showing: false}
+                {name: 'titleInput', kind: Input, classes: 'moon-header-text moon-header-title',  onblur: 'inputBlur'},
+                {name: 'titleInputMarquee', kind: MarqueeText, classes: 'moon-header-text moon-header-title moon-input header-marquee hidden', marqueeOnRender: true, ontap: "marqueToggle"}
 			]}
 		]},
 		{name: 'titleBelow', kind: MarqueeText, classes: 'moon-sub-header-text moon-header-title-below'},
@@ -801,19 +800,8 @@ module.exports = kind(
 	*/
     handleChange: function (inSender, inEvent) {
         this.doInputHeaderChange({originalEvent: util.clone(inEvent, true)});
-        this.$.titleInputMarquee.set('content', inEvent.originator.getValue());
-    },
-
-    /**
-    * Handling Enter press, to act as tap.
-    *
-    * @private
-    */
-
-    handleSpotlightKeyDown: function (inSender, inEvent) {
-        if (inEvent.keyCode == 13) {
-            this.marqueeToggle();
-        }
+        this.$.titleInputMarquee.setContent(event.originator.getValue());
+        this.marqueToggle();
     },
 
     /**
@@ -821,14 +809,18 @@ module.exports = kind(
     *
     * @private
     */
-
-    marqueeToggle: function (){
-        if (this.$.titleInputMarquee.get('content') === '') return;
-
-            var isInputShowing = this.$.titleInput.get('showing');
-            this.$.titleInput.set('showing', !isInputShowing);
-            this.$.titleInputMarquee.set('showing', isInputShowing);
-        },
+    marqueToggle: function (){
+        if (this.$.titleInputMarquee.getContent() === '') return ;
+        if (this.$.titleInput.hasClass('hidden')){
+            this.$.titleInput.removeClass('hidden');
+            this.$.titleInputMarquee.addClass('hidden');
+        }
+    },
+    inputBlur: function () {
+        if (this.$.titleInputMarquee.getContent() === '') return ;
+        this.$.titleInput.addClass('hidden');
+        this.$.titleInputMarquee.removeClass('hidden');
+    },
 
 
 	/**
