bff8a56a0d972e8d3259e1d6bc4a743da7d79cf1
Jaewon Jang <jaewon98.jang@lgepartner.com>
2015-09-18 15:56:25 +0900

    ENYO-2499: Accessibility: Screen reader reads popup contents repeatedly when an event occurs in the popup.
    Issue
    Screen reader try read the popup contents repeatedly.
    
    Cause
    We set the alert role in Panels and LightPanels to read title automatically
    and set aria-live 'off' in Panel and LightPanel, but we can use any control
    in Panels not Panel in this case this issue occurs.
    
    Fix
    The alert role is needed in case there is title in panel, so check whether
    title is exist or not.
    
    https://jira2.lgsvl.com/browse/ENYO-2499
    Enyo-DCO-1.1-Signed-off-by: Jaewon Jang <jaewon98.jang@lgepartner.com>

--- src/LightPanels/LightPanels.js
+++ src/LightPanels/LightPanels.js
@@ -81,7 +81,9 @@ module.exports = kind(
 
 			while (--l >= 0) {
 				panel = panels[l];
-				panel.set('accessibilityRole', panel === active ? 'alert' : 'region');
+				if (panel.title) {
+					panel.set('accessibilityRole', panel === active ? 'alert' : 'region');
+				}
 			}
 		}}
 	]
