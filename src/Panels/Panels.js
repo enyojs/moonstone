4673a652e483e5706ef98bd00c1ab969c5250e3c
Aaron Tam <aaron.tam@lge.com>
2016-03-03 13:57:17 -0800

    ENYO-3161: Always adjust panel when changing indices.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- src/Panels/Panels.js
+++ src/Panels/Panels.js
@@ -1527,14 +1527,14 @@ module.exports = kind(
 	indexChanged: function (was) {
 		var current, delta, deltaAbs, idx;
 
+		this.adjustFirstPanelBeforeTransition();
+
 		if (this.getPanels().length > 0) {
 			this.assignBreadcrumbIndex();
 
 			// Set animation direction to use proper timing function before start animation
 			// This direction is only consumed by MoonAnimator.
 			this.$.animator.direction = this.getDirection();
-
-			this.adjustFirstPanelBeforeTransition();
 
 			// Push or drop history, based on the direction of the index change
 			if (this.allowBackKey) {
