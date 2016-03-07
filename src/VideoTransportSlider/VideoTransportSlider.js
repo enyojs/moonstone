c3e7d198bfa7e31fd3216805f626d1ed9ce4b269
jeremy.thomas <jeremy.thomas@lge.com>
2016-03-07 18:02:33 -0500

    ENYO-3093: Fix video-slider focus bug

--- src/VideoTransportSlider/VideoTransportSlider.js
+++ src/VideoTransportSlider/VideoTransportSlider.js
@@ -9,6 +9,8 @@ var
 	kind = require('enyo/kind'),
 	dom = require('enyo/dom'),
 	ri = require('enyo/resolution'),
+	gesture = require('enyo/gesture'),
+	util = require('enyo/utils'),
 	Control = require('enyo/Control'),
 	Popup = require('enyo/Popup');
 
@@ -454,6 +456,9 @@ module.exports = kind(
 		this.selected = false;
 		this.removeClass('visible');
 		this.endPreview();
+		if (gesture.downEvent) {
+			gesture.up(util.clone(gesture.downEvent));
+		}
 		//fires enyo.VideoTransportSlider#onLeaveTapArea
 		this.doLeaveTapArea();
 	},
