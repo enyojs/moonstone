0ee47e57a58bca547fff4a41a286d21b85260c6c
Jason Robitaille <jason.robitaille@lge.com>
2016-03-30 15:27:43 -0700

    Update version string to 2.6.4-rc.1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var
 	gesture = require('enyo/gesture');
 
 exports = module.exports = require('./src/options');
-exports.version = '2.6.3-rc.2';
+exports.version = '2.6.4-rc.1';
 
 // Override the default holdpulse config to account for greater delays between keydown and keyup
 // events in Moonstone with certain input devices.
