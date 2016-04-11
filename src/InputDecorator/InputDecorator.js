9812da71811b0f3b0996fb0ba5ee77ceecb7951e
bongsub.kim <bongsub.kim@lgepartner.com>
2016-04-11 13:40:38 +0900

    ENYO-3317: Change 'edit box' string in audio guidance
    According to accessibility UX changes, replace 'edit box' to 'input field'
    string.
    
    https://jira2.lgsvl.com/browse/ENYO-3317
    Enyo-DCO-1.1-Signed-off-by: Bongsub Kim <bongsub.kim@lgepartner.com>
    
    Change-Id: I52d5d01fe497af559ee00b42df07334213a0afae

--- src/InputDecorator/InputDecorator.js
+++ src/InputDecorator/InputDecorator.js
@@ -416,12 +416,12 @@ module.exports = kind(
 			this.set('accessibilityLive', this.focused || !this.spotted ? null : 'polite');
 			if (oInput) {
 				if (oInput instanceof RichText && oInput.hasNode()) {
-					text = (oInput.hasNode().innerText || oInput.getPlaceholder()) + ' ' + $L('edit box');
+					text = (oInput.hasNode().innerText || oInput.getPlaceholder()) + ' ' + $L('input field');
 				} else if (oInput.type == 'password' && oInput.getValue()) {
 					var character = (oInput.getValue().length > 1) ? $L('characters') : $L('character');
-					text = oInput.getValue().length + ' ' + character + ' ' + $L('edit box');
+					text = oInput.getValue().length + ' ' + character + ' ' + $L('input field');
 				} else {
-					text = (oInput.getValue() || oInput.getPlaceholder()) + ' ' + $L('edit box');
+					text = (oInput.getValue() || oInput.getPlaceholder()) + ' ' + $L('input field');
 				}
 			}
 			this.set('accessibilityLabel', this.spotted && !this.focused ? text : null);
