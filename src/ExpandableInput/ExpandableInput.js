e8639a739348a0da5653858a4089152295746bfe
rajyavardhanp <rajyavardhan.p@lge.com>
2016-06-27 15:40:57 +0530

    update base on review
    Enyo-DCO-1.1-Signed-off-by: Rajyavardhan Reddy P
    <rajyavardhan.p@lge.com>

--- src/ExpandableInput/ExpandableInput.js
+++ src/ExpandableInput/ExpandableInput.js
@@ -6,7 +6,8 @@ require('moonstone');
 */
 
 var
-	kind = require('enyo/kind');
+	kind = require('enyo/kind'),
+	platform = require('enyo/platform');
 
 var
 	Spotlight = require('spotlight');
@@ -241,7 +242,7 @@ module.exports = kind(
 	*/
 	headerDown: function (sender, ev) {
 		Spotlight.unfreeze();
-		if(event.type == 'touchstart') {
+		if(platform.touch && event.type == 'touchstart') {
 			ev.preventDefault();
 		}
 	},
