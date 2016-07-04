2cb1a0884ce73d00b37c1640822a500bce66b8e5
Kunmyon Choi <kunmyon.choi@lge.com>
2016-07-04 20:03:34 +0900

    Add space between number and colon.

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -160,7 +160,7 @@ module.exports = kind(
 	* @private
 	*/
 	right: function () {
-		var end = this.rtl ? 0: this.node.value.length;
+		var end = this.rtl ? 0 : this.node.value.length;
 		if (!this.hasNode() || this.node.selectionStart == end) {
 			return false;
 		}
