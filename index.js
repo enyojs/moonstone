549acdc7f52fe957a10fb9660c67da04c57d801c
Yeram Choi <yeram.choi@lge.com>
2017-08-23 17:45:22 +0900

    Update version string to 2.6.4-rc.16

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (option.brandTheme) {
 }
 
 exports = module.exports = option;
-exports.version = '2.6.4-rc.15';
+exports.version = '2.6.4-rc.16';
 
 // Override the default holdpulse config to account for greater delays between keydown and keyup
 // events in Moonstone with certain input devices.
