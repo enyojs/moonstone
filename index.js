581ba391ea2d0e921ec81d473463061291551a9e
Jason Robitaille <jason.robitaille@lge.com>
2017-11-15 19:02:39 -0800

    Update version string to 2.6.4-rc.18.1

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (option.brandTheme) {
 }
 
 exports = module.exports = option;
-exports.version = '2.6.4-rc.18';
+exports.version = '2.6.4-rc.18.1';
 
 // Override the default holdpulse config to account for greater delays between keydown and keyup
 // events in Moonstone with certain input devices.
